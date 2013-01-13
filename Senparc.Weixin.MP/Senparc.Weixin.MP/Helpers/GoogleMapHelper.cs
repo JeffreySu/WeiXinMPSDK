744c711f81642efa6e1c4370683d1b38f34420cd
JeffreySu <www.jeffrey.su@gmail.com>
2013-01-13 18:41:45 +0800

    add google map helper

---
+++ Senparc.Weixin.MP/Senparc.Weixin.MP/Helpers/GoogleMapHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Senparc.Weixin.MP.Helpers
+{
+    public static class GoogleMapHelper
+    {
+        /// <summary>
+        /// 获取谷歌今天静态地图Url。API介绍：https://developers.google.com/maps/documentation/staticmaps/?hl=zh-CN
+        /// </summary>
+        /// <returns></returns>
+        public static string GetGoogleStaticMap(double x, double y, int scale, string label, string size = "640x640", string color = "red")
+        {
+            string parameters = string.Format("center=&zoom=&size={0}&maptype=roadmap&format=jpg&sensor=false", size);
+            string url = "https://maps.googleapis.com/maps/api/staticmap?" + parameters;
+            return url;
+        }
+    }
+}

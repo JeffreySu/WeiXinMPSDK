d090a5b51c129452ababd34c5a18d9db372cd35e
JeffreySu <www.jeffrey.su@gmail.com>
2013-03-19 14:16:44 +0800

    添加GpsHelper，提供计算量间距离的方法

---
+++ Senparc.Weixin.MP/Senparc.Weixin.MP/Helpers/GpsHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Senparc.Weixin.MP.Helpers
+{
+    public class GpsHelper
+    {
+        /// <summary>
+        ///计算两点GPS坐标的距离
+        /// </summary>
+        /// <param name="n1">第一点的纬度坐标</param>
+        /// <param name="e1">第一点的经度坐标</param>
+        /// <param name="n2">第二点的纬度坐标</param>
+        /// <param name="e2">第二点的经度坐标</param>
+        /// <returns></returns>
+        public static double Distance(double n1, double e1, double n2, double e2)
+        {
+            double jl_jd = 102834.74258026089786013677476285;
+            double jl_wd = 111712.69150641055729984301412873;
+            double b = Math.Abs((e1 - e2) * jl_jd);
+            double a = Math.Abs((n1 - n2) * jl_wd);
+            return Math.Sqrt((a * a + b * b));
+        }
+    }
+}

76ed7decd012e2135f17bd4ae17f46e8ffde8a24
MagicHu <huweimin@cn.ibm.com>
2013-03-23 23:03:06 +0800

    1.删除了Enter和Location事件 2.追加了subscribe，Unsubscribe，Click事件

---
+++ Senparc.Weixin.MP/Senparc.Weixin.MP/Entities/Request/RequestMessageEvent_UnSubscribe.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Senparc.Weixin.MP.Entities
+{
+    public class RequestMessageEvent_UnSubscribe : RequestMessageEventBase, IRequestMessageEventBase
+    {
+    }
+}

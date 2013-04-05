e82b04669f4b8cec8a05a10d2c3c252fabc12c13
JeffreySu <www.jeffrey.su@gmail.com>
2013-04-05 19:01:57 +0800

    为MessageHandler添加简单测试

---
+++ Senparc.Weixin.MP/Senparc.Weixin.MP.Test/MessageHandlers/MessageHandlersTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Xml.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Senparc.Weixin.MP.Entities;
+using Senparc.Weixin.MP.MessageHandlers;
+
+namespace Senparc.Weixin.MP.Test.MessageHandlers
+{
+    public class CustomerMessageHandlers : MessageHandler
+    {
+        public CustomerMessageHandlers(XDocument requestDoc)
+            : base(requestDoc)
+        {
+        }
+
+        public override Entities.IResponseMessageBase OnTextRequest(Entities.RequestMessageText requestMessage)
+        {
+            var responseMessage =
+               ResponseMessageBase.CreateFromRequestMessage(RequestMessage, ResponseMsgType.Text) as
+               ResponseMessageText;
+            responseMessage.Content = "文字信息";
+            return responseMessage;
+        }
+    }
+
+    [TestClass]
+    public class MessageHandlersTest
+    {
+        string xmlText = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<xml>
+    <ToUserName><![CDATA[gh_a96a4a619366]]></ToUserName>
+    <FromUserName><![CDATA[olPjZjsXuQPJoV0HlruZkNzKc91E]]></FromUserName>
+    <CreateTime>1357986928</CreateTime>
+    <MsgType><![CDATA[text]]></MsgType>
+    <Content><![CDATA[TNT2]]></Content>
+    <MsgId>5832509444155992350</MsgId>
+</xml>
+";
+
+        [TestMethod]
+        public void TextMessageRequestTest()
+        {
+            var messageHandlers = new CustomerMessageHandlers(XDocument.Parse(xmlText));
+            Assert.IsNotNull(messageHandlers.RequestDocument);
+            Assert.IsNotNull(messageHandlers.RequestDocument);
+            messageHandlers.Execute();
+            Assert.IsNotNull(messageHandlers.ResponseMessage);
+            Assert.IsNotNull(messageHandlers.ResponseDocument);
+            Assert.AreEqual("gh_a96a4a619366", messageHandlers.ResponseMessage.FromUserName);
+
+            var responseMessage = messageHandlers.ResponseMessage as ResponseMessageText;
+            Assert.IsNotNull(responseMessage);
+            Assert.AreEqual("文字信息", responseMessage.Content);
+        }
+    }
+}

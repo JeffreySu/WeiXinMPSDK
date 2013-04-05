d5a0cb6c2e72f8f35c0fc8778abd4c47382004fe
JeffreySu <www.jeffrey.su@gmail.com>
2013-04-05 19:17:58 +0800

    v 0.3.3.0 提供更加简化的消息处理操作

--- Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/Controllers/WeixinController.cs
+++ Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/Controllers/WeixinController.cs
@@ -15,7 +15,7 @@ namespace Senparc.Weixin.MP.Sample.Controllers
     using Senparc.Weixin.MP.Sample.Service;
     using Senparc.Weixin.MP.Sample.CustomerMessageHandler;
 
-    public class WeixinController : Controller
+    public partial class WeixinController : Controller
     {
         private LocationService _locationService;
         private EventService _eventService;
@@ -88,117 +88,12 @@ namespace Senparc.Weixin.MP.Sample.Controllers
             }
         }
 
-        /// <summary>
-        /// 用户发送消息后，微信平台自动Post一个请求到这里，并等待响应XML
-        /// PS：此方法为常规switch判断方法，从v0.3.3版本起，此Demo不再更新
-        /// </summary>
-        [HttpPost]
-        [ActionName("OldIndex")]
-        public ActionResult OldPost(string signature, string timestamp, string nonce, string echostr)
-        {
-            if (!CheckSignature.Check(signature, timestamp, nonce, Token))
-            {
-                return Content("参数错误！");
-            }
-            XDocument requestDoc = null;
-            try
-            {
-                requestDoc = XDocument.Load(Request.InputStream);
-
-                var requestMessage = RequestMessageFactory.GetRequestEntity(requestDoc);
-                //如果不需要记录requestDoc，只需要：
-                //var requestMessage = RequestMessageFactory.GetRequestEntity(Request.InputStream);
-
-                requestDoc.Save(Server.MapPath("~/App_Data/" + DateTime.Now.Ticks + "_Request_" + requestMessage.FromUserName + ".txt"));//测试时可开启，帮助跟踪数据
-                ResponseMessageBase responseMessage = null;
-                switch (requestMessage.MsgType)
-                {
-                    case RequestMsgType.Text://文字
-                        {
-                            //TODO:交给Service处理具体信息，参考/Service/EventSercice.cs 及 /Service/LocationSercice.cs
-                            var strongRequestMessage = requestMessage as RequestMessageText;
-                            var strongresponseMessage =
-                                ResponseMessageBase.CreateFromRequestMessage(requestMessage, ResponseMsgType.Text) as
-                                ResponseMessageText;
-                            strongresponseMessage.Content =
-                                string.Format(
-                                    "您刚才发送了文字信息：{0}\r\n您还可以发送【位置】【图片】【语音】等类型的信息，查看不同格式的回复。\r\nSDK官方地址：http://weixin.senparc.com",
-                                    strongRequestMessage.Content);
-                            responseMessage = strongresponseMessage;
-                            break;
-                        }
-                    case RequestMsgType.Location://位置
-                        {
-                            responseMessage = _locationService.GetResponseMessage(requestMessage as RequestMessageLocation);
-                            break;
-                        }
-                    case RequestMsgType.Image://图片
-                        {
-                            //TODO:交给Service处理具体信息
-                            var strongRequestMessage = requestMessage as RequestMessageImage;
-                            var strongresponseMessage =
-                                ResponseMessageBase.CreateFromRequestMessage(requestMessage, ResponseMsgType.News) as
-                                ResponseMessageNews;
-                            strongresponseMessage.Content = "这里是正文内容，一共将发2条Article。";
-                            strongresponseMessage.Articles.Add(new Article()
-                                                                   {
-                                                                       Title = "您刚才发送了图片信息",
-                                                                       Description = "您发送的图片将会显示在边上",
-                                                                       PicUrl = strongRequestMessage.PicUrl,
-                                                                       Url = "http://weixin.senparc.com"
-                                                                   });
-                            strongresponseMessage.Articles.Add(new Article()
-                                                                   {
-                                                                       Title = "第二条",
-                                                                       Description = "第二条带连接的内容",
-                                                                       PicUrl = strongRequestMessage.PicUrl,
-                                                                       Url = "http://weixin.senparc.com"
-                                                                   });
-                            responseMessage = strongresponseMessage;
-                            break;
-                        }
-                    case RequestMsgType.Voice://语音
-                        {
-                            //TODO:交给Service处理具体信息
-                            var strongRequestMessage = requestMessage as RequestMessageVoice;
-                            var strongresponseMessage =
-                               ResponseMessageBase.CreateFromRequestMessage(requestMessage, ResponseMsgType.Music) as
-                               ResponseMessageMusic;
-                            strongresponseMessage.Music.MusicUrl = "http://weixin.senparc.com/Content/music1.mp3";
-                            responseMessage = strongresponseMessage;
-                            break;
-                        }
-                    case RequestMsgType.Event://事件
-                        {
-                            responseMessage = _eventService.GetResponseMessage(requestMessage as RequestMessageEventBase);
-                            break;
-                        }
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-                var responseDoc = MP.Helpers.EntityHelper.ConvertEntityToXml(responseMessage);
-                responseDoc.Save(Server.MapPath("~/App_Data/" + DateTime.Now.Ticks + "_Response_" + responseMessage.ToUserName + ".txt"));//测试时可开启，帮助跟踪数据
-
-                return Content(responseDoc.ToString());
-                //如果不需要记录responseDoc，只需要：
-                //return Content(responseMessage.ConvertEntityToXmlString());
-            }
-            catch (Exception ex)
-            {
-                using (
-                    TextWriter tw = new StreamWriter(Server.MapPath("~/App_Data/Error_" + DateTime.Now.Ticks + ".txt")))
-                {
-                    tw.WriteLine(ex.Message);
-                    tw.WriteLine(ex.InnerException.Message);
-                    if (requestDoc != null)
-                    {
-                        tw.WriteLine(requestDoc.ToString());
-                    }
-                    tw.Flush();
-                    tw.Close();
-                }
-                return Content("");
-            }
-        }
+        /*
+         * v0.3.0之前的原始Post方法见：WeixinController_OldPost.cs
+         * 
+         * 注意：虽然这里提倡使用CustomerMessageHandler的方法，但是MessageHandler基类最终还是基于OldPost的判断逻辑，
+         * 因此如果需要深入了解Senparc.Weixin.MP内部处理消息的机制，可以查看WeixinController_OldPost.cs中的OldPost方法。
+         * 目前为止OldPost依然有效，依然可用于生产。
+         */
     }
 }

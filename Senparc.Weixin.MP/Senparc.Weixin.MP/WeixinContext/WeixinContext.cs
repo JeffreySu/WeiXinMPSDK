be54d61860ae9fc3cdaae1d228d18daf2a1e95d2
JeffreySu <www.jeffrey.su@gmail.com>
2013-04-26 10:49:34 +0800

    添加WeixinContext，上下文

---
+++ Senparc.Weixin.MP/Senparc.Weixin.MP/WeixinContext/WeixinContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Senparc.Weixin.MP.WeixinContext
+{
+    public class WeixinContext
+    {
+    }
+}

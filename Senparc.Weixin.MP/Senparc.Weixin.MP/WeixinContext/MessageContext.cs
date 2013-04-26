2a282339c51c910cbd986d4cf4b08f2314043575
JeffreySu <www.jeffrey.su@gmail.com>
2013-04-26 10:55:46 +0800

    add MessageContext

---
+++ Senparc.Weixin.MP/Senparc.Weixin.MP/WeixinContext/MessageContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Senparc.Weixin.MP.WeixinContext
+{
+    /// <summary>
+    /// 微信消息上下文（单个用户）
+    /// </summary>
+    public class MessageContext
+    {
+        /// <summary>
+        /// 用户名（OpenID）
+        /// </summary>
+        public string UserName { get; set; }
+    }
+}
--- Senparc.Weixin.MP/Senparc.Weixin.MP/WeixinContext/WeixinContext.cs
+++ Senparc.Weixin.MP/Senparc.Weixin.MP/WeixinContext/WeixinContext.cs
@@ -5,7 +5,16 @@ using System.Text;
 
 namespace Senparc.Weixin.MP.WeixinContext
 {
+    /// <summary>
+    /// 微信消息上下文（全局）
+    /// </summary>
     public class WeixinContext
     {
+        public static object WeixinContextLock = new object();
+        public static Dictionary<string, MessageContext> MessageCollection = new Dictionary<string, MessageContext>(StringComparer.OrdinalIgnoreCase);
+
+        public WeixinContext()
+        {
+        }
     }
 }

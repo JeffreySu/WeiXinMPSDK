87549b2228637600689fe45ba586e1d4c074159f
JeffreySu <www.jeffrey.su@gmail.com>
2013-05-01 03:17:56 +0800

    丰富注释

--- Senparc.Weixin.MP/Senparc.Weixin.MP/Entities/Response/ResponseMessageNews.cs
+++ Senparc.Weixin.MP/Senparc.Weixin.MP/Entities/Response/ResponseMessageNews.cs
@@ -23,6 +23,9 @@ namespace Senparc.Weixin.MP.Entities
             }
         }
 
+        /// <summary>
+        /// 文章列表，微信客户端只能输出前10条（可能未来数字会有变化，出于视觉效果考虑，建议控制在8条以内）
+        /// </summary>
         public List<Article> Articles { get; set; }
 
         public ResponseMessageNews()

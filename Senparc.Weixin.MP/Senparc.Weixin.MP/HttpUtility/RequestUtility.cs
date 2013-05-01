67bebd340f8112a99a33b99270668ab9b0549096
JeffreySu <www.jeffrey.su@gmail.com>
2013-05-01 16:01:42 +0800

    添加Senparc.Weixin.MP.HttpUtility.RequestUtility.IsWeixinClientRequest()方法，用于判断请求是否发起自微信客户端的浏览器

--- Senparc.Weixin.MP/Senparc.Weixin.MP/HttpUtility/RequestUtility.cs
+++ Senparc.Weixin.MP/Senparc.Weixin.MP/HttpUtility/RequestUtility.cs
@@ -14,5 +14,16 @@ namespace Senparc.Weixin.MP.HttpUtility
             WebClient wc = new WebClient();
             return wc.DownloadString(url);
         }
+
+        /// <summary>
+        /// 请求是否发起自微信客户端的浏览器
+        /// </summary>
+        /// <param name="httpContext"></param>
+        /// <returns></returns>
+        public static bool IsWeixinClientRequest(HttpContext httpContext)
+        {
+            return !string.IsNullOrEmpty(httpContext.Request.UserAgent) &&
+                   httpContext.Request.UserAgent.Contains("MicroMessenger");
+        }
     }
 }

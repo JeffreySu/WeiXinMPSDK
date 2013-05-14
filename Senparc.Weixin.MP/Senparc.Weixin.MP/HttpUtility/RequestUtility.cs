702118ccfd181ef89c444019e54149922ed9ec92
JeffreySu <www.jeffrey.su@gmail.com>
2013-05-14 18:31:21 +0800

    v0.7 添加“通用接口”及文件上传功能（目前非内测用户无法使用）

--- Senparc.Weixin.MP/Senparc.Weixin.MP/HttpUtility/RequestUtility.cs
+++ Senparc.Weixin.MP/Senparc.Weixin.MP/HttpUtility/RequestUtility.cs
@@ -40,8 +40,8 @@ namespace Senparc.Weixin.MP.HttpUtility
 
             if (postStream != null)
             {
-                Stream myRequestStream = request.GetRequestStream();
-                StreamWriter myStreamWriter = new StreamWriter(myRequestStream, Encoding.GetEncoding("gb2312"));
+                Stream requestStream = request.GetRequestStream();
+                StreamWriter myStreamWriter = new StreamWriter(requestStream, Encoding.GetEncoding("gb2312"));
                 myStreamWriter.Write(postStream);
                 myStreamWriter.Close();
             }
@@ -53,11 +53,11 @@ namespace Senparc.Weixin.MP.HttpUtility
                 response.Cookies = cookieContainer.GetCookies(response.ResponseUri);
             }
 
-            Stream myResponseStream = response.GetResponseStream();
-            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+            Stream responseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(responseStream, Encoding.GetEncoding("utf-8"));
             string retString = myStreamReader.ReadToEnd();
             myStreamReader.Close();
-            myResponseStream.Close();
+            responseStream.Close();
 
             return retString;
         }

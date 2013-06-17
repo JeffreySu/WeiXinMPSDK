796ecdd2fc1399d1b5f8ba212a1dd17f3920bc70
JeffreySu <www.jeffrey.su@gmail.com>
2013-06-17 12:08:31 +0800

    为Sample示例添加单元测试

---
+++ Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample.Tests/Controllers/WeixinControllerTest.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Senparc.Weixin.MP.Helpers;
+using Senparc.Weixin.MP.MvcExtension;
+using Senparc.Weixin.MP.Sample.Controllers;
+using Senparc.Weixin.MP.Sample.Tests.Mock;
+
+namespace Senparc.Weixin.MP.Sample.Tests.Controllers
+{
+    [TestClass]
+    public class WeixinControllerTest : BaseTest
+    {
+        WeixinController target;
+        Stream inputStream;
+
+        string xmlTextFormat = @"<xml>
+    <ToUserName><![CDATA[gh_a96a4a619366]]></ToUserName>
+    <FromUserName><![CDATA[olPjZjsXuQPJoV0HlruZkNzKc91E]]></FromUserName>
+    <CreateTime>{0}</CreateTime>
+    <MsgType><![CDATA[text]]></MsgType>
+    <Content><![CDATA[TNT2]]></Content>
+    <MsgId>5832509444155992350</MsgId>
+</xml>
+";
+
+        string xmlLocationFormat = @"<xml>
+  <ToUserName><![CDATA[gh_a96a4a619366]]></ToUserName>
+  <FromUserName><![CDATA[olPjZjsXuQPJoV0HlruZkNzKc91E]]></FromUserName>
+  <CreateTime>{0}</CreateTime>
+  <MsgType><![CDATA[location]]></MsgType>
+  <Location_X>31.285774</Location_X>
+  <Location_Y>120.597610</Location_Y>
+  <Scale>19</Scale>
+  <Label><![CDATA[中国江苏省苏州市沧浪区桐泾南路251号-309号]]></Label>
+  <MsgId>5832828233808572154</MsgId>
+</xml>";
+
+        /// <summary>
+        /// 初始化控制器及相关请求参数
+        /// </summary>
+        /// <param name="xml"></param>
+        private void Init(string xml)
+        {
+            //target = StructureMap.ObjectFactory.GetInstance<WeixinController>();//使用IoC的在这里必须注入，不要直接实例化
+            target = new WeixinController();
+
+            inputStream = new MemoryStream();
+            xml = string.Format(xmlLocationFormat, DateTimeHelper.GetWeixinDateTime(DateTime.Now));
+            var bytes = System.Text.Encoding.UTF8.GetBytes(xml);
+            inputStream.Write(bytes, 0, bytes.Length);
+            inputStream.Flush();
+            inputStream.Seek(0, SeekOrigin.Begin);
+            target.SetFakeControllerContext(inputStream);
+        }
+
+        /// <summary>
+        /// 测试不同类型的请求
+        /// </summary>
+        /// <param name="xml">微信发过来的xml原文</param>
+        private void PostTest(string xml)
+        {
+            Init(xml);//初始化
+
+            var timestamp = "itsafaketimestamp";
+            var nonce = "whateveryouwant";
+            var signature = Senparc.Weixin.MP.CheckSignature.GetSignature(timestamp, nonce, target.Token);
+
+            DateTime st = DateTime.Now;
+            //这里使用MiniPost，绕过日志记录
+            ContentResult actual = target.MiniPost(signature, timestamp, nonce, "echostr") as ContentResult;
+            DateTime et = DateTime.Now;
+
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(typeof(WeixinResult), actual.GetType());
+
+            Console.WriteLine(actual.Content);
+            Console.WriteLine("页面用时（ms）：" + (et - st).TotalMilliseconds);
+        }
+
+        [TestMethod]
+        public void TextPostTest()
+        {
+            PostTest(xmlTextFormat);
+        }
+
+        [TestMethod]
+        public void LocationPostTest()
+        {
+            PostTest(xmlLocationFormat);
+        }
+
+    }
+}

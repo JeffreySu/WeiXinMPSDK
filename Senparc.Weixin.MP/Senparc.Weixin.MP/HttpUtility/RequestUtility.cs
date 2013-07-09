08a7be1015dcb1222ed2376237ae2927eae97201
JeffreySu <www.jeffrey.su@gmail.com>
2013-07-09 21:31:43 +0800

    v1.3 封装System.Web.HttpUtility下HTML及Url的Encode及Decode方法

--- Senparc.Weixin.MP/Senparc.Weixin.MP/HttpUtility/RequestUtility.cs
+++ Senparc.Weixin.MP/Senparc.Weixin.MP/HttpUtility/RequestUtility.cs
@@ -141,5 +141,42 @@ namespace Senparc.Weixin.MP.HttpUtility
 
             return sb.ToString();
         }
+
+        /// <summary>
+        /// 封装System.Web.HttpUtility.HtmlEncode
+        /// </summary>
+        /// <param name="html"></param>
+        /// <returns></returns>
+        public static string HtmlEncode(this string html)
+        {
+            return System.Web.HttpUtility.HtmlEncode(html);
+        }
+        /// <summary>
+        /// 封装System.Web.HttpUtility.HtmlDecode
+        /// </summary>
+        /// <param name="html"></param>
+        /// <returns></returns>
+        public static string HtmlDecode(this string html)
+        {
+            return System.Web.HttpUtility.HtmlDecode(html);
+        }
+        /// <summary>
+        /// 封装System.Web.HttpUtility.UrlEncode
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static string UrlEncode(this string url)
+        {
+            return System.Web.HttpUtility.UrlEncode(url);
+        }
+        /// <summary>
+        /// 封装System.Web.HttpUtility.UrlDecode
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static string UrlDecode(this string url)
+        {
+            return System.Web.HttpUtility.UrlDecode(url);
+        }
     }
 }

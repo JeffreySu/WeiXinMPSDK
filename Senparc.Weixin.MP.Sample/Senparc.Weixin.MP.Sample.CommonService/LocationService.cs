31563c4a8281468eb3cd65ae86bee1436849ce55
JeffreySu <www.jeffrey.su@gmail.com>
2013-07-22 14:57:50 +0800

    修改Webforms 3.5兼容代码。

--- Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample.CommonService/LocationService.cs
+++ Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample.CommonService/LocationService.cs
@@ -22,7 +22,7 @@ namespace Senparc.Weixin.MP.Sample.CommonService
             });
             var mapSize = "480x600";
             var mapUrl = GoogleMapHelper.GetGoogleStaticMap(19 /*requestMessage.Scale*//*微信和GoogleMap的Scale不一致，这里建议使用固定值*/,
-                                                            markersList, size: mapSize);
+                                                            markersList, mapSize);
             responseMessage.Articles.Add(new Article()
             {
                 Description = string.Format("您刚才发送了地理位置信息。Location_X：{0}，Location_Y：{1}，Scale：{2}，标签：{3}",

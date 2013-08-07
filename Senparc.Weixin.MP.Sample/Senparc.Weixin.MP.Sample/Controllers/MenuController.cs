859defb1f6151701fde8ffd996f4b558d1561427
JeffreySu <www.jeffrey.su@gmail.com>
2013-08-07 20:59:44 +0800

    添加自定义菜单编辑器

---
+++ Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/Controllers/MenuController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Senparc.Weixin.MP.Entities;
+using Senparc.Weixin.MP.Entities.Menu;
+
+namespace Senparc.Weixin.MP.Sample.Controllers
+{
+    public class MenuController : Controller
+    {
+        //
+        // GET: /Menu/
+
+        public ActionResult Index()
+        {
+            GetMenuResult reslt = new GetMenuResult();
+
+            //初始化
+            for (int i = 0; i < 3; i++)
+            {
+                var subButton = new SubButton();
+                for (int j = 0; j < 6; j++)
+                {
+                    var singleButton = new SingleButton();
+                    subButton.sub_button.Add(singleButton);
+                }
+            }
+
+            return View(reslt);
+        }
+    }
+}

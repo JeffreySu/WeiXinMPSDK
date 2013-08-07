61acc54edc9c60d6dbc34c6cf107e2bb9c6f867d
JeffreySu <www.jeffrey.su@gmail.com>
2013-08-07 22:32:11 +0800

    完善菜单编辑器

--- Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/Controllers/MenuController.cs
+++ Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/Controllers/MenuController.cs
@@ -21,7 +21,7 @@ namespace Senparc.Weixin.MP.Sample.Controllers
             for (int i = 0; i < 3; i++)
             {
                 var subButton = new SubButton();
-                for (int j = 0; j < 6; j++)
+                for (int j = 0; j < 5; j++)
                 {
                     var singleButton = new SingleButton();
                     subButton.sub_button.Add(singleButton);
@@ -29,6 +29,30 @@ namespace Senparc.Weixin.MP.Sample.Controllers
             }
 
             return View(reslt);
+        }
+
+        public ActionResult GetToken(string appId, string appSecret)
+        {
+            try
+            {
+                var result = CommonAPIs.CommonApi.GetToken(appId, appSecret);
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception)
+            {
+                //TODO:为简化代码，这里不处理异常（如Token过期）
+                return Json(new { error = "执行过程发生错误！" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public ActionResult GetMenu(string token)
+        {
+            var result = CommonAPIs.CommonApi.GetMenu(token);
+            if (result == null)
+            {
+                return Json(new { error = "没有菜单信息！" }, JsonRequestBehavior.AllowGet);
+            }
+            return Json(result, JsonRequestBehavior.AllowGet);
         }
     }
 }
--- Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/Scripts/senparc.menu.js
+++ Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/Scripts/senparc.menu.js
@@ -1,8 +1,40 @@
-﻿var senparc = { };
+﻿var senparc = {};
 senparc.menu = {
+    token: '',
     init: function () {
+        $('#buttonDetails').hide();
+        $('#menuEditor').hide();
+
         $(':input[id^=button_]').click(function () {
-            alert();
+            $('#buttonDetails').show();
+            var keyId = 'menu_button' + $(this).attr('data-j') + '_sub_button' + $(this).attr('data-i') + '_key';
+            var txtDetailsKey = $('#buttonDetails_key');
+            var txtButtonKey = $('#' + keyId);
+            txtDetailsKey.val(txtButtonKey.val());
+            txtDetailsKey.unbind('blur').blur(function () {
+                txtButtonKey.val($(this).val());
+            });
         });
+
+        $('#menuLogin_Submit').click(function () {
+            $.getJSON('/Menu/GetToken', { appId: $('#menuLogin_AppId').val(), appSecret: $('#menuLogin_AppSecret').val() },
+                function (json) {
+                    if (json.access_token) {
+                        senparc.menu.setToken(json.access_token);
+                    } else {
+                        alert(json.error || '执行过程有错误，请检查！');
+                    }
+                });
+        });
+
+        $('#menuLogin_SubmitOldToken').click(function() {
+            senparc.menu.setToken($('#menuLogin_OldToken').val());
+        });
+    },
+    setToken:function (token) {
+        senparc.menu.token = token;
+        $('#tokenStr').html(token);
+        $('#menuEditor').show();
+        $('#menuLogin').hide();
     }
 };

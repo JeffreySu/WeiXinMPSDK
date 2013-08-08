db3caccf8486b0a452134880dc8ac1d94abc761e
JeffreySu <www.jeffrey.su@gmail.com>
2013-08-08 11:59:42 +0800

    优化在线菜单编辑

--- Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/Scripts/senparc.menu.js
+++ Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/Scripts/senparc.menu.js
@@ -1,8 +1,11 @@
 ﻿var senparc = {};
 var maxSubMenuCount = 5;
+var menuState;
 senparc.menu = {
     token: '',
     init: function () {
+        menuState = $('#menuState');
+
         $('#buttonDetails').hide();
         $('#menuEditor').hide();
 
@@ -35,26 +38,33 @@ senparc.menu = {
         });
 
         $('#btnGetMenu').click(function () {
+            menuState.html('获取菜单中');
             $.getJSON('/Menu/GetMenu?t=' + Math.random(), { token: senparc.menu.token }, function (json) {
                 if (json.menu) {
                     var buttons = json.menu.button;
-                    console.log(buttons);
+                    //此处i与j和页面中反转
                     for (var i = 0; i < buttons.length; i++) {
                         var button = buttons[i];
-                        $('#rootButton_' + i).val(button.name);
+                        $('#menu_button' + i + '_name').val(button.name);
+                        $('#menu_button' + i + '_key').val(button.key);
+
                         if (button.sub_button.length > 0) {
                             //二级菜单
-                            for (var j = 0; j < button.sub_button; j++) {
+                            for (var j = 0; j < button.sub_button.length; j++) {
                                 var subButton = button.sub_button[j];
-                                var idPrefix = '#button_' + i + '_' + j;
+                                var idPrefix = '#menu_button' + i + '_sub_button' + j;
                                 $(idPrefix + "_name").val(subButton.name);
                                 $(idPrefix + "_type").val(subButton.type);
                                 $(idPrefix + "_key").val(subButton.key);
                             }
+                        } else {
+                            //底部菜单
+                            //...
                         }
                     }
+                    menuState.html('已完成');
                 } else {
-                    alert(json.error || '执行过程有错误，请检查！');
+                    menuState.html(json.error || '执行过程有错误，请检查！');
                 }
             });
         });

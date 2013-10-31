983771e25f8974ea443dabf0ad7cf4099fb3319b
Jeffrey Su <zsu@senparc.com>
2013-10-31 18:31:19 +0800

    v3.1

--- Senparc.Weixin.MP/Senparc.Weixin.MP/Entities/Request/RequestMessageEvent_Location.cs
+++ Senparc.Weixin.MP/Senparc.Weixin.MP/Entities/Request/RequestMessageEvent_Location.cs
@@ -5,6 +5,11 @@ using System.Text;
 
 namespace Senparc.Weixin.MP.Entities
 {
+    /// <summary>
+    /// 获取用户地理位置（高级接口下才能用）
+    /// 获取用户地理位置的方式有两种，一种是仅在进入会话时上报一次，一种是进入会话后每隔5秒上报一次。公众号可以在公众平台网站中设置。
+    /// 用户同意上报地理位置后，每次进入公众号会话时，都会在进入时上报地理位置，或在进入会话后每5秒上报一次地理位置，上报地理位置以推送XML数据包到开发者填写的URL来实现。
+    /// </summary>
     public class RequestMessageEvent_Location : RequestMessageEventBase, IRequestMessageEventBase
     {
         /// <summary>

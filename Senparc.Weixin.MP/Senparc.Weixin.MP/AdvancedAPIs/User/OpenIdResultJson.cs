05989dd652b6c020db48ab2c2321341d655160e8
Jeffrey Su <zsu@senparc.com>
2013-10-31 18:56:24 +0800

    v3.3

---
+++ Senparc.Weixin.MP/Senparc.Weixin.MP/AdvancedAPIs/User/OpenIdResultJson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Senparc.Weixin.MP.AdvancedAPIs
+{
+   public class OpenIdResultJson
+    {
+       public int total { get; set; }
+       public int count { get; set; }
+       public OpenIdResultJson_Data data { get; set; }
+       public string next_openid { get; set; }
+    }
+
+    public class OpenIdResultJson_Data
+    {
+        public List<string> openid { get; set; }
+    }
+}

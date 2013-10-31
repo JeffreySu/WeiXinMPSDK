6e2b74d0bb5809a76f923902555bf12c3d01691f
Jeffrey Su <zsu@senparc.com>
2013-10-31 15:59:42 +0800

    添加20131029更新的高级接口：发送客服消息

---
+++ Senparc.Weixin.MP/Senparc.Weixin.MP/AdvancedAPIs/Custom/Send.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web.Script.Serialization;
+using Senparc.Weixin.MP.CommonAPIs;
+using Senparc.Weixin.MP.Entities;
+using Senparc.Weixin.MP.HttpUtility;
+
+namespace Senparc.Weixin.MP.AdvancedAPIs.Custom
+{
+    public static class Send
+    {
+        private const string URL_FORMAT = "https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token={0}";
+
+        /// <summary>
+        /// 发送文本信息
+        /// </summary>
+        /// <param name="accessToken"></param>
+        /// <param name="openId"></param>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        public static WxJsonResult SendText(string accessToken, string openId, string content)
+        {
+            var data = new
+            {
+                touser = openId,
+                msgtype = "text",
+                text = new
+                {
+                    content = content
+                }
+            };
+            return CommonJsonSend.Send(accessToken, URL_FORMAT, accessToken);
+        }
+    }
+}
---
+++ Senparc.Weixin.MP/Senparc.Weixin.MP/CommonAPIs/CommonJsonSend.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web.Script.Serialization;
+using Senparc.Weixin.MP.Entities;
+using Senparc.Weixin.MP.HttpUtility;
+
+namespace Senparc.Weixin.MP.CommonAPIs
+{
+   public static class CommonJsonSend
+    {
+       /// <summary>
+       /// 向需要AccessToken的API发送消息的公共方法
+       /// </summary>
+       /// <param name="accessToken"></param>
+       /// <param name="urlFormat"></param>
+       /// <param name="data"></param>
+       /// <returns></returns>
+       public static WxJsonResult Send(string accessToken, string urlFormat,object data)
+       {
+           JavaScriptSerializer js = new JavaScriptSerializer();
+           var jsonString = js.Serialize(data);
+
+           using (MemoryStream ms = new MemoryStream())
+           {
+               var bytes = Encoding.UTF8.GetBytes(jsonString);
+               ms.Write(bytes, 0, bytes.Length);
+               ms.Seek(0, SeekOrigin.Begin);
+
+               var url = string.Format(urlFormat, accessToken);
+               var result = Post.PostGetJson<WxJsonResult>(url, null, ms);
+               return result;
+           }
+       } 
+    }
+}

d8a8244781d2bc3158811a772bc689dd34ad46c4
Jeffrey Su <zsu@senparc.com>
2014-01-27 20:44:47 +0800

    v4.9.0 添加获取用户分组ID的高级接口

--- Senparc.Weixin.MP/Senparc.Weixin.MP/AdvancedAPIs/Groups/Groups.cs
+++ Senparc.Weixin.MP/Senparc.Weixin.MP/AdvancedAPIs/Groups/Groups.cs
@@ -14,19 +14,6 @@ namespace Senparc.Weixin.MP.AdvancedAPIs
 {
     public static class Groups
     {
-        /// <summary>
-        /// 发送文本信息
-        /// </summary>
-        /// <param name="accessToken"></param>
-        /// <param name="openId"></param>
-        /// <param name="content"></param>
-        /// <returns></returns>
-        public static GroupsJson Get(string accessToken)
-        {
-            var urlFormat = "https://api.weixin.qq.com/cgi-bin/groups/get?access_token={0}";
-            var url = string.Format(urlFormat, accessToken);
-            return HttpUtility.Get.GetJson<GroupsJson>(url);
-        }
 
         /// <summary>
         /// 创建分组
@@ -43,6 +30,31 @@ namespace Senparc.Weixin.MP.AdvancedAPIs
                 }
             };
             return CommonJsonSend.Send<CreateGroupResult>(accessToken, urlFormat, data);
+        }
+
+        /// <summary>
+        /// 发送文本信息
+        /// </summary>
+        /// <param name="accessToken"></param>
+        /// <returns></returns>
+        public static GroupsJson Get(string accessToken)
+        {
+            var urlFormat = "https://api.weixin.qq.com/cgi-bin/groups/get?access_token={0}";
+            var url = string.Format(urlFormat, accessToken);
+            return HttpUtility.Get.GetJson<GroupsJson>(url);
+        }
+
+        /// <summary>
+        /// 获取用户分组
+        /// </summary>
+        /// <param name="accessToken"></param>
+        /// <param name="openId"></param>
+        /// <returns></returns>
+        public static GetGroupIdResult GetId(string accessToken, string openId)
+        {
+            var urlFormat = "https://api.weixin.qq.com/cgi-bin/groups/getid?access_token={0}";
+            var data = new { openid = openId };
+            return CommonJsonSend.Send<GetGroupIdResult>(accessToken, urlFormat, data);
         }
 
         /// <summary>
@@ -73,13 +85,13 @@ namespace Senparc.Weixin.MP.AdvancedAPIs
         /// <param name="openId"></param>
         /// <param name="toGroupId"></param>
         /// <returns></returns>
-        public static WxJsonResult MemberUpdate(string accessToken, string openId,int toGroupId)
+        public static WxJsonResult MemberUpdate(string accessToken, string openId, int toGroupId)
         {
             var urlFormat = "https://api.weixin.qq.com/cgi-bin/groups/members/update?access_token={0}";
             var data = new
             {
-                openid=openId,
-                to_groupid =toGroupId
+                openid = openId,
+                to_groupid = toGroupId
             };
             return CommonJsonSend.Send(accessToken, urlFormat, data);
         }

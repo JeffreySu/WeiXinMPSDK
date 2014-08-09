83d6312043d179c2f671091302d840dac903020a
TYSZCC <czhang@senparc.com>
2014-08-09 18:16:20 +0800

    微信支付Demo修改

--- Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/wx/pay/jsapi.aspx.cs
+++ Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/wx/pay/jsapi.aspx.cs
@@ -70,6 +70,12 @@ public partial class _Default : System.Web.UI.Page
         paySignReqHandler.SetParameter("noncestr", nonceStr);
         paySignReqHandler.SetParameter("timestamp", timeStamp);
         paySignReqHandler.SetParameter("package", packageValue);
+        //paySignReqHandler.SetParameter("appid", "wxd930ea5d5a258f4f");
+        //paySignReqHandler.SetParameter("appkey", "L8LrMqqeGRxST5reouB0K66CaYAWpqhAVsq7ggKkxHCOastWksvuX1uvmvQclxaHoYd3ElNBrNO2DHnnzgfVG9Qs473M3DTOZug5er46FhuGofumV8H2FVR9qkjSlC5K");
+        //paySignReqHandler.SetParameter("noncestr", "e7d161ac8d8a76529d39d9f5b4249ccb");
+        //paySignReqHandler.SetParameter("timestamp", "1399514976");
+        //paySignReqHandler.SetParameter("traceid", "test_1399514976");
+        //paySignReqHandler.SetParameter("package", "bank_type=WX&body=%E6%94%AF%E4%BB%98%E6%B5%8B%E8%AF%95&fee_type=1&input_charset=UTF-8&notify_url=http%3A%2F%2Fweixin.qq.com&out_trade_no=7240b65810859cbf2a8d9f76a638c0a3&partner=1900000109&spbill_create_ip=196.168.1.1&total_fee=1&sign=7F77B507B755B3262884291517E380F8");
         paySign = paySignReqHandler.CreateSHA1Sign();
 
 

aff7cd7f8bca62ad057cd80b878075bf5bd76cca
Jeffrey Su <zsu@senparc.com>
2014-08-19 18:19:34 +0800

    v7.0.14 添加AccessTokenHandlerWapper

---
+++ Senparc.Weixin.MP/Senparc.Weixin.MP/CommonAPIs/AccessTokenHandlerWapper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Senparc.Weixin.MP.Entities;
+
+namespace Senparc.Weixin.MP.CommonAPIs
+{
+    public static class AccessTokenHandlerWapper
+    {
+        /// <summary>
+        /// 使用AccessToken进行操作时，如果遇到AccessToken错误的情况，重新获取AccessToken一次，并重试
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="appId"></param>
+        /// <param name="appSecret"></param>
+        /// <param name="fun">第一个参数为accessToken</param>
+        /// <param name="retryIfFaild"></param>
+        /// <returns></returns>
+        public static T Do<T>(string appId, string appSecret, Func<string, T> fun, bool retryIfFaild = true) where T : WxJsonResult
+        {
+            T result = null;
+            try
+            {
+                var accessToken = AccessTokenContainer.TryGetToken(appId, appSecret, false);
+                result = fun(accessToken);
+            }
+            catch (ErrorJsonResultException ex)
+            {
+                if (retryIfFaild && ex.JsonResult.errcode == ReturnCode.验证失败)
+                {
+                    //尝试重新验证
+                    var accessToken = AccessTokenContainer.TryGetToken(appId, appSecret, true);
+                    result = Do<T>(appId, appSecret, fun, false);
+                }
+            }
+            return result;
+        }
+    }
+}

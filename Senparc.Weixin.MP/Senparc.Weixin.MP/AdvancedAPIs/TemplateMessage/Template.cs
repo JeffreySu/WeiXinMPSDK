c310f2ab8bbc938e7787fd309efd7500847d2eec
Jeffrey Su <zsu@senparc.com>
2014-09-02 10:37:15 +0800

    v7.1.1 完善微信支付、微小店接口；完善模板消息响应事件。

--- Senparc.Weixin.MP/Senparc.Weixin.MP/AdvancedAPIs/TemplateMessage/Template.cs
+++ Senparc.Weixin.MP/Senparc.Weixin.MP/AdvancedAPIs/TemplateMessage/Template.cs
@@ -12,7 +12,7 @@ namespace Senparc.Weixin.MP.AdvancedAPIs
     /// </summary>
     public static class Template
     {
-        public static WxJsonResult SendTemplateMessage<T>(string accessToken, string openId, string templateId, string topcolor, T data)
+        public static SendTemplateMessageResult SendTemplateMessage<T>(string accessToken, string openId, string templateId, string topcolor, T data)
         {
             const string urlFormat = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={0}";
             var msgData = new TempleteModel()
@@ -22,7 +22,7 @@ namespace Senparc.Weixin.MP.AdvancedAPIs
                 touser = openId,
                 data = data
             };
-            return CommonJsonSend.Send<WxJsonResult>(accessToken, urlFormat, msgData);
+            return CommonJsonSend.Send<SendTemplateMessageResult>(accessToken, urlFormat, msgData);
         }
     }
 }

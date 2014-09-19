ec03076fbd64879c83826c60dd3c0eb57b9083d9
TYSZCC <czhang@senparc.com>
2014-09-19 13:48:51 +0800

    新增获取QY微信账号accessToken；完成管理部门部分

---
+++ Senparc.Weixin.MP/Senparc.Weixin.MP/QYAPIs/MailList/Department/DepartmentResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Senparc.Weixin.MP.Entities;
+
+namespace Senparc.Weixin.MP.QYPIs
+{
+    public class CreateDepartmentResult : WxJsonResult
+    {
+        public int id { get; set; }
+    }
+
+    public class GetDepartmentListResult : WxJsonResult
+    {
+        public List<DepartmentList> department { get; set; }
+    }
+
+    public class DepartmentList
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int parentid { get; set; }
+    }
+}

c68dbb5d9d7dbcc0859d3ed8f9ca6a0041260742
Jeffrey Su <zsu@senparc.com>
2014-10-07 15:19:14 +0800

    企业号添加AccssTokenContainer辅助功能

---
+++ Senparc.Weixin.QY/Senparc.Weixin.QY/CommonAPIs/AccessTokenContainer.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Senparc.Weixin.Exceptions;
+using Senparc.Weixin.QY.Entities;
+
+namespace Senparc.Weixin.QY.CommonAPIs
+{
+    class AccessTokenBag
+    {
+        public string CorpId { get; set; }
+        public string CorpSecret { get; set; }
+        public DateTime ExpireTime { get; set; }
+        public AccessTokenResult AccessTokenResult { get; set; }
+    }
+
+    /// <summary>
+    /// 通用接口AccessToken容器，用于自动管理AccessToken，如果过期会重新获取
+    /// </summary>
+    public class AccessTokenContainer
+    {
+        static Dictionary<string, AccessTokenBag> AccessTokenCollection =
+           new Dictionary<string, AccessTokenBag>(StringComparer.OrdinalIgnoreCase);
+
+        /// <summary>
+        /// 注册应用凭证信息，此操作只是注册，不会马上获取Token，并将清空之前的Token，
+        /// </summary>
+        /// <param name="corpId"></param>
+        /// <param name="corpSecret"></param>
+        public static void Register(string corpId, string corpSecret)
+        {
+            AccessTokenCollection[corpId] = new AccessTokenBag()
+            {
+                CorpId = corpId,
+                CorpSecret = corpSecret,
+                ExpireTime = DateTime.MinValue,
+                AccessTokenResult = new AccessTokenResult()
+            };
+        }
+
+        /// <summary>
+        /// 使用完整的应用凭证获取Token，如果不存在将自动注册
+        /// </summary>
+        /// <param name="corpId"></param>
+        /// <param name="corpSecret"></param>
+        /// <param name="getNewToken"></param>
+        /// <returns></returns>
+        public static string TryGetToken(string corpId, string corpSecret, bool getNewToken = false)
+        {
+            if (!CheckRegistered(corpId) || getNewToken)
+            {
+                Register(corpId, corpSecret);
+            }
+            return GetToken(corpId);
+        }
+
+        /// <summary>
+        /// 获取可用Token
+        /// </summary>
+        /// <param name="corpId"></param>
+        /// <param name="getNewToken">是否强制重新获取新的Token</param>
+        /// <returns></returns>
+        public static string GetToken(string corpId, bool getNewToken = false)
+        {
+            return GetTokenResult(corpId, getNewToken).access_token;
+        }
+
+        /// <summary>
+        /// 获取可用Token
+        /// </summary>
+        /// <param name="corpId"></param>
+        /// <param name="getNewToken">是否强制重新获取新的Token</param>
+        /// <returns></returns>
+        public static AccessTokenResult GetTokenResult(string corpId, bool getNewToken = false)
+        {
+            if (!AccessTokenCollection.ContainsKey(corpId))
+            {
+                throw new WeixinException("此CorpId尚未注册，请先使用AccessTokenContainer.Register完成注册（全局执行一次即可）！");
+            }
+
+            var accessTokenBag = AccessTokenCollection[corpId];
+            if (getNewToken || accessTokenBag.ExpireTime <= DateTime.Now)
+            {
+                //已过期，重新获取
+                accessTokenBag.AccessTokenResult = CommonApi.GetToken(accessTokenBag.CorpId, accessTokenBag.CorpSecret);
+                accessTokenBag.ExpireTime = DateTime.Now.AddSeconds(7200);
+            }
+            return accessTokenBag.AccessTokenResult;
+        }
+
+        /// <summary>
+        /// 检查是否已经注册
+        /// </summary>
+        /// <param name="corpId"></param>
+        /// <returns></returns>
+        public static bool CheckRegistered(string corpId)
+        {
+            return AccessTokenCollection.ContainsKey(corpId);
+        }
+    }
+}

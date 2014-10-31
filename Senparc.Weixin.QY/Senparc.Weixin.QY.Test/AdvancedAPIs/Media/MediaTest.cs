cf38b0feb882f23e6bf04673b38e1f6d003c6368
TYSZCC <czhang@senparc.com>
2014-10-31 14:57:05 +0800

    增加媒体上传下载单元测试

---
+++ Senparc.Weixin.QY/Senparc.Weixin.QY.Test/AdvancedAPIs/Media/MediaTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Senparc.Weixin.Exceptions;
+using Senparc.Weixin.QY.AdvancedAPIs;
+using Senparc.Weixin.QY.CommonAPIs;
+
+namespace Senparc.Weixin.QY.Test.AdvancedAPIs
+{
+    /// <summary>
+    /// CommonApiTest 的摘要说明
+    /// </summary>
+    [TestClass]
+    public partial class MediaTest
+    {
+        protected string _corpId = "wxccd01c4e6bf59232"; //换成你的信息
+        protected string _corpSecret = "ejXcV7rb9OtakBucpMji1kUtPmnKy4hNCskW_bUKLx8lRxO_aVrcc0gVTMEv13Gt"; //换成你的信息
+
+        public MediaTest()
+        {
+            //全局只需注册一次
+            AccessTokenContainer.Register(_corpId, _corpSecret);
+        }
+
+        [TestMethod]
+        public void UploadVideoTest()
+        {
+            string _media = "E:\\test2.mp4";
+            var accessToken = AccessTokenContainer.GetToken(_corpId);
+            var result = Media.Upload(accessToken, UploadMediaFileType.video, _media);
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.errcode == ReturnCode.请求成功);
+        }
+
+        //[TestMethod]
+        public string UploadImageTest()
+        {
+            string _media = "E:\\1.jpg";
+            var accessToken = AccessTokenContainer.GetToken(_corpId);
+            var result = Media.Upload(accessToken, UploadMediaFileType.image, _media);
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.errcode == ReturnCode.请求成功);
+            return result.media_id;
+        }
+
+        [TestMethod]
+        public void GetImageTest()
+        {
+            string mediaId = UploadImageTest();
+            var accessToken = AccessTokenContainer.GetToken(_corpId);
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                Media.Get(accessToken, mediaId, ms);
+                Assert.IsTrue(ms.Length > 0);
+
+                //保存到文件
+                var fileName = string.Format(@"E:\testpic_{0}.jpg", DateTime.Now.Ticks);
+                using (FileStream fs = new FileStream(fileName, FileMode.Create))
+                {
+                    ms.Position = 0;
+                    byte[] buffer = new byte[1024];
+                    int bytesRead = 0;
+                    while ((bytesRead = ms.Read(buffer, 0, buffer.Length)) != 0)
+                    {
+                        fs.Write(buffer, 0, bytesRead);
+                    }
+                    fs.Flush();
+                }
+
+                Assert.IsTrue(File.Exists(fileName));
+            }
+        }
+    }
+}

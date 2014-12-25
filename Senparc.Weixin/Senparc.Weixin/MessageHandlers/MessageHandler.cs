7c0c6cf256c3c028ffa53212ea81edba02b157c8
Jeffrey Su <zsu@senparc.com>
2014-12-25 13:15:05 +0800

    Senparc.Weixin.dll v1.6.1 完善去重功能

--- Senparc.Weixin/Senparc.Weixin/MessageHandlers/MessageHandler.cs
+++ Senparc.Weixin/Senparc.Weixin/MessageHandlers/MessageHandler.cs
@@ -164,7 +164,10 @@ namespace Senparc.Weixin.MessageHandlers
             if (OmitRepeatedMessage && CurrentMessageContext.RequestMessages.Count > 1)
             {
                 var lastMessage = CurrentMessageContext.RequestMessages[CurrentMessageContext.RequestMessages.Count - 2];
-                if (lastMessage.MsgId != 0 && lastMessage.MsgId == RequestMessage.MsgId)
+                if ((lastMessage.MsgId != 0 && lastMessage.MsgId == RequestMessage.MsgId)//使用MsgId去重
+                    || 
+                    (lastMessage.CreateTime == RequestMessage.CreateTime)//使用CreateTime去重（OpenId对象已经是同一个）
+                    )
                 {
                     CancelExcute = true;//重复消息，取消执行
                 }

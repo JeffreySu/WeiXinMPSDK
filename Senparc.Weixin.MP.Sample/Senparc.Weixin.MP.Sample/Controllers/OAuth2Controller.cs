dec11df248469949fb8da91179b6820f2398c887
TYSZCC <czhang@senparc.com>
2015-03-04 12:09:01 +0800

    修改oauth demo

--- Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/Controllers/OAuth2Controller.cs
+++ Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/Controllers/OAuth2Controller.cs
@@ -36,13 +36,22 @@ namespace Senparc.Weixin.MP.Sample.Controllers
                 return Content("验证失败！请从正规途径进入！");
             }
 
+            OAuthAccessTokenResult result = null;
+
             //通过，用code换取access_token
-            var result = OAuthApi.GetAccessToken(appId, secret, code);
+            try
+            {
+                result = OAuthApi.GetAccessToken(appId, secret, code);
+
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.Message);
+            }
             if (result.errcode != ReturnCode.请求成功)
             {
                 return Content("错误：" + result.errmsg);
             }
-
             //下面2个数据也可以自己封装成一个类，储存在数据库中（建议结合缓存）
             //如果可以确保安全，可以将access_token存入用户的cookie中，每一个人的access_token是不一样的
             Session["OAuthAccessTokenStartTime"] = DateTime.Now;
@@ -51,7 +60,7 @@ namespace Senparc.Weixin.MP.Sample.Controllers
             //因为第一步选择的是OAuthScope.snsapi_userinfo，这里可以进一步获取用户详细信息
             try
             {
-                var userInfo = OAuthApi.GetUserInfo(result.access_token, result.openid);
+                OAuthUserInfo userInfo = OAuthApi.GetUserInfo(result.access_token, result.openid);
                 return View(userInfo);
             }
             catch (ErrorJsonResultException ex)

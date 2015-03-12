9b7af228fd19f4b6878d496632d7d7516d403a78
Jeffrey Su <zsu@senparc.com>
2015-03-12 14:42:35 +0800

    优化异步Demo

---
+++ Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample.Tests/Controllers/WeixinAsyncControllerTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Senparc.Weixin.MP.Sample.Tests.Controllers
+{
+    class WeixinAsyncControllerTest
+    {
+    }
+}

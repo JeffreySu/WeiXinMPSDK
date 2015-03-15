25b0dc83dff088030000bdf071dc85d99ef4099f
Jeffrey Su <zsu@senparc.com>
2015-03-16 02:29:55 +0800

    更新Demo

--- Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/Controllers/BaseController.cs
+++ Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/Controllers/BaseController.cs
@@ -25,9 +25,9 @@ namespace Senparc.Weixin.MP.Sample.Controllers
             var mpFileVersionInfo = FileVersionInfo.GetVersionInfo(Server.MapPath("~/bin/Senparc.Weixin.MP.dll"));
             var extensionFileVersionInfo = FileVersionInfo.GetVersionInfo(Server.MapPath("~/bin/Senparc.Weixin.MP.MvcExtension.dll"));
 
-            TempData["WeixinVersion"] = string.Format("{0}.{1}", weixinFileVersionInfo.FileMajorPart, weixinFileVersionInfo.FileMinorPart); //Regex.Match(fileVersionInfo.FileVersion, @"\d+\.\d+");
-            TempData["MpVersion"] = string.Format("{0}.{1}", mpFileVersionInfo.FileMajorPart, mpFileVersionInfo.FileMinorPart); //Regex.Match(fileVersionInfo.FileVersion, @"\d+\.\d+");
-            TempData["ExtensionVersion"] = string.Format("{0}.{1}", extensionFileVersionInfo.FileMajorPart, extensionFileVersionInfo.FileMinorPart); //Regex.Match(fileVersionInfo.FileVersion, @"\d+\.\d+");
+            TempData["WeixinVersion"] = string.Format("{0}.{1}", weixinFileVersionInfo.FileMajorPart, weixinFileVersionInfo.FileMinorPart); //Regex.Match(weixinFileVersionInfo.FileVersion, @"\d+\.\d+");
+            TempData["MpVersion"] = string.Format("{0}.{1}", mpFileVersionInfo.FileMajorPart, mpFileVersionInfo.FileMinorPart); //Regex.Match(mpFileVersionInfo.FileVersion, @"\d+\.\d+");
+            TempData["ExtensionVersion"] = string.Format("{0}.{1}", extensionFileVersionInfo.FileMajorPart, extensionFileVersionInfo.FileMinorPart); //Regex.Match(extensionFileVersionInfo.FileVersion, @"\d+\.\d+");
 
             base.OnResultExecuting(filterContext);
         }

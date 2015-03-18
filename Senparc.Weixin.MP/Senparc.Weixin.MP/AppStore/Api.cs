dfe65f620d4a21adb6095ad12cd242635239bb56
Jeffrey Su <zsu@senparc.com>
2015-03-18 23:01:24 +0800

    .MP  v11.2.1 完善AppStore Api相关

--- Senparc.Weixin.MP/Senparc.Weixin.MP/AppStore/Api.cs
+++ Senparc.Weixin.MP/Senparc.Weixin.MP/AppStore/Api.cs
@@ -8,13 +8,13 @@ using Senparc.Weixin.MP.MessageHandlers;
 
 namespace Senparc.Weixin.MP.AppStore
 {
-    public static class Api
-    {
-        public static IResponseMessageBase Request(IMessageHandler messageHandler, DeveloperInfo developerInfo, string xml)
-        {
-            var responseMessage = MessageAgent.RequestWeiweihiResponseMessage(messageHandler, developerInfo.WeiweihiKey, messageHandler.RequestMessage as RequestMessageBase);
+    //public static class Api
+    //{
+    //    public static IResponseMessageBase Request(IMessageHandler messageHandler, DeveloperInfo developerInfo, string xml)
+    //    {
+    //        var responseMessage = MessageAgent.RequestWeiweihiResponseMessage(messageHandler, developerInfo.WeiweihiKey, messageHandler.RequestMessage as RequestMessageBase);
 
-            return responseMessage;
-        }
-    }
+    //        return responseMessage;
+    //    }
+    //}
 }

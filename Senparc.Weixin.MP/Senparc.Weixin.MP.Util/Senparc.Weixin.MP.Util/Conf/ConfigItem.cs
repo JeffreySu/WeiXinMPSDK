907df8f792f06df6cd5cc8638c06e477436ad862
sonven <new.min@msn.com>
2015-03-22 05:26:46 +0800

    commit

---
+++ Senparc.Weixin.MP/Senparc.Weixin.MP.Util/Senparc.Weixin.MP.Util/Conf/ConfigItem.cs
@@ -0,0 +1,75 @@
+﻿
+using System;
+using AtNet.DevFw.Framework;
+
+namespace Senparc.Weixin.MP.Util.Conf
+{
+    public class ConfigItem
+    {
+        public static string Token;
+
+        public static string AppId;
+
+        public static string AppSecret;
+
+        /// <summary>
+        /// 解密字符串
+        /// </summary>
+        public static string AppEncodeString;
+        public static string ApiDomain;         //
+        public static string MenuButtons;       //自定义菜单
+        public static string WxWelcomeMessage;    //微信欢迎文字
+        public static string WxEnterMessage; //微信进入文字
+        public static string WxDefaultResponseMessage;  //微信默认自动回复消息
+        public static bool DebugMode = false;
+        private string _filePath;
+        private string _key;
+        private SettingFile _file;
+
+        public ConfigItem(string filePath)
+        {
+            this._filePath = filePath;
+            this._file = new SettingFile(this._filePath);
+        }
+
+        #region 按钮模板
+
+        /*
+         {
+     "button":[
+     {    
+          "type":"click",
+          "name":"今日歌曲",
+          "key":"V1001_TODAY_MUSIC"
+      },
+      {
+           "name":"菜单",
+           "sub_button":[
+           {    
+               "type":"view",
+               "name":"搜索",
+               "url":"http://www.soso.com/"
+            }
+           ]
+       }]
+ }
+         */
+
+        #endregion
+
+        /// <summary>
+        /// Get Weixin Key
+        /// </summary>
+        /// <returns></returns>
+        public string GetKey()
+        {
+            if (this._key == null)
+            {
+                this._key = _filePath.Substring(
+                    _filePath.LastIndexOfAny(new char[] {'\\', '/'}) + 1,
+                    _filePath.LastIndexOf(".", StringComparison.Ordinal));
+            }
+            return this._key;
+        }
+    }
+}
---
+++ Senparc.Weixin.MP/Senparc.Weixin.MP.Util/Senparc.Weixin.MP.Util/Conf/ConfigManager.cs
@@ -0,0 +1,65 @@
+﻿
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Senparc.Weixin.MP.Util.Conf
+{
+    public static class ConfigManager
+    {
+        private static IDictionary<String, ConfigItem> _items;
+        /// <summary>
+        /// Get config of micro message by config key.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static ConfigItem GetConfig(String key)
+        {
+            if (_items == null || _items.Count == 0)
+            {
+                throw new ArgumentNullException("not contain any config.");
+            }
+
+            if (_items.ContainsKey(key))
+            {
+                return _items[key];
+            }
+
+            throw new ArgumentException("not contain config named \"" + key + "\"");
+        }
+
+
+        public static void Load(String directoryPath)
+        {
+            DirectoryInfo dir = new DirectoryInfo(directoryPath);
+            if (dir.Exists)
+            {
+                throw new IOException(directoryPath + " not exist!");
+            }
+
+            ResetItems();
+
+            FileInfo[] confFiles = dir.GetFiles("*.config");
+
+            foreach (FileInfo file in confFiles)
+            {
+                LoadFormFile(file.FullName);
+            }
+
+        }
+
+        private static void ResetItems()
+        {
+            _items = new ConcurrentDictionary<string, ConfigItem>();
+        }
+
+        private static void LoadFormFile(String filePath)
+        {
+            ConfigItem item = new ConfigItem(filePath);
+            _items.Add(item.GetKey(), item);
+        }
+
+    }
+}
+

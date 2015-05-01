7503ea3639b89f0bdd8ba3d0a3a22e206a89a0c9
Jeffrey Su <zsu@senparc.com>
2015-05-01 16:30:32 +0800

    Senparc.Weixin.MvcExtension.dll v3.1.1 fix bug

--- Senparc.Weixin.MP.MvcExtension/Senparc.Weixin.MP.MvcExtension/Results/FixWeixinBugWeixinResult.cs
+++ Senparc.Weixin.MP.MvcExtension/Senparc.Weixin.MP.MvcExtension/Results/FixWeixinBugWeixinResult.cs
@@ -40,13 +40,18 @@ namespace Senparc.Weixin.MP.MvcExtension
                 }
                 else if (_messageHandlerDocument.TextResponseMessage != null)
                 {
-                    return _messageHandlerDocument.TextResponseMessage;
-                }
-                else if (_messageHandlerDocument != null && _messageHandlerDocument.FinalResponseDocument != null)
-                {
-                    return _messageHandlerDocument.FinalResponseDocument.ToString(SaveOptions.OmitDuplicateNamespaces).Replace("\r\n", "\n");
-                }
+                    if (_messageHandlerDocument != null && _messageHandlerDocument.FinalResponseDocument!=null)
+                    {
+                        //返回XML响应信息
+                        return _messageHandlerDocument.TextResponseMessage.Replace("\r\n", "\n");
+                    }
+                    else
+                    {
+                        //返回XML响应信息或用户指定的文本内容
+                        return _messageHandlerDocument.TextResponseMessage;
+                    }
 
+                }
                 else
                 {
                     return null;

661bd15530c672a8d01360809e614eab71269bc9
Jeffrey Su <zsu@senparc.com>
2015-05-01 12:20:19 +0800

    v0.1.2，完善ThirdPartyMessageHandler

--- Senparc.Weixin.Open/Senparc.Weixin.Open/ThirdPartyMessageHandlers/ThirdPartyMessageHandler.cs
+++ Senparc.Weixin.Open/Senparc.Weixin.Open/ThirdPartyMessageHandlers/ThirdPartyMessageHandler.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Xml.Linq;
+using Senparc.Weixin.Context;
+using Senparc.Weixin.Exceptions;
 using Senparc.Weixin.Open.Helpers;
 
 namespace Senparc.Weixin.Open.MessageHandlers
@@ -11,15 +13,17 @@ namespace Senparc.Weixin.Open.MessageHandlers
     public class ThirdPartyMessageHandler
     {
         public XDocument RequestDocument { get; set; }
-        public object RequestMessage { get; set; }
+        public RequestMessageBase RequestMessage { get; set; }
 
         public string ResponseMessageText { get; set; }
+
+        public bool CancelExcute { get; set; }
 
         public ThirdPartyMessageHandler(Stream inputStream)
         {
             RequestDocument = XmlUtility.XmlUtility.Convert(inputStream);//转成XDocument
 
-//转成实体
+            //转成实体
             RequestMessageBase requestMessage = null;
             InfoType infoType;
             try
@@ -39,7 +43,7 @@ namespace Senparc.Weixin.Open.MessageHandlers
             }
             catch (Exception ex)
             {
-                
+
                 throw;
             }
 
@@ -51,9 +55,82 @@ namespace Senparc.Weixin.Open.MessageHandlers
 
         public void Excute()
         {
+            if (CancelExcute)
+            {
+                return;
+            }
 
+            OnExecuting();
+
+            if (CancelExcute)
+            {
+                return;
+            }
+
+            try
+            {
+                if (RequestMessage == null)
+                {
+                    return;
+                }
+
+                switch (RequestMessage.InfoType)
+                {
+                    case InfoType.component_verify_ticket:
+                        {
+                            var requestMessage = RequestMessage as RequestMessageComponentVerifyTicket;
+                            ResponseMessageText = OnComponentVerifyTicketRequest(requestMessage);
+                        }
+                        break;
+                    case InfoType.unauthorized:
+                        {
+                            var requestMessage = RequestMessage as RequestMessageUnauthorized;
+                            ResponseMessageText = OnUnauthorizedRequest(requestMessage);
+                        }
+                        break;
+                    default:
+                        throw new UnknownRequestMsgTypeException("未知的InfoType请求类型", null);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw new MessageHandlerException("ThirdPartyMessageHandler中Execute()过程发生错误：" + ex.Message, ex);
+            }
+            finally
+            {
+                OnExecuted();
+            }
+        }
+
+        public virtual void OnExecuting()
+        {
+        }
+
+        public virtual void OnExecuted()
+        {
         }
 
 
+
+        /// <summary>
+        /// 推送component_verify_ticket协议
+        /// </summary>
+        /// <param name="requestMessage"></param>
+        /// <returns></returns>
+        public virtual string OnComponentVerifyTicketRequest(RequestMessageComponentVerifyTicket requestMessage)
+        {
+            return "success";
+        }
+
+        /// <summary>
+        /// 推送取消授权通知
+        /// </summary>
+        /// <param name="requestMessage"></param>
+        /// <returns></returns>
+        public virtual string OnUnauthorizedRequest(RequestMessageUnauthorized requestMessage)
+        {
+            return "success";
+        }
     }
 }

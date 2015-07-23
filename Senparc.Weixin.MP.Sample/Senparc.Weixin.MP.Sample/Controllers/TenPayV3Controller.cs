99b1780bc7bdb2e559c6df58cc008724c0616818
TYSZCC <czhang@senparc.com>
2015-07-23 12:23:21 +0800

    添加裂变红包demo  查询红包结果demo

--- Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/Controllers/TenPayV3Controller.cs
+++ Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/Controllers/TenPayV3Controller.cs
@@ -583,6 +583,120 @@ namespace Senparc.Weixin.MP.Sample.Controllers
         }
         #endregion
 
+        #region 裂变红包
+
+        /// <summary>
+        /// 目前支持向指定微信用户的openid发放指定金额红包
+        /// 注意total_amount、min_value、max_value值相同
+        /// total_num=1固定
+        /// 单个红包金额介于[1.00元，200.00元]之间
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult SendGroupRedPack()
+        {
+            string mchbillno = DateTime.Now.ToString("HHmmss") + TenPayV3Util.BuildRandomStr(28);
+
+            string nonceStr = TenPayV3Util.GetNoncestr();
+            RequestHandler packageReqHandler = new RequestHandler(null);
+
+            packageReqHandler.SetParameter("nonce_str", nonceStr);              //随机字符串
+            packageReqHandler.SetParameter("wxappid", TenPayV3Info.AppId);		  //公众账号ID
+            packageReqHandler.SetParameter("mch_id", TenPayV3Info.MchId);		  //商户号
+            packageReqHandler.SetParameter("mch_billno", mchbillno);                 //填入商家订单号
+            packageReqHandler.SetParameter("send_name", "商户名称");                 //红包发送者名称
+            packageReqHandler.SetParameter("re_openid", "接受收红包的用户的openId");                 //接受收红包的用户的openId
+            packageReqHandler.SetParameter("total_amount", "300");                //付款金额，单位分
+            packageReqHandler.SetParameter("total_num", "3");               //红包发放总人数  必须介于(包括)3到20之间
+            packageReqHandler.SetParameter("wishing", "红包祝福语");               //红包祝福语
+            packageReqHandler.SetParameter("amt_type", "ALL_RAND");               //红包金额设置方式ALL_RAND—全部随机,商户指定总金额和红包发放总人数，由微信支付随机计算出各红包金额
+            //packageReqHandler.SetParameter("amt_list", "各红包具体金额");               //各红包具体金额，自定义金额时必须设置，单位分  示例值"200|100|100"
+            packageReqHandler.SetParameter("act_name", "活动名称");   //活动名称
+            packageReqHandler.SetParameter("remark", "备注信息");   //备注信息
+            string sign = packageReqHandler.CreateMd5Sign("key", TenPayV3Info.Key);
+            packageReqHandler.SetParameter("sign", sign);	                    //签名
+            //发红包需要post的数据
+            string data = packageReqHandler.ParseXML();
+
+            //发红包接口地址
+            string url = "https://api.mch.weixin.qq.com/mmpaymkttransfers/sendgroupredpack";
+            //本地或者服务器的证书位置（证书在微信支付申请成功发来的通知邮件中）
+            string cert = @"F:\apiclient_cert.p12";
+            //私钥（在安装证书时设置）
+            string password = "";
+            ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);
+            //调用证书
+            X509Certificate2 cer = new X509Certificate2(cert, password, X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.MachineKeySet);
+
+            #region 发起post请求
+            HttpWebRequest webrequest = (HttpWebRequest)HttpWebRequest.Create(url);
+            webrequest.ClientCertificates.Add(cer);
+            webrequest.Method = "post";
+
+            byte[] postdatabyte = Encoding.UTF8.GetBytes(data);
+            webrequest.ContentLength = postdatabyte.Length;
+            Stream stream;
+            stream = webrequest.GetRequestStream();
+            stream.Write(postdatabyte, 0, postdatabyte.Length);
+            stream.Close();
+
+            HttpWebResponse httpWebResponse = (HttpWebResponse)webrequest.GetResponse();
+            StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream());
+            string responseContent = streamReader.ReadToEnd();
+            #endregion
+
+            return Content(responseContent);
+        }
+        #endregion
+
+        #region 红包查询接口
+
+        public ActionResult GetHBInfo(string mchbillno)
+        {
+            string nonceStr = TenPayV3Util.GetNoncestr();
+            RequestHandler packageReqHandler = new RequestHandler(null);
+
+            packageReqHandler.SetParameter("nonce_str", nonceStr);              //随机字符串
+            packageReqHandler.SetParameter("appid", TenPayV3Info.AppId);		  //公众账号ID
+            packageReqHandler.SetParameter("mch_id", TenPayV3Info.MchId);		  //商户号
+            packageReqHandler.SetParameter("mch_billno", mchbillno);                 //填入商家订单号
+            packageReqHandler.SetParameter("bill_type", "MCHT");                 //红包发送者名称
+            string sign = packageReqHandler.CreateMd5Sign("key", TenPayV3Info.Key);
+            packageReqHandler.SetParameter("sign", sign);	                    //签名
+            //发红包需要post的数据
+            string data = packageReqHandler.ParseXML();
+
+            //发红包接口地址
+            string url = "https://api.mch.weixin.qq.com/mmpaymkttransfers/gethbinfo";
+            //本地或者服务器的证书位置（证书在微信支付申请成功发来的通知邮件中）
+            string cert = @"F:\apiclient_cert.p12";
+            //私钥（在安装证书时设置）
+            string password = "";
+            ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);
+            //调用证书
+            X509Certificate2 cer = new X509Certificate2(cert, password, X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.MachineKeySet);
+
+            #region 发起post请求
+            HttpWebRequest webrequest = (HttpWebRequest)HttpWebRequest.Create(url);
+            webrequest.ClientCertificates.Add(cer);
+            webrequest.Method = "post";
+
+            byte[] postdatabyte = Encoding.UTF8.GetBytes(data);
+            webrequest.ContentLength = postdatabyte.Length;
+            Stream stream;
+            stream = webrequest.GetRequestStream();
+            stream.Write(postdatabyte, 0, postdatabyte.Length);
+            stream.Close();
+
+            HttpWebResponse httpWebResponse = (HttpWebResponse)webrequest.GetResponse();
+            StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream());
+            string responseContent = streamReader.ReadToEnd();
+            #endregion
+
+            return Content(responseContent);
+        }
+
+        #endregion
+
         #region 产品展示
 
         public ActionResult ProductList()

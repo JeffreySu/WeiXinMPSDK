5a61ba56d668250fd9e5704719481c9e8827ac83
震巍 苏 <zsu@senparc.com>
2015-09-24 15:52:05 +0800

    建立MessageHandler部分类

---
+++ Senparc.Weixin.MP/Senparc.Weixin.MP/MessageHandlers/IMessageHandler.cs
@@ -0,0 +1,21 @@
+﻿/*----------------------------------------------------------------
+    Copyright (C) 2015 Senparc
+    
+    文件名：IMessageHandler.cs
+    文件功能描述：MessageHandler接口
+    
+    
+    创建标识：Senparc - 20150924
+    
+----------------------------------------------------------------*/
+using Senparc.Weixin.MP.Entities;
+
+namespace Senparc.Weixin.MP.MessageHandlers
+{
+
+    public interface IMessageHandler : Weixin.MessageHandlers.IMessageHandler<IRequestMessageBase, IResponseMessageBase>
+    {
+        new IRequestMessageBase RequestMessage { get; set; }
+        new IResponseMessageBase ResponseMessage { get; set; }
+    }
+}

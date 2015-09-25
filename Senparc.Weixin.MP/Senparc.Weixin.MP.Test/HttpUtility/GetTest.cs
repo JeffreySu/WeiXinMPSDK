aa64f32a9a27aceca72a474b3617c6ab8de62331
震巍 苏 <zsu@senparc.com>
2015-09-25 21:38:39 +0800

    增加异步方法单元测试

--- Senparc.Weixin.MP/Senparc.Weixin.MP.Test/HttpUtility/GetTest.cs
+++ Senparc.Weixin.MP/Senparc.Weixin.MP.Test/HttpUtility/GetTest.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Senparc.Weixin.Exceptions;
 using Senparc.Weixin.MP.Entities;
@@ -26,5 +29,44 @@ namespace Senparc.Weixin.MP.Test.HttpUtility
                 Assert.AreEqual(ex.JsonResult.errcode, ReturnCode.不合法的APPID);
             }
         }
+
+        [TestMethod]
+        public void GetJsonAsyncTest()
+        {
+            //return;//已经通过，但需要连接远程测试，太耗时，常规测试时暂时忽略。
+            var url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET";
+
+            var t1 = Task.Factory.StartNew(async delegate { await Run(url); });
+            var t2 = Task.Factory.StartNew(delegate { Run(url); });
+            var t3 = Task.Factory.StartNew(delegate { Run(url); });
+            var t4 = Task.Factory.StartNew(delegate { Run(url); });
+
+
+            Enumerable.Range(0, 30).ToList().ForEach(i =>
+            { });
+
+            Console.WriteLine("Waiting...");
+            Task.WaitAll(t1, t2, t3, t4);
+        }
+
+        private async Task Run(string url)
+        {
+            Console.WriteLine("Start Task.CurrentId：{0}，Time：{1}",Task.CurrentId,DateTime.Now.Ticks);
+
+            try
+            {
+                //这里因为参数错误，系统会返回错误信息
+                AccessTokenResult resultFail = await Get.GetJsonAsync<AccessTokenResult>(url);
+                Assert.Fail();//上一步就应该已经抛出异常
+            }
+            catch (ErrorJsonResultException ex)
+            {
+                //实际返回的信息（错误信息）
+                Assert.AreEqual(ex.JsonResult.errcode, ReturnCode.不合法的APPID);
+
+                Console.WriteLine("End Task.CurrentId：{0}，Time：{1}", Task.CurrentId, DateTime.Now.Ticks);
+            }
+        }
     }
 }
+

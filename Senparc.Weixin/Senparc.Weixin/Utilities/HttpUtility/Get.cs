741c7c73e1b192445d96db7b847b3f0bb16db3e1
震巍 苏 <zsu@senparc.com>
2015-09-25 18:35:12 +0800

    Senparc.Weixin升级到v4.1.0，提供异步http请求方法（未全面单元测试）

--- Senparc.Weixin/Senparc.Weixin/Utilities/HttpUtility/Get.cs
+++ Senparc.Weixin/Senparc.Weixin/Utilities/HttpUtility/Get.cs
@@ -17,6 +17,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading.Tasks;
 using System.Web.Script.Serialization;
 using Senparc.Weixin.Entities;
 using Senparc.Weixin.Exceptions;
@@ -25,6 +26,8 @@ namespace Senparc.Weixin.HttpUtility
 {
     public static class Get
     {
+        #region 同步方法
+
         public static T GetJson<T>(string url, Encoding encoding = null)
         {
             string returnText = HttpUtility.RequestUtility.HttpGet(url, encoding);
@@ -55,7 +58,7 @@ namespace Senparc.Weixin.HttpUtility
         {
             //ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3
             //ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);  
-            
+
             WebClient wc = new WebClient();
             var data = wc.DownloadData(url);
             foreach (var b in data)
@@ -63,5 +66,51 @@ namespace Senparc.Weixin.HttpUtility
                 stream.WriteByte(b);
             }
         }
+
+        #endregion
+
+        #region 异步方法
+
+        public static async Task<T> GetJsonAsync<T>(string url, Encoding encoding = null)
+        {
+            string returnText = await HttpUtility.RequestUtility.HttpGetAsync(url, encoding);
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+
+            if (returnText.Contains("errcode"))
+            {
+                //可能发生错误
+                WxJsonResult errorResult = js.Deserialize<WxJsonResult>(returnText);
+                if (errorResult.errcode != ReturnCode.请求成功)
+                {
+                    //发生错误
+                    throw new ErrorJsonResultException(
+                        string.Format("微信请求发生错误！错误代码：{0}，说明：{1}",
+                                        (int)errorResult.errcode,
+                                        errorResult.errmsg),
+                                      null, errorResult);
+                }
+            }
+
+            T result = js.Deserialize<T>(returnText);
+
+            return result;
+        }
+
+        public static async void DownloadAsync(string url, Stream stream)
+        {
+            //ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3
+            //ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);  
+
+            WebClient wc = new WebClient();
+            var data = await wc.DownloadDataTaskAsync(url);
+            await stream.WriteAsync(data, 0, data.Length);
+            //foreach (var b in data)
+            //{
+            //    stream.WriteAsync(b);
+            //}
+        }
+
+        #endregion
     }
 }

d645d9a64cece2691ad0112ac59372765e1cb548
震巍 苏 <zsu@senparc.com>
2015-10-12 08:48:48 +0800

    将CommonJsonSend统一到Senparc.Weixin中

--- Senparc.Weixin.Open/Senparc.Weixin.Open/CommonAPIs/CommonJsonSend.cs
+++ Senparc.Weixin.Open/Senparc.Weixin.Open/CommonAPIs/CommonJsonSend.cs
@@ -18,11 +18,11 @@ using Senparc.Weixin.HttpUtility;
 
 namespace Senparc.Weixin.Open.CommonAPIs
 {
-    public enum CommonJsonSendType
-    {
-        GET,
-        POST
-    }
+    //public enum CommonJsonSendType
+    //{
+    //    GET,
+    //    POST
+    //}
 
     public static class CommonJsonSend
     {
@@ -32,11 +32,12 @@ namespace Senparc.Weixin.Open.CommonAPIs
         /// <param name="accessToken">这里的AccessToken是通用接口的AccessToken，非OAuth的。如果不需要，可以为null，此时urlFormat不要提供{0}参数</param>
         /// <param name="urlFormat"></param>
         /// <param name="data">如果是Get方式，可以为null</param>
+        /// <param name="sendType"></param>
         /// <param name="timeOut">代理请求超时时间（毫秒）</param>
         /// <returns></returns>
         public static WxJsonResult Send(string accessToken, string urlFormat, object data, CommonJsonSendType sendType = CommonJsonSendType.POST, int timeOut = Config.TIME_OUT)
         {
-            return Send<WxJsonResult>(accessToken, urlFormat, data, sendType, timeOut);
+            return Senparc.Weixin.CommonAPIs.CommonJsonSend.Send(accessToken, urlFormat, data, sendType, timeOut);
         }
 
         /// <summary>
@@ -45,41 +46,12 @@ namespace Senparc.Weixin.Open.CommonAPIs
         /// <param name="accessToken">这里的AccessToken是通用接口的AccessToken，非OAuth的。如果不需要，可以为null，此时urlFormat不要提供{0}参数</param>
         /// <param name="urlFormat">用accessToken参数填充{0}</param>
         /// <param name="data">如果是Get方式，可以为null</param>
+        /// <param name="sendType"></param>
         /// <param name="timeOut">代理请求超时时间（毫秒）</param>
         /// <returns></returns>
         public static T Send<T>(string accessToken, string urlFormat, object data, CommonJsonSendType sendType = CommonJsonSendType.POST, int timeOut = Config.TIME_OUT)
         {
-            //TODO:此方法可以设定一个日志记录开关
-
-            try
-            {
-                var url = string.IsNullOrEmpty(accessToken) ? urlFormat : string.Format(urlFormat, accessToken);
-                switch (sendType)
-                {
-                    case CommonJsonSendType.GET:
-                        return Get.GetJson<T>(url);
-                    case CommonJsonSendType.POST:
-                        SerializerHelper serializerHelper = new SerializerHelper();
-                        var jsonString = serializerHelper.GetJsonString(data);
-                        using (MemoryStream ms = new MemoryStream())
-                        {
-                            var bytes = Encoding.UTF8.GetBytes(jsonString);
-                            ms.Write(bytes, 0, bytes.Length);
-                            ms.Seek(0, SeekOrigin.Begin);
-
-                            return Post.PostGetJson<T>(url, null, ms, timeOut: timeOut);
-                        }
-
-                    //TODO:对于特定的错误类型自动进行一次重试，如40001（目前的问题是同样40001会出现在不同的情况下面）
-                    default:
-                        throw new ArgumentOutOfRangeException("sendType");
-                }
-            }
-            catch (ErrorJsonResultException ex)
-            {
-                ex.ApiUrl = urlFormat;
-                throw;
-            }
+            return Senparc.Weixin.CommonAPIs.CommonJsonSend.Send<T>(accessToken, urlFormat, data, sendType, timeOut);
         }
     }
 }

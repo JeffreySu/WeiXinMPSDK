10d1e099c3f2803e9e135895f418682521356edc
震巍 苏 <zsu@senparc.com>
2015-10-21 13:04:26 +0800

    开始增加分布式缓存功能

---
+++ Senparc.Weixin/Senparc.Weixin/Cache/ISenparcCacheStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.Weixin.Cache
+{
+    //interface ISenparcCacheStrategy
+    //{
+    //    //TODO：有更复杂缓存控制需要的时候启用
+    //}
+}
---
+++ Senparc.Weixin/Senparc.Weixin/Cache/SenparcCache.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.Weixin.Cache
+{
+    /// <summary>
+    /// 缓存类
+    /// </summary>
+    public class SenparcCache
+    {
+        public static void Register(Func<object> getContainerCollectionFunc)
+        {
+
+        }
+    }
+}

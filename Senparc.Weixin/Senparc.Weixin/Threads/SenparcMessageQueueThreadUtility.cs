b3f7182064aad34bcc5bad0485f8d3827eca43d6
震巍 苏 <zsu@senparc.com>
2015-12-26 16:07:20 +0800

    完成列队线程

--- Senparc.Weixin/Senparc.Weixin/Threads/SenparcMessageQueueThreadUtility.cs
+++ Senparc.Weixin/Senparc.Weixin/Threads/SenparcMessageQueueThreadUtility.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Senparc.Weixin.MessageQueue
+namespace Senparc.Weixin.Threads
 {
     public class SenparcMessageQueueThreadUtility
     {
---
+++ Senparc.Weixin/Senparc.Weixin/Threads/ThreadUtility.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Senparc.Weixin.Threads
+{
+    /// <summary>
+    /// 线程处理类
+    /// </summary>
+    public static class ThreadUtility
+    {
+
+        /// <summary>
+        /// 异步线程容器
+        /// </summary>
+        public static Dictionary<string, Thread> AsynThreadCollection = new Dictionary<string, Thread>();//后台运行线程
+
+        static ThreadUtility()
+        {
+            {
+                SenparcMessageQueueThreadUtility senparcMessageQueue = new SenparcMessageQueueThreadUtility();
+                Thread senparcMessageQueueThread = new Thread(senparcMessageQueue.Run) { Name = "SenparcMessageQueue" };
+                AsynThreadCollection.Add(senparcMessageQueueThread.Name, senparcMessageQueueThread);
+            }
+
+            AsynThreadCollection.Values.ToList().ForEach(z =>
+            {
+                z.IsBackground = true;
+                z.Start();
+            });//全部运行
+        }
+    }
+}

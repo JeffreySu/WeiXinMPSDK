aae26d68def1ce4a3c3d9f8fc1f5996d5a936b10
震巍 苏 <zsu@senparc.com>
2016-02-06 01:16:00 +0800

    modify

--- Senparc.Weixin/Senparc.Weixin/Containers/BaseContainer.cs
+++ Senparc.Weixin/Senparc.Weixin/Containers/BaseContainer.cs
@@ -91,131 +91,131 @@ namespace Senparc.Weixin.Containers
             }
         }
 
-/// <summary>
-/// 获取缓存Key
-/// </summary>
-/// <returns></returns>
-public static string GetCacheKey()
-{
-    return string.Format("Container:{0}", typeof(TBag));
-}
-
-/// <summary>
-/// 获取完整的数据集合的列表，包括所有的Container数据在内（建议不要进行任何修改操作）
-/// </summary>
-/// <returns></returns>
-public static IDictionary<string, IContainerItemCollection> GetCollectionList()
-{
-    return CollectionList;
-}
-
-/// <summary>
-/// 获取所有容器内已经注册的项目
-/// （此方法将会遍历Dictionary，当数据项很多的时候效率会明显降低）
-/// </summary>
-/// <returns></returns>
-public static List<TBag> GetAllItems()
-{
-    return ItemCollection.Values.Select(z => z as TBag).ToList();
-}
-
-/// <summary>
-/// 尝试获取某一项Bag
-/// </summary>
-/// <param name="key"></param>
-/// <returns></returns>
-public static TBag TryGetItem(string key)
-{
-    if (ItemCollection.ContainsKey(key))
-    {
-        return ItemCollection[key] as TBag;
-    }
-
-    return default(TBag);
-}
-
-/// <summary>
-/// 尝试获取某一项Bag中的具体某个属性
-/// </summary>
-/// <param name="key"></param>
-/// <param name="property">具体某个属性</param>
-/// <returns></returns>
-public static TK TryGetItem<TK>(string key, Func<TBag, TK> property)
-{
-    if (ItemCollection.ContainsKey(key))
-    {
-        var item = ItemCollection[key] as TBag;
-        return property(item);
-    }
-    return default(TK);
-}
-
-/// <summary>
-/// 更新数据项
-/// </summary>
-/// <param name="key"></param>
-/// <param name="value">为null时删除该项</param>
-public static void Update(string key, TBag value)
-{
-    if (value == null)
-    {
-        ItemCollection.Remove(key);
-    }
-    else
-    {
-        if (string.IsNullOrEmpty(value.Key))
+        /// <summary>
+        /// 获取缓存Key
+        /// </summary>
+        /// <returns></returns>
+        public static string GetCacheKey()
         {
-            value.Key = key;//确保Key有值
-        }
-        else
-        {
-            key = value.Key;//统一key
+            return string.Format("Container:{0}", typeof(TBag));
         }
 
-        if (string.IsNullOrEmpty(key))
+        /// <summary>
+        /// 获取完整的数据集合的列表，包括所有的Container数据在内（建议不要进行任何修改操作）
+        /// </summary>
+        /// <returns></returns>
+        public static IDictionary<string, IContainerItemCollection> GetCollectionList()
         {
-            throw new WeixinException("key和value,Key不可以同时为null或空字符串！");
+            return CollectionList;
         }
 
-        ItemCollection[key] = value;
-
-        Cache.Update(key, ItemCollection);//更新到缓存，TODO：有的缓存框架可一直更新Hash中的某个键值对
-    }
-}
-
-/// <summary>
-/// 更新数据项
-/// </summary>
-/// <param name="key"></param>
-/// <param name="partialUpdate">为null时删除该项</param>
-public static void Update(string key, Action<TBag> partialUpdate)
-{
-    if (partialUpdate == null)
-    {
-        ItemCollection.Remove(key);//移除对象
-    }
-    else
-    {
-        if (!ItemCollection.ContainsKey(key))
+        /// <summary>
+        /// 获取所有容器内已经注册的项目
+        /// （此方法将会遍历Dictionary，当数据项很多的时候效率会明显降低）
+        /// </summary>
+        /// <returns></returns>
+        public static List<TBag> GetAllItems()
         {
-            ItemCollection[key] = new TBag()
+            return ItemCollection.Values.Select(z => z as TBag).ToList();
+        }
+
+        /// <summary>
+        /// 尝试获取某一项Bag
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static TBag TryGetItem(string key)
+        {
+            if (ItemCollection.ContainsKey(key))
             {
-                Key = key//确保这一项Key已经被记录
-            };
-        }
-        partialUpdate(ItemCollection[key] as TBag);//更新对象
-        Cache.Update(key, ItemCollection);//更新到缓存，TODO：有的缓存框架可一直更新Hash中的某个键值对
-    }
-}
+                return ItemCollection[key] as TBag;
+            }
 
-/// <summary>
-/// 检查Key是否已经注册
-/// </summary>
-/// <param name="key"></param>
-/// <returns></returns>
-public static bool CheckRegistered(string key)
-{
-    return ItemCollection.ContainsKey(key);
-}
+            return default(TBag);
+        }
+
+        /// <summary>
+        /// 尝试获取某一项Bag中的具体某个属性
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="property">具体某个属性</param>
+        /// <returns></returns>
+        public static TK TryGetItem<TK>(string key, Func<TBag, TK> property)
+        {
+            if (ItemCollection.ContainsKey(key))
+            {
+                var item = ItemCollection[key] as TBag;
+                return property(item);
+            }
+            return default(TK);
+        }
+
+        /// <summary>
+        /// 更新数据项
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value">为null时删除该项</param>
+        public static void Update(string key, TBag value)
+        {
+            if (value == null)
+            {
+                ItemCollection.Remove(key);
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(value.Key))
+                {
+                    value.Key = key;//确保Key有值
+                }
+                else
+                {
+                    key = value.Key;//统一key
+                }
+
+                if (string.IsNullOrEmpty(key))
+                {
+                    throw new WeixinException("key和value,Key不可以同时为null或空字符串！");
+                }
+
+                ItemCollection[key] = value;
+
+                Cache.Update(key, ItemCollection);//更新到缓存，TODO：有的缓存框架可一直更新Hash中的某个键值对
+            }
+        }
+
+        /// <summary>
+        /// 更新数据项
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="partialUpdate">为null时删除该项</param>
+        public static void Update(string key, Action<TBag> partialUpdate)
+        {
+            if (partialUpdate == null)
+            {
+                ItemCollection.Remove(key);//移除对象
+            }
+            else
+            {
+                if (!ItemCollection.ContainsKey(key))
+                {
+                    ItemCollection[key] = new TBag()
+                    {
+                        Key = key//确保这一项Key已经被记录
+                    };
+                }
+                partialUpdate(ItemCollection[key] as TBag);//更新对象
+                Cache.Update(key, ItemCollection);//更新到缓存，TODO：有的缓存框架可一直更新Hash中的某个键值对
+            }
+        }
+
+        /// <summary>
+        /// 检查Key是否已经注册
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static bool CheckRegistered(string key)
+        {
+            return ItemCollection.ContainsKey(key);
+        }
     }
 }

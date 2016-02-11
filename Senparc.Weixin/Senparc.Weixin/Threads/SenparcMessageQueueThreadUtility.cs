01fdab008588837ae3124674854f3846ed80fb41
震巍 苏 <zsu@senparc.com>
2016-02-11 21:00:33 +0800

    优化消息列队处理

--- Senparc.Weixin/Senparc.Weixin/Threads/SenparcMessageQueueThreadUtility.cs
+++ Senparc.Weixin/Senparc.Weixin/Threads/SenparcMessageQueueThreadUtility.cs
@@ -32,21 +32,50 @@ namespace Senparc.Weixin.Threads
         }
 
         /// <summary>
+        /// 析构函数，将未处理的列队处理掉
+        /// </summary>
+        ~SenparcMessageQueueThreadUtility()
+        {
+            try
+            {
+                var mq = new SenparcMessageQueue();
+                System.Diagnostics.Trace.WriteLine(string.Format("SenparcMessageQueueThreadUtility执行析构函数"));
+                System.Diagnostics.Trace.WriteLine(string.Format("当前列队数量：{0}", mq.GetCount()));
+
+                OperateQueue();//处理列队
+            }
+            catch (Exception ex)
+            {
+                //此处可以添加日志
+                System.Diagnostics.Trace.WriteLine(string.Format("SenparcMessageQueueThreadUtility执行析构函数错误：{0}", ex.Message));
+            }
+
+        }
+
+        /// <summary>
+        /// 操作列队
+        /// </summary>
+        private void OperateQueue()
+        {
+            var mq = new SenparcMessageQueue();
+            var key = mq.GetCurrentKey(); //获取最新的Key
+            while (!string.IsNullOrEmpty(key))
+            {
+                var mqItem = mq.GetItem(key); //获取任务项
+                mqItem.Action(); //执行
+                mq.Remove(key); //清除
+                key = mq.GetCurrentKey(); //获取最新的Key
+            }
+        }
+
+        /// <summary>
         /// 启动线程轮询
         /// </summary>
         public void Run()
         {
             do
             {
-                var mq = new SenparcMessageQueue();
-                var key = mq.GetCurrentKey();//获取最新的Key
-                while (!string.IsNullOrEmpty(key))
-                {
-                    var mqItem = mq.GetItem(key);//获取任务项
-                    mqItem.Action();//执行
-                    mq.Remove(key);//清除
-                    key = mq.GetCurrentKey();//获取最新的Key
-                }
+                OperateQueue();
                 Thread.Sleep(_sleepMilliSeconds);
             } while (true);
         }

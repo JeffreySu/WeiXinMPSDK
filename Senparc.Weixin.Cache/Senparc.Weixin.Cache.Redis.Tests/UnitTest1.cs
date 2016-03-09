fd1fe9c3b955f51db239e2dd8b65612250d43351
震巍 苏 <zsu@senparc.com>
2016-03-09 17:02:47 +0800

    创建Redis测试项目

---
+++ Senparc.Weixin.Cache/Senparc.Weixin.Cache.Redis.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Senparc.Weixin.Cache.Redis.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

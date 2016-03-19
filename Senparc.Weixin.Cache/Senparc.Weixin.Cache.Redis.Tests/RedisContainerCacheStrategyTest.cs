ce906029cab1ae0fdfa15281ad5995899ea03ea5
震巍 苏 <zsu@senparc.com>
2016-03-19 23:12:16 +0800

    完善Redis单元测试

--- Senparc.Weixin.Cache/Senparc.Weixin.Cache.Redis.Tests/RedisContainerCacheStrategyTest.cs
+++ Senparc.Weixin.Cache/Senparc.Weixin.Cache.Redis.Tests/RedisContainerCacheStrategyTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Senparc.Weixin.CacheUtility;
 
 namespace Senparc.Weixin.Cache.Redis.Tests
 {
@@ -37,6 +38,28 @@ namespace Senparc.Weixin.Cache.Redis.Tests
 
             var count2 = cache.GetCount();
             Assert.AreEqual(count + 1, count2);
+        }
+
+        [TestMethod]
+        public void RemoveTest()
+        {
+            var key = Guid.NewGuid().ToString();
+            var count = cache.GetCount();
+            cache.InsertToCache(key, new ContainerItemCollection()
+            {
+
+            });
+            
+            var item = cache.Get(key);
+            Assert.IsNotNull(item);
+            var count2 = cache.GetCount();
+            Assert.AreEqual(count + 1, count2);
+          
+            cache.RemoveFromCache(key);
+            item = cache.Get(key);
+            Assert.IsNull(item);
+            var count3 = cache.GetCount();
+            Assert.AreEqual(count,count3);
         }
     }
 }

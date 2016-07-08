52f3457bd08d58471f975363c19ad527ddb1ded7
jiehanlin <jiehanlin@qq.com>
2016-07-08 21:33:41 +0800

    增加Senparc.Weixin.Cache解决方案文件

---
+++ DotNetFramework/Senparc.Weixin.MP/Senparc.Weixin.MP/Agents/AgentHttpUtility.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.Weixin.MP.Agents
+{
+   public class AgentHttpUtility
+    {
+        /// <summary>
+        /// 使用Post方法获取字符串结果
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="cookieContainer"></param>
+        /// <param name="postStream"></param>
+        /// <param name="encoding"></param>
+        /// <param name="timeOut"></param>
+        /// <param name="checkValidationResult">验证服务器证书回调自动验证</param>        
+        /// <returns></returns>
+        public static string HttpPost(string url, CookieContainer cookieContainer = null, Stream postStream = null, Encoding encoding = null, int timeOut = Config.TIME_OUT, bool checkValidationResult = false)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.Timeout = timeOut;
+
+            //request.ContentType = "application/x-www-form-urlencoded";
+            //MVC5的应用在接受含有=号的xml数据时不能用application/x-www-form-urlencoded 的contentype，否则将出现500错误。
+            //但加密后的数据往往含有=号。因此改用xml。因为至少转发事件数据的xml，因此直接使用text/xml.
+            request.ContentType = "text/xml";
+
+            request.ContentLength = postStream != null ? postStream.Length : 0;
+            request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+            request.KeepAlive = true;
+            request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.57 Safari/537.36";
+
+            if (cookieContainer != null)
+            {
+                request.CookieContainer = cookieContainer;
+            }
+
+            #region 输入二进制流
+            if (postStream != null)
+            {
+                postStream.Position = 0;
+
+                //直接写入流
+                Stream requestStream = request.GetRequestStream();
+
+                byte[] buffer = new byte[1024];
+                int bytesRead = 0;
+                while ((bytesRead = postStream.Read(buffer, 0, buffer.Length)) != 0)
+                {
+                    requestStream.Write(buffer, 0, bytesRead);
+                }
+
+                //debug
+                //postStream.Seek(0, SeekOrigin.Begin);
+                //StreamReader sr = new StreamReader(postStream);
+                //var postStr = sr.ReadToEnd();
+
+                postStream.Close();//关闭文件访问
+            }
+            #endregion
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if (cookieContainer != null)
+            {
+                response.Cookies = cookieContainer.GetCookies(response.ResponseUri);
+            }
+
+            using (Stream responseStream = response.GetResponseStream())
+            {
+                using (StreamReader myStreamReader = new StreamReader(responseStream, encoding ?? Encoding.GetEncoding("utf-8")))
+                {
+                    string retString = myStreamReader.ReadToEnd();
+                    return retString;
+                }
+            }
+        }
+    }
+}

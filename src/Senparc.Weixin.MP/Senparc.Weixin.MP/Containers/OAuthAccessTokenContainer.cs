988e5139c847fbfa6622b2754f2bfbce598c6191
Sara Xu <hxu@senparc.com>
2016-08-04 14:03:28 +0800

    modify

--- src/Senparc.Weixin.MP/Senparc.Weixin.MP/Containers/OAuthAccessTokenContainer.cs
+++ src/Senparc.Weixin.MP/Senparc.Weixin.MP/Containers/OAuthAccessTokenContainer.cs
@@ -9,6 +9,7 @@
 
     修改标识：Senparc - 20160803
     修改描述：v14.2.3 使用ApiUtility.GetExpireTime()方法处理过期
+ 
     修改标识：Senparc - 20160804
     修改描述：增加TryGetOAuthAccessTokenAsync，GetOAuthAccessTokenAsync，GetOAuthAccessTokenResultAsync的异步方法
 

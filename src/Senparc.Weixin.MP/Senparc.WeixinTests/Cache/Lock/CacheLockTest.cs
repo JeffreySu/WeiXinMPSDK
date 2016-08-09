f97d01f2ac8876df5822a2b1cfeb59c55d98a7bf
Jeffrey Su <zsu@senparc.com>
2016-08-09 22:51:11 +0800

    测试优化分布锁

--- src/Senparc.Weixin.MP/Senparc.WeixinTests/Cache/Lock/CacheLockTest.cs
+++ src/Senparc.Weixin.MP/Senparc.WeixinTests/Cache/Lock/CacheLockTest.cs
@@ -24,7 +24,35 @@ namespace Senparc.WeixinTests.Cache.Lock
             });
             var dt2 = DateTime.Now;
             Console.WriteLine("Working Threads Count:{0}", 100 * 20 / (dt2 - dt1).TotalMilliseconds);
-            //测试结果：同时运行的线程数约为4（平均3.6）
+            //测试结果：同时运行的线程数约为4（平均3.6）,实际目测为5
+
+            Console.WriteLine("测试Threads");
+
+            List<Thread> list = new List<Thread>();
+            int[] runThreads = {0};
+
+            for (int i = 0; i < 100; i++)
+            {
+                runThreads[0]++;
+                var thread = new Thread(() =>
+                {
+                    Console.WriteLine("T:{0}，Use Time：{1}ms", Thread.CurrentThread.GetHashCode(),
+                                (DateTime.Now - dt2).TotalMilliseconds);
+                    Thread.Sleep(20);
+                    runThreads[0]--;
+                });
+                list.Add(thread);
+            }
+
+            var dt3 = DateTime.Now;
+            list.ForEach(z => z.Start());
+            while (runThreads[0] > 0)
+            {
+                Thread.Sleep(100);
+            }
+            var dt4 = DateTime.Now;
+            Console.WriteLine("Working Threads Count:{0}", 1000 * 20 / (dt4 - dt3).TotalMilliseconds);
+            //测试结果：无限制
         }
 
         [TestMethod]
@@ -52,7 +80,6 @@ namespace Senparc.WeixinTests.Cache.Lock
                 CacheStrategyFactory.RegisterContainerCacheStrategy(() => RedisContainerCacheStrategy.Instance);//Redis
             }
 
-            var cache = CacheStrategyFactory.GetContainerCacheStragegyInstance();
 
             Random rnd = new Random();
             var threadsCount = 20M;
@@ -61,7 +88,7 @@ namespace Senparc.WeixinTests.Cache.Lock
             //初步估计需要重试时间
             var differentLocksCount = (2 /*appId*/* 2 /*resource*/) /* = diffrent locks count */;
             var runCycle = threadsCount / differentLocksCount /* = Run Cycle*/;
-            var hopedTotalTime = runCycle * sleepMillionSeconds /* = Hoped Total Time */;
+            var hopedTotalTime = runCycle * (sleepMillionSeconds + 100) /* = Hoped Total Time */;
             var randomRetryDelay = (20 / 2) /*random retry delay*/;
             var retryTimes = hopedTotalTime / randomRetryDelay; /* = maxium retry times */;
 
@@ -78,11 +105,15 @@ namespace Senparc.WeixinTests.Cache.Lock
             for (int i = 0; i < (int)threadsCount; i++)
             {
                 runThreads++;
+                var i1 = i;
                 var thread = new Thread(() =>
                 {
-                    var appId = (i % 2).ToString();
+                    var appId = (i1 % 2).ToString();
                     var resourceName = "Test-" + rnd.Next(0, 2);
-                    Console.WriteLine("线程 {0} / {1} : {2} 进入，准备尝试锁", Thread.CurrentThread.GetHashCode(), resourceName, appId);
+                    var cache = CacheStrategyFactory.GetContainerCacheStragegyInstance();//每次重新获取实例（因为单例模式，所以其实是同一个）
+
+                    Console.WriteLine("线程 {0} / {1} : {2} 进入，准备尝试锁。Cache实例：{3}", Thread.CurrentThread.GetHashCode(), resourceName, appId,cache.GetHashCode());
+
 
                     DateTime dt1 = DateTime.Now;
                     using (var cacheLock = cache.InstanceCacheLockWrapper(resourceName, appId, (int)retryTimes, new TimeSpan(0, 0, 0, 0, 20)))
@@ -101,12 +132,17 @@ namespace Senparc.WeixinTests.Cache.Lock
                 list.Add(thread);
             }
 
+            var dtAll1 = DateTime.Now;
+
             list.ForEach(z => z.Start());
 
             while (runThreads > 0)
             {
                 Thread.Sleep(10);
             }
+
+            Console.WriteLine("Real Time:{0}ms", (DateTime.Now - dtAll1).TotalMilliseconds);
+
             //Thread.Sleep((int)hopedTotalTime + 1000);
             //while (true)
             //{

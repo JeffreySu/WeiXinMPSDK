5678aea232d6e68090f749a0757209c8247d4ad9
Jeffrey Su <zsu@senparc.com>
2016-08-10 09:51:46 +0800

    QY v4.1.4 AccessTokenResult添加序列化标签

--- src/Senparc.Weixin.QY/Senparc.Weixin.QY/Entities/JsonResult/AccessTokenResult.cs
+++ src/Senparc.Weixin.QY/Senparc.Weixin.QY/Entities/JsonResult/AccessTokenResult.cs
@@ -9,8 +9,12 @@
     
     修改标识：Senparc - 20150313
     修改描述：整理接口
+
+    修改标识：Senparc - 20160810
+    修改描述：v4.1.4 AccessTokenResult添加序列化标签
 ----------------------------------------------------------------*/
 
+using System;
 using Senparc.Weixin.Entities;
 
 namespace Senparc.Weixin.QY.Entities
@@ -18,6 +22,7 @@ namespace Senparc.Weixin.QY.Entities
     /// <summary>
     /// GetToken请求后的JSON返回格式
     /// </summary>
+    [Serializable]
     public class AccessTokenResult : QyJsonResult
     {
         /// <summary>

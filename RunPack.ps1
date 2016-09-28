2725caf10d68d04f72a59501d2005a4174267034
算神 <lishewen@gmail.com>
2016-09-28 11:00:34 +0800

    加入打包脚本供TFS使用

---
+++ RunPack.ps1
@@ -0,0 +1,4 @@
+dotnet pack src\Senparc.Weixin -c release
+dotnet pack src\Senparc.Weixin.MP -c release
+dotnet pack src\Senparc.Weixin.MP.MvcExtension -c release
+dotnet pack src\Senparc.Weixin.QY -c release

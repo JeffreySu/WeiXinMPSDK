8aec6ca31c83a0fac2a68bdbafdeeafc6f5e43f0
算神 <lishewen@gmail.com>
2016-09-28 11:29:52 +0800

    脚本调整

--- RunPack.ps1
+++ RunPack.ps1
@@ -1,4 +1,4 @@
-dotnet pack src\Senparc.Weixin -c release
-dotnet pack src\Senparc.Weixin.MP -c release
-dotnet pack src\Senparc.Weixin.MP.MvcExtension -c release
-dotnet pack src\Senparc.Weixin.QY -c release
+dotnet pack src\Senparc.Weixin\Senparc.Weixin -c release
+dotnet pack src\Senparc.Weixin.MP\Senparc.Weixin.MP -c release
+dotnet pack src\Senparc.Weixin.MP.MvcExtension\Senparc.Weixin.MP.MvcExtension -c release
+dotnet pack src\Senparc.Weixin.QY\Senparc.Weixin.QY -c release

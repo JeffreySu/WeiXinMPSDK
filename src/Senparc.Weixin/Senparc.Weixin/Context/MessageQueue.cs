0310f17229449ed6da24e2a7816e485e6b7e5c3a
算神 <lishewen@gmail.com>
2016-09-28 11:42:00 +0800

    忽略一下编译警告，以免TFS进行CI时候误报

--- src/Senparc.Weixin/Senparc.Weixin/Context/MessageQueue.cs
+++ src/Senparc.Weixin/Senparc.Weixin/Context/MessageQueue.cs
@@ -1,14 +1,14 @@
 ﻿/*----------------------------------------------------------------
-    Copyright (C) 2016 Senparc
-    
-    文件名：MessageQueue.cs
-    文件功能描述：微信消息列队（针对单个账号的往来消息）
-    
-    
-    创建标识：Senparc - 20150211
-    
-    修改标识：Senparc - 20150303
-    修改描述：整理接口
+	Copyright (C) 2016 Senparc
+	
+	文件名：MessageQueue.cs
+	文件功能描述：微信消息列队（针对单个账号的往来消息）
+	
+	
+	创建标识：Senparc - 20150211
+	
+	修改标识：Senparc - 20150303
+	修改描述：整理接口
 ----------------------------------------------------------------*/
 
 using System.Collections.Generic;
@@ -16,15 +16,17 @@ using Senparc.Weixin.Entities;
 
 namespace Senparc.Weixin.Context
 {
-    /// <summary>
-    /// 微信消息列队（针对单个账号的往来消息）
-    /// </summary>
-    /// <typeparam name="TM"></typeparam>
-    public class MessageQueue<TM,TRequest, TResponse> : List<TM> 
-        where TM : class, IMessageContext<TRequest, TResponse>, new()
-        where TRequest : IRequestMessageBase
-        where TResponse : IResponseMessageBase
-    {
+	/// <summary>
+	/// 微信消息列队（针对单个账号的往来消息）
+	/// </summary>
+	/// <typeparam name="TM"></typeparam>
+	/// <typeparam name="TRequest"></typeparam>
+	/// <typeparam name="TResponse"></typeparam>
+	public class MessageQueue<TM, TRequest, TResponse> : List<TM>
+		where TM : class, IMessageContext<TRequest, TResponse>, new()
+		where TRequest : IRequestMessageBase
+		where TResponse : IResponseMessageBase
+	{
 
-    }
+	}
 }

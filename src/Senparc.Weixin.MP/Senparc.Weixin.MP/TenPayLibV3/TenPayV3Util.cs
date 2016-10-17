a04ccfcfaf478f2cfaff1a046100235abf8c8f3a
Jeffrey Su <zsu@senparc.com>
2016-10-17 20:57:55 +0800

    modify

---
+++ src/Senparc.Weixin.MP/Senparc.Weixin.MP/TenPayLibV3/TenPayV3Util.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senparc.Weixin.MP.TenPayLibV3
+{
+    public class TenPayV3Util
+    {
+    }
+}

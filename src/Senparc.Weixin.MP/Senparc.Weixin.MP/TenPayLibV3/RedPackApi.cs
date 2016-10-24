c4f4b02de549077491d53eea6a6cc2621fc60393
Jeffrey Su <zsu@senparc.com>
2016-10-24 13:45:40 +0800

    修改RedPackApi

--- src/Senparc.Weixin.MP/Senparc.Weixin.MP/TenPayLibV3/RedPackApi.cs
+++ src/Senparc.Weixin.MP/Senparc.Weixin.MP/TenPayLibV3/RedPackApi.cs
@@ -6,6 +6,9 @@
     
     
     创建标识：Yu XiaoChou - 20160107
+        
+    修改标识：Senparc - 20161024
+    修改描述：v14.3.1024 重新整理红包发送方法
 ----------------------------------------------------------------*/
 
 using System;
@@ -16,6 +19,8 @@ using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Xml;
+using System.Xml.Linq;
+
 namespace Senparc.Weixin.MP.TenPayLibV3
 {
     /// <summary>
@@ -25,7 +30,7 @@ namespace Senparc.Weixin.MP.TenPayLibV3
     {
         private static string GetNewBillNo(string mchId)
         {
-            return string.Format("{0}{1}{2}",mchId,DateTime.Now.ToString("yyyyMMdd"), TenPayV3Util.BuildRandomStr(10));
+            return string.Format("{0}{1}{2}", mchId, DateTime.Now.ToString("yyyyMMdd"), TenPayV3Util.BuildRandomStr(10));
         }
 
         /// <summary>
@@ -34,7 +39,7 @@ namespace Senparc.Weixin.MP.TenPayLibV3
         /// <param name="appId">公众账号AppID</param>
         /// <param name="mchId">商户MchID</param>
         /// <param name="tenPayKey">支付密钥，微信商户平台(pay.weixin.qq.com)-->账户设置-->API安全-->密钥设置</param>
-        /// <param name="tenPayCertPath">证书地址（硬盘地址，形如E://cert//apiclient_cert.p12）</param>
+        /// <param name="tenPayCertPath">证书地址（硬盘物理地址，形如E:\\cert\\apiclient_cert.p12）</param>
         /// <param name="openId">要发红包的用户的OpenID</param>
         /// <param name="senderName">红包发送者名称，会显示给接收红包的用户</param>
         /// <param name="iP">发送红包的服务器地址</param>
@@ -44,11 +49,21 @@ namespace Senparc.Weixin.MP.TenPayLibV3
         /// <param name="remark">活动描述，用于低版本微信显示</param>
         /// <param name="nonceStr">将nonceStr随机字符串返回，开发者可以存到数据库用于校验</param>
         /// <param name="paySign">将支付签名返回，开发者可以存到数据库用于校验</param>
+        /// <param name="scene">场景id（非必填）</param>
+        /// <param name="riskInfo">活动信息,String(128)posttime:用户操作的时间戳。
+        /// <para>示例：posttime%3d123123412%26clientversion%3d234134%26mobile%3d122344545%26deviceid%3dIOS</para>
+        /// <para>mobile:业务系统账号的手机号，国家代码-手机号。不需要+号</para>
+        /// <para>deviceid :mac 地址或者设备唯一标识</para>
+        /// <para>clientversion :用户操作的客户端版本</para>
+        /// <para>把值为非空的信息用key = value进行拼接，再进行urlencode</para>
+        /// <para>urlencode(posttime= xx & mobile = xx & deviceid = xx)</para>
+        /// </param>
+        /// <param name="consumeMchId">资金授权商户号，服务商替特约商户发放时使用，String(32)。示例：1222000096</param>
         /// <returns></returns>
         public static NormalRedPackResult SendNormalRedPack(string appId, string mchId, string tenPayKey, string tenPayCertPath,
             string openId, string senderName,
-            string iP, int redPackAmount, string wishingWord, string actionName, string remark, 
-            out string nonceStr, out string paySign)
+            string iP, int redPackAmount, string wishingWord, string actionName, string remark,
+            out string nonceStr, out string paySign, RedPack_Scene? scene = null, string riskInfo = null, string consumeMchId = null)
         {
             string mchbillno = GetNewBillNo(mchId);
 
@@ -75,11 +90,23 @@ namespace Senparc.Weixin.MP.TenPayLibV3
             paySign = packageReqHandler.CreateMd5Sign("key", tenPayKey);
             packageReqHandler.SetParameter("sign", paySign);	                    //签名
 
+            if (scene.HasValue)
+            {
+                packageReqHandler.SetParameter("scene_id", scene.Value.ToString());//场景id
+            }
+            if (riskInfo != null)
+            {
+                packageReqHandler.SetParameter("risk_info", riskInfo);//活动信息	
+            }
+            if (consumeMchId != null)
+            {
+                packageReqHandler.SetParameter("consume_mch_id", consumeMchId);//活动信息	
+            }
+
             //最新的官方文档中将以下三个字段去除了
             //packageReqHandler.SetParameter("nick_name", "提供方名称");                 //提供方名称
             //packageReqHandler.SetParameter("max_value", "100");                //最大红包金额，单位分
             //packageReqHandler.SetParameter("min_value", "100");                //最小红包金额，单位分
-
 
             //发红包需要post的数据
             string data = packageReqHandler.ParseXML();
@@ -92,9 +119,9 @@ namespace Senparc.Weixin.MP.TenPayLibV3
             string password = mchId;
 
             //调用证书
-            //X509Certificate2 cer = new X509Certificate2(cert, password, X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.MachineKeySet);
+            X509Certificate2 cer = new X509Certificate2(cert, password, X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.MachineKeySet);
             ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);
-            X509Certificate cer = new X509Certificate(cert, password);
+            //X509Certificate cer = new X509Certificate(cert, password);
 
             #region 发起post请求
             HttpWebRequest webrequest = (HttpWebRequest)HttpWebRequest.Create(url);
@@ -116,12 +143,14 @@ namespace Senparc.Weixin.MP.TenPayLibV3
             XmlDocument doc = new XmlDocument();
             doc.LoadXml(responseContent);
 
+            XDocument xDoc = XDocument.Load(responseContent);
 
             NormalRedPackResult normalReturn = new NormalRedPackResult
             {
                 err_code = "",
                 err_code_des = ""
             };
+
             if (doc.SelectSingleNode("/xml/return_code") != null)
             {
                 normalReturn.return_code = (doc.SelectSingleNode("/xml/return_code").InnerText.ToUpper() == "SUCCESS");
@@ -193,7 +222,7 @@ namespace Senparc.Weixin.MP.TenPayLibV3
         /// <param name="mchId">商户MchID</param>
         /// <param name="tenPayKey">支付密钥，微信商户平台(pay.weixin.qq.com)-->账户设置-->API安全-->密钥设置</param>
         /// <param name="tenPayCertPath">证书地址（硬盘地址，形如E://cert//apiclient_cert.p12）</param>
-        /// <param name="MchBillNo">商家订单号</param>
+        /// <param name="mchBillNo">商家订单号</param>
         /// <returns></returns>
         public static SearchRedPackResult SearchRedPack(string appId, string mchId, string tenPayKey, string tenPayCertPath, string mchBillNo)
         {

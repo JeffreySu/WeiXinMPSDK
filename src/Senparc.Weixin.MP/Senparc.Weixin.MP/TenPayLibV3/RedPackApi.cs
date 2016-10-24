773915758e70102f4831d9be51bb1862203ec5c1
Jeffrey Su <www.jeffrey.su@gmail.com>
2016-10-24 20:46:51 +0800

    隐藏裂变红包接口，下一个版本发布稳定方法

--- src/Senparc.Weixin.MP/Senparc.Weixin.MP/TenPayLibV3/RedPackApi.cs
+++ src/Senparc.Weixin.MP/Senparc.Weixin.MP/TenPayLibV3/RedPackApi.cs
@@ -215,188 +215,190 @@ namespace Senparc.Weixin.MP.TenPayLibV3
             return normalReturn;
         }
 
-        /// <summary>
-        /// 裂变红包发送
-        /// <para>裂变红包：一次可以发放一组红包。首先领取的用户为种子用户，种子用户领取一组红包当中的一个，并可以通过社交分享将剩下的红包给其他用户。裂变红包充分利用了人际传播的优势。</para>
-        /// </summary>
-        /// <param name="appId">公众账号AppID</param>
-        /// <param name="mchId">商户MchID</param>
-        /// <param name="tenPayKey">支付密钥，微信商户平台(pay.weixin.qq.com)-->账户设置-->API安全-->密钥设置</param>
-        /// <param name="tenPayCertPath">证书地址（硬盘物理地址，形如E:\\cert\\apiclient_cert.p12）</param>
-        /// <param name="openId">要发红包的用户的OpenID</param>
-        /// <param name="senderName">红包发送者名称，会显示给接收红包的用户</param>
-        /// <param name="iP">发送红包的服务器地址</param>
-        /// <param name="redPackAmount">红包金额（单位分）</param>
-        /// <param name="wishingWord">祝福语</param>
-        /// <param name="actionName">活动名称</param>
-        /// <param name="remark">活动描述，用于低版本微信显示</param>
-        /// <param name="nonceStr">将nonceStr随机字符串返回，开发者可以存到数据库用于校验</param>
-        /// <param name="paySign">将支付签名返回，开发者可以存到数据库用于校验</param>
-        /// <param name="scene">场景id（非必填）</param>
-        /// <param name="riskInfo">活动信息（非必填）,String(128)posttime:用户操作的时间戳。
-        /// <para>示例：posttime%3d123123412%26clientversion%3d234134%26mobile%3d122344545%26deviceid%3dIOS</para>
-        /// <para>mobile:业务系统账号的手机号，国家代码-手机号。不需要+号</para>
-        /// <para>deviceid :mac 地址或者设备唯一标识</para>
-        /// <para>clientversion :用户操作的客户端版本</para>
-        /// <para>把值为非空的信息用key = value进行拼接，再进行urlencode</para>
-        /// <para>urlencode(posttime= xx & mobile = xx & deviceid = xx)</para>
-        /// </param>
-        /// <param name="consumeMchId">资金授权商户号，服务商替特约商户发放时使用（非必填），String(32)。示例：1222000096</param>
-        /// <returns></returns>
-        public static NormalRedPackResult SendNGroupRedPack(string appId, string mchId, string tenPayKey, string tenPayCertPath,
-            string openId, string senderName,
-            string iP, int redPackAmount, string wishingWord, string actionName, string remark,
-            out string nonceStr, out string paySign, RedPack_Scene? scene = null, string riskInfo = null, string consumeMchId = null)
-        {
-            string mchbillno = GetNewBillNo(mchId);
+        #region v14.3.105中将发布
+        ///// <summary>
+        ///// 裂变红包发送
+        ///// <para>裂变红包：一次可以发放一组红包。首先领取的用户为种子用户，种子用户领取一组红包当中的一个，并可以通过社交分享将剩下的红包给其他用户。裂变红包充分利用了人际传播的优势。</para>
+        ///// </summary>
+        ///// <param name="appId">公众账号AppID</param>
+        ///// <param name="mchId">商户MchID</param>
+        ///// <param name="tenPayKey">支付密钥，微信商户平台(pay.weixin.qq.com)-->账户设置-->API安全-->密钥设置</param>
+        ///// <param name="tenPayCertPath">证书地址（硬盘物理地址，形如E:\\cert\\apiclient_cert.p12）</param>
+        ///// <param name="openId">要发红包的用户的OpenID</param>
+        ///// <param name="senderName">红包发送者名称，会显示给接收红包的用户</param>
+        ///// <param name="iP">发送红包的服务器地址</param>
+        ///// <param name="redPackAmount">红包金额（单位分）</param>
+        ///// <param name="wishingWord">祝福语</param>
+        ///// <param name="actionName">活动名称</param>
+        ///// <param name="remark">活动描述，用于低版本微信显示</param>
+        ///// <param name="nonceStr">将nonceStr随机字符串返回，开发者可以存到数据库用于校验</param>
+        ///// <param name="paySign">将支付签名返回，开发者可以存到数据库用于校验</param>
+        ///// <param name="scene">场景id（非必填）</param>
+        ///// <param name="riskInfo">活动信息（非必填）,String(128)posttime:用户操作的时间戳。
+        ///// <para>示例：posttime%3d123123412%26clientversion%3d234134%26mobile%3d122344545%26deviceid%3dIOS</para>
+        ///// <para>mobile:业务系统账号的手机号，国家代码-手机号。不需要+号</para>
+        ///// <para>deviceid :mac 地址或者设备唯一标识</para>
+        ///// <para>clientversion :用户操作的客户端版本</para>
+        ///// <para>把值为非空的信息用key = value进行拼接，再进行urlencode</para>
+        ///// <para>urlencode(posttime= xx & mobile = xx & deviceid = xx)</para>
+        ///// </param>
+        ///// <param name="consumeMchId">资金授权商户号，服务商替特约商户发放时使用（非必填），String(32)。示例：1222000096</param>
+        ///// <returns></returns>
+        //public static NormalRedPackResult SendNGroupRedPack(string appId, string mchId, string tenPayKey, string tenPayCertPath,
+        //    string openId, string senderName,
+        //    string iP, int redPackAmount, string wishingWord, string actionName, string remark,
+        //    out string nonceStr, out string paySign, RedPack_Scene? scene = null, string riskInfo = null, string consumeMchId = null)
+        //{
+        //    string mchbillno = GetNewBillNo(mchId);
 
-            nonceStr = TenPayV3Util.GetNoncestr();
-            //RequestHandler packageReqHandler = new RequestHandler(null);
+        //    nonceStr = TenPayV3Util.GetNoncestr();
+        //    //RequestHandler packageReqHandler = new RequestHandler(null);
 
-            //string accessToken = AccessTokenContainer.GetAccessToken(ConstantClass.AppID);
-            //UserInfoJson userInforResult = UserApi.Info(accessToken, openID);
+        //    //string accessToken = AccessTokenContainer.GetAccessToken(ConstantClass.AppID);
+        //    //UserInfoJson userInforResult = UserApi.Info(accessToken, openID);
 
-            RequestHandler packageReqHandler = new RequestHandler();
-            //设置package订单参数
-            packageReqHandler.SetParameter("nonce_str", nonceStr);              //随机字符串
-            packageReqHandler.SetParameter("wxappid", appId);		  //公众账号ID
-            packageReqHandler.SetParameter("mch_id", mchId);		  //商户号
-            packageReqHandler.SetParameter("mch_billno", mchbillno);                 //填入商家订单号
-            packageReqHandler.SetParameter("send_name", senderName);                //红包发送者名称
-            packageReqHandler.SetParameter("re_openid", openId);                 //接受收红包的用户的openId
-            packageReqHandler.SetParameter("total_amount", redPackAmount.ToString());                //付款金额，单位分
-            packageReqHandler.SetParameter("total_num", "1");               //红包发放总人数
-            packageReqHandler.SetParameter("wishing", wishingWord);               //红包祝福语
-            packageReqHandler.SetParameter("client_ip", iP);               //调用接口的机器Ip地址
-            packageReqHandler.SetParameter("act_name", actionName);   //活动名称
-            packageReqHandler.SetParameter("remark", remark);   //备注信息
-            paySign = packageReqHandler.CreateMd5Sign("key", tenPayKey);
-            packageReqHandler.SetParameter("sign", paySign);	                    //签名
+        //    RequestHandler packageReqHandler = new RequestHandler();
+        //    //设置package订单参数
+        //    packageReqHandler.SetParameter("nonce_str", nonceStr);              //随机字符串
+        //    packageReqHandler.SetParameter("wxappid", appId);		  //公众账号ID
+        //    packageReqHandler.SetParameter("mch_id", mchId);		  //商户号
+        //    packageReqHandler.SetParameter("mch_billno", mchbillno);                 //填入商家订单号
+        //    packageReqHandler.SetParameter("send_name", senderName);                //红包发送者名称
+        //    packageReqHandler.SetParameter("re_openid", openId);                 //接受收红包的用户的openId
+        //    packageReqHandler.SetParameter("total_amount", redPackAmount.ToString());                //付款金额，单位分
+        //    packageReqHandler.SetParameter("total_num", "1");               //红包发放总人数
+        //    packageReqHandler.SetParameter("wishing", wishingWord);               //红包祝福语
+        //    packageReqHandler.SetParameter("client_ip", iP);               //调用接口的机器Ip地址
+        //    packageReqHandler.SetParameter("act_name", actionName);   //活动名称
+        //    packageReqHandler.SetParameter("remark", remark);   //备注信息
+        //    paySign = packageReqHandler.CreateMd5Sign("key", tenPayKey);
+        //    packageReqHandler.SetParameter("sign", paySign);	                    //签名
 
-            if (scene.HasValue)
-            {
-                packageReqHandler.SetParameter("scene_id", scene.Value.ToString());//场景id
-            }
-            if (riskInfo != null)
-            {
-                packageReqHandler.SetParameter("risk_info", riskInfo);//活动信息	
-            }
-            if (consumeMchId != null)
-            {
-                packageReqHandler.SetParameter("consume_mch_id", consumeMchId);//活动信息	
-            }
+        //    if (scene.HasValue)
+        //    {
+        //        packageReqHandler.SetParameter("scene_id", scene.Value.ToString());//场景id
+        //    }
+        //    if (riskInfo != null)
+        //    {
+        //        packageReqHandler.SetParameter("risk_info", riskInfo);//活动信息	
+        //    }
+        //    if (consumeMchId != null)
+        //    {
+        //        packageReqHandler.SetParameter("consume_mch_id", consumeMchId);//活动信息	
+        //    }
 
-            //最新的官方文档中将以下三个字段去除了
-            //packageReqHandler.SetParameter("nick_name", "提供方名称");                 //提供方名称
-            //packageReqHandler.SetParameter("max_value", "100");                //最大红包金额，单位分
-            //packageReqHandler.SetParameter("min_value", "100");                //最小红包金额，单位分
+        //    //最新的官方文档中将以下三个字段去除了
+        //    //packageReqHandler.SetParameter("nick_name", "提供方名称");                 //提供方名称
+        //    //packageReqHandler.SetParameter("max_value", "100");                //最大红包金额，单位分
+        //    //packageReqHandler.SetParameter("min_value", "100");                //最小红包金额，单位分
 
-            //发红包需要post的数据
-            string data = packageReqHandler.ParseXML();
+        //    //发红包需要post的数据
+        //    string data = packageReqHandler.ParseXML();
 
-            //发红包接口地址
-            string url = "https://api.mch.weixin.qq.com/mmpaymkttransfers/sendredpack";
-            //本地或者服务器的证书位置（证书在微信支付申请成功发来的通知邮件中）
-            string cert = tenPayCertPath;
-            //私钥（在安装证书时设置）
-            string password = mchId;
+        //    //发红包接口地址
+        //    string url = "https://api.mch.weixin.qq.com/mmpaymkttransfers/sendredpack";
+        //    //本地或者服务器的证书位置（证书在微信支付申请成功发来的通知邮件中）
+        //    string cert = tenPayCertPath;
+        //    //私钥（在安装证书时设置）
+        //    string password = mchId;
 
-            //调用证书
-            X509Certificate2 cer = new X509Certificate2(cert, password, X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.MachineKeySet);
-            ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);
-            //X509Certificate cer = new X509Certificate(cert, password);
+        //    //调用证书
+        //    X509Certificate2 cer = new X509Certificate2(cert, password, X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.MachineKeySet);
+        //    ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);
+        //    //X509Certificate cer = new X509Certificate(cert, password);
 
-            #region 发起post请求
-            HttpWebRequest webrequest = (HttpWebRequest)HttpWebRequest.Create(url);
-            webrequest.ClientCertificates.Add(cer);
-            webrequest.Method = "post";
+        //    #region 发起post请求
+        //    HttpWebRequest webrequest = (HttpWebRequest)HttpWebRequest.Create(url);
+        //    webrequest.ClientCertificates.Add(cer);
+        //    webrequest.Method = "post";
 
 
-            byte[] postdatabyte = Encoding.UTF8.GetBytes(data);
-            webrequest.ContentLength = postdatabyte.Length;
-            Stream stream = webrequest.GetRequestStream();
-            stream.Write(postdatabyte, 0, postdatabyte.Length);
-            stream.Close();
+        //    byte[] postdatabyte = Encoding.UTF8.GetBytes(data);
+        //    webrequest.ContentLength = postdatabyte.Length;
+        //    Stream stream = webrequest.GetRequestStream();
+        //    stream.Write(postdatabyte, 0, postdatabyte.Length);
+        //    stream.Close();
 
-            HttpWebResponse httpWebResponse = (HttpWebResponse)webrequest.GetResponse();
-            StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream());
-            string responseContent = streamReader.ReadToEnd();
-            #endregion
+        //    HttpWebResponse httpWebResponse = (HttpWebResponse)webrequest.GetResponse();
+        //    StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream());
+        //    string responseContent = streamReader.ReadToEnd();
+        //    #endregion
 
-            XmlDocument doc = new XmlDocument();
-            doc.LoadXml(responseContent);
+        //    XmlDocument doc = new XmlDocument();
+        //    doc.LoadXml(responseContent);
 
-            XDocument xDoc = XDocument.Load(responseContent);
+        //    XDocument xDoc = XDocument.Load(responseContent);
 
-            NormalRedPackResult normalReturn = new NormalRedPackResult
-            {
-                err_code = "",
-                err_code_des = ""
-            };
+        //    NormalRedPackResult normalReturn = new NormalRedPackResult
+        //    {
+        //        err_code = "",
+        //        err_code_des = ""
+        //    };
 
-            if (doc.SelectSingleNode("/xml/return_code") != null)
-            {
-                normalReturn.return_code = (doc.SelectSingleNode("/xml/return_code").InnerText.ToUpper() == "SUCCESS");
-            }
-            if (doc.SelectSingleNode("/xml/return_msg") != null)
-            {
-                normalReturn.return_msg = doc.SelectSingleNode("/xml/return_msg").InnerText;
-            }
+        //    if (doc.SelectSingleNode("/xml/return_code") != null)
+        //    {
+        //        normalReturn.return_code = (doc.SelectSingleNode("/xml/return_code").InnerText.ToUpper() == "SUCCESS");
+        //    }
+        //    if (doc.SelectSingleNode("/xml/return_msg") != null)
+        //    {
+        //        normalReturn.return_msg = doc.SelectSingleNode("/xml/return_msg").InnerText;
+        //    }
 
-            if (normalReturn.return_code == true)
-            {
-                //redReturn.sign = doc.SelectSingleNode("/xml/sign").InnerText;
-                if (doc.SelectSingleNode("/xml/result_code") != null)
-                {
-                    normalReturn.result_code = (doc.SelectSingleNode("/xml/result_code").InnerText.ToUpper() == "SUCCESS");
-                }
+        //    if (normalReturn.return_code == true)
+        //    {
+        //        //redReturn.sign = doc.SelectSingleNode("/xml/sign").InnerText;
+        //        if (doc.SelectSingleNode("/xml/result_code") != null)
+        //        {
+        //            normalReturn.result_code = (doc.SelectSingleNode("/xml/result_code").InnerText.ToUpper() == "SUCCESS");
+        //        }
 
-                if (normalReturn.result_code == true)
-                {
-                    if (doc.SelectSingleNode("/xml/mch_billno") != null)
-                    {
-                        normalReturn.mch_billno = doc.SelectSingleNode("/xml/mch_billno").InnerText;
-                    }
-                    if (doc.SelectSingleNode("/xml/mch_id") != null)
-                    {
-                        normalReturn.mch_id = doc.SelectSingleNode("/xml/mch_id").InnerText;
-                    }
-                    if (doc.SelectSingleNode("/xml/wxappid") != null)
-                    {
-                        normalReturn.wxappid = doc.SelectSingleNode("/xml/wxappid").InnerText;
-                    }
-                    if (doc.SelectSingleNode("/xml/re_openid") != null)
-                    {
-                        normalReturn.re_openid = doc.SelectSingleNode("/xml/re_openid").InnerText;
-                    }
-                    if (doc.SelectSingleNode("/xml/total_amount") != null)
-                    {
-                        normalReturn.total_amount = doc.SelectSingleNode("/xml/total_amount").InnerText;
-                    }
-                    if (doc.SelectSingleNode("/xml/send_time") != null)
-                    {
-                        normalReturn.send_time = doc.SelectSingleNode("/xml/send_time").InnerText;
-                    }
-                    if (doc.SelectSingleNode("/xml/send_listid") != null)
-                    {
-                        normalReturn.send_listid = doc.SelectSingleNode("/xml/send_listid").InnerText;
-                    }
-                }
-                else
-                {
-                    if (doc.SelectSingleNode("/xml/err_code") != null)
-                    {
-                        normalReturn.err_code = doc.SelectSingleNode("/xml/err_code").InnerText;
-                    }
-                    if (doc.SelectSingleNode("/xml/err_code_des") != null)
-                    {
-                        normalReturn.err_code_des = doc.SelectSingleNode("/xml/err_code_des").InnerText;
-                    }
-                }
-            }
+        //        if (normalReturn.result_code == true)
+        //        {
+        //            if (doc.SelectSingleNode("/xml/mch_billno") != null)
+        //            {
+        //                normalReturn.mch_billno = doc.SelectSingleNode("/xml/mch_billno").InnerText;
+        //            }
+        //            if (doc.SelectSingleNode("/xml/mch_id") != null)
+        //            {
+        //                normalReturn.mch_id = doc.SelectSingleNode("/xml/mch_id").InnerText;
+        //            }
+        //            if (doc.SelectSingleNode("/xml/wxappid") != null)
+        //            {
+        //                normalReturn.wxappid = doc.SelectSingleNode("/xml/wxappid").InnerText;
+        //            }
+        //            if (doc.SelectSingleNode("/xml/re_openid") != null)
+        //            {
+        //                normalReturn.re_openid = doc.SelectSingleNode("/xml/re_openid").InnerText;
+        //            }
+        //            if (doc.SelectSingleNode("/xml/total_amount") != null)
+        //            {
+        //                normalReturn.total_amount = doc.SelectSingleNode("/xml/total_amount").InnerText;
+        //            }
+        //            if (doc.SelectSingleNode("/xml/send_time") != null)
+        //            {
+        //                normalReturn.send_time = doc.SelectSingleNode("/xml/send_time").InnerText;
+        //            }
+        //            if (doc.SelectSingleNode("/xml/send_listid") != null)
+        //            {
+        //                normalReturn.send_listid = doc.SelectSingleNode("/xml/send_listid").InnerText;
+        //            }
+        //        }
+        //        else
+        //        {
+        //            if (doc.SelectSingleNode("/xml/err_code") != null)
+        //            {
+        //                normalReturn.err_code = doc.SelectSingleNode("/xml/err_code").InnerText;
+        //            }
+        //            if (doc.SelectSingleNode("/xml/err_code_des") != null)
+        //            {
+        //                normalReturn.err_code_des = doc.SelectSingleNode("/xml/err_code_des").InnerText;
+        //            }
+        //        }
+        //    }
 
-            return normalReturn;
-        }
+        //    return normalReturn;
+        //}
+        #endregion
 
 
         /// <summary>

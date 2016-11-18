34681d37491e46f63f6f943200b66d6dcc5607ce
算神 <lishewen@gmail.com>
2016-11-18 15:12:50 +0800

    重写WxJsonResult.ToString()方法，输出带格式的内容

--- src/Senparc.Weixin/Senparc.Weixin/Entities/JsonResult/WxJsonResult.cs
+++ src/Senparc.Weixin/Senparc.Weixin/Entities/JsonResult/WxJsonResult.cs
@@ -15,47 +15,63 @@
 
     修改标识：Senparc - 20150706
     修改描述：调整位置，去除MP下的WxJsonResult
+    
+    修改标识：Senparc - 20161108
+    修改描述：重写ToString()方法，快捷输出结果
+
 ----------------------------------------------------------------*/
 
 using System;
 
 namespace Senparc.Weixin.Entities
 {
-    public interface IJsonResult
-    {
-        string errmsg { get; set; }
-        object P2PData { get; set; }
-    }
+	public interface IJsonResult
+	{
+		string errmsg { get; set; }
+		object P2PData { get; set; }
+	}
 
-    public interface IWxJsonResult : IJsonResult
-    {
-        ReturnCode errcode { get; set; }
-    }
+	public interface IWxJsonResult : IJsonResult
+	{
+		ReturnCode errcode { get; set; }
+	}
 
-    /// <summary>
-    /// 公众号JSON返回结果（用于菜单接口等）
-    /// </summary>
-    public class WxJsonResult : IWxJsonResult
-    {
-        public ReturnCode errcode { get; set; }
-        public string errmsg { get; set; }
-        /// <summary>
-        /// 为P2P返回结果做准备
-        /// </summary>
-        public virtual object P2PData { get; set; }
-        //public ReturnCode ReturnCode
-        //{
-        //    get
-        //    {
-        //        try
-        //        {
-        //            return (ReturnCode) errorcode;
-        //        }
-        //        catch
-        //        {
-        //            return ReturnCode.系统繁忙;//如果有“其他错误”的话可以指向其他错误
-        //        }
-        //    }
-        //}
-        }
+	/// <summary>
+	/// 公众号JSON返回结果（用于菜单接口等）
+	/// </summary>
+	public class WxJsonResult : IWxJsonResult
+	{
+		public WxJsonResult BaseResult
+		{
+			get { return this; }
+		}
+
+		public ReturnCode errcode { get; set; }
+		public string errmsg { get; set; }
+		/// <summary>
+		/// 为P2P返回结果做准备
+		/// </summary>
+		public virtual object P2PData { get; set; }
+
+		public override string ToString()
+		{
+			return string.Format("WxJsonResult：{{errcode:'{0}',errcode_name:'{1}',errmsg:'{2}'}}",
+				(int)errcode, errcode.ToString(), errmsg);
+		}
+
+		//public ReturnCode ReturnCode
+		//{
+		//    get
+		//    {
+		//        try
+		//        {
+		//            return (ReturnCode) errorcode;
+		//        }
+		//        catch
+		//        {
+		//            return ReturnCode.系统繁忙;//如果有“其他错误”的话可以指向其他错误
+		//        }
+		//    }
+		//}
+	}
 }

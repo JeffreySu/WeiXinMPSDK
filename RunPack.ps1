67d6cc904872d9456b5de9af13cd850b4f01b6c0
算神 <lishewen@msn.com>
2016-11-18 16:27:58 +0800

    TFS2017打包脚本

--- RunPack.ps1
+++ RunPack.ps1
@@ -1,4 +1,4 @@
-dotnet pack src\Senparc.Weixin\Senparc.Weixin -c release
-dotnet pack src\Senparc.Weixin.MP\Senparc.Weixin.MP -c release
-dotnet pack src\Senparc.Weixin.MP.MvcExtension\Senparc.Weixin.MP.MvcExtension -c release
-dotnet pack src\Senparc.Weixin.QY\Senparc.Weixin.QY -c release
+dotnet pack src\Senparc.Weixin\Senparc.Weixin\Senparc.Weixin.csproj -c release
+dotnet pack src\Senparc.Weixin.MP\Senparc.Weixin.MP\Senparc.Weixin.MP.csproj -c release
+dotnet pack src\Senparc.Weixin.MP.MvcExtension\Senparc.Weixin.MP.MvcExtension\Senparc.Weixin.MP.MvcExtension.csproj -c release
+dotnet pack src\Senparc.Weixin.QY\Senparc.Weixin.QY\Senparc.Weixin.QY.csproj -c release

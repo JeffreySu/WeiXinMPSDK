1c9e7506ac4a6fe1238a6194c2aaf7ea909c5182
yfu <yfu@senparc.com>
2016-12-26 17:14:41 +0800

    MP 增加 MicropayResult

--- src/Senparc.Weixin.MP/Senparc.Weixin.MP/TenPayLibV3/TenPayV3/TenPayV3Result.cs
+++ src/Senparc.Weixin.MP/Senparc.Weixin.MP/TenPayLibV3/TenPayV3/TenPayV3Result.cs
@@ -20,7 +20,7 @@
     修改描述：v14.3.111 处理UnifiedorderResult数据处理问题
 
     修改标识：Senparc - 20161226
-    修改描述：v14.3.112 增加OrderQueryResult,CloseOrderResult,RefundQuery,ShortUrlResult,ReverseResult
+    修改描述：v14.3.112 增加OrderQueryResult,CloseOrderResult,RefundQuery,ShortUrlResult,ReverseResult,MicropayResult
 
 ----------------------------------------------------------------*/
 
@@ -545,6 +545,112 @@ namespace Senparc.Weixin.MP.TenPayLibV3
             if (base.IsReturnCodeSuccess())
             {
                 recall = GetXmlValue("recall") ?? "";
+            }
+        }
+    }
+
+    /// <summary>
+    /// 刷卡支付
+    /// 提交被扫支付
+    /// </summary>
+    public class MicropayResult : Result
+    {
+        /// <summary>
+        /// 调用接口提交的终端设备号
+        /// </summary>
+        public string device_info { get; set; }
+        /// <summary>
+        /// 用户在商户appid 下的唯一标识
+        /// </summary>
+        public string openid { get; set; }
+
+        /// <summary>
+        /// 用户是否关注公众账号，仅在公众账号类型支付有效，取值范围：Y或N;Y-关注;N-未关注
+        /// </summary>
+        public string is_subscribe { get; set; }
+
+        /// <summary>
+        /// 支付类型为MICROPAY(即扫码支付)
+        /// </summary>
+        public string trade_type { get; set; }
+
+        /// <summary>
+        /// 银行类型，采用字符串类型的银行标识
+        /// </summary>
+        public string bank_type { get; set; }
+
+        /// <summary>
+        /// 符合ISO 4217标准的三位字母代码，默认人民币：CNY
+        /// </summary>
+        public string fee_type { get; set; }
+
+        /// <summary>
+        /// 订单总金额，单位为分，只能为整数
+        /// </summary>
+        public string total_fee { get; set; }
+
+        /// <summary>
+        /// 应结订单金额=订单金额-非充值代金券金额，应结订单金额<=订单金额
+        /// </summary>
+        public string settlement_total_fee { get; set; }
+
+        /// <summary>
+        /// “代金券”金额<=订单金额，订单金额-“代金券”金额=现金支付金额
+        /// </summary>
+        public string coupon_fee { get; set; }
+
+        /// <summary>
+        /// 符合ISO 4217标准的三位字母代码，默认人民币：CNY
+        /// </summary>
+        public string cash_fee_type { get; set; }
+
+        /// <summary>
+        /// 订单现金支付金额
+        /// </summary>
+        public string cash_fee { get; set; }
+
+        /// <summary>
+        /// 微信支付订单号
+        /// </summary>
+        public string transaction_id { get; set; }
+
+        /// <summary>
+        /// 商户系统的订单号，与请求一致
+        /// </summary>
+        public string out_trade_no { get; set; }
+
+        /// <summary>
+        /// 商家数据包，原样返回
+        /// </summary>
+        public string attach { get; set; }
+
+        /// <summary>
+        /// 订单生成时间，格式为yyyyMMddHHmmss，如2009年12月25日9点10分10秒表示为20091225091010
+        /// </summary>
+        public string time_end { get; set; }
+
+        public MicropayResult(string resultXml) : base(resultXml)
+        {
+            if (base.IsReturnCodeSuccess())
+            {
+                device_info = GetXmlValue("device_info") ?? "";
+                if (base.IsResultCodeSuccess())
+                {
+                    openid = GetXmlValue("openid") ?? "";
+                    is_subscribe = GetXmlValue("is_subscribe") ?? "";
+                    trade_type = GetXmlValue("trade_type") ?? "";
+                    bank_type = GetXmlValue("bank_type") ?? "";
+                    fee_type = GetXmlValue("fee_type") ?? "";
+                    total_fee = GetXmlValue("total_fee") ?? "";
+                    settlement_total_fee = GetXmlValue("settlement_total_fee") ?? "";
+                    coupon_fee = GetXmlValue("coupon_fee") ?? "";
+                    cash_fee_type = GetXmlValue("cash_fee_type") ?? "";
+                    cash_fee = GetXmlValue("cash_fee") ?? "";
+                    transaction_id = GetXmlValue("transaction_id") ?? "";
+                    out_trade_no = GetXmlValue("out_trade_no") ?? "";
+                    attach = GetXmlValue("attach") ?? "";
+                    time_end = GetXmlValue("time_end") ?? "";
+                }
             }
         }
     }

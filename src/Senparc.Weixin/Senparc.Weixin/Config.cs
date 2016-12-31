57bd4fa01626c7706f7748fa9d24c2e50658a617
Jeffrey Su <www.jeffrey.su@gmail.com>
2016-12-31 23:02:14 +0800

    modify

--- src/Senparc.Weixin/Senparc.Weixin/Config.cs
+++ src/Senparc.Weixin/Senparc.Weixin/Config.cs
@@ -30,7 +30,7 @@ namespace Senparc.Weixin
         /// 请求超时设置（以毫秒为单位），默认为10秒。
         /// 说明：此处常量专为提供给方法的参数的默认值，不是方法内所有请求的默认超时时间。
         /// </summary>
-        public const int TIME_OUT = 10000;//TODO:需要考虑分布式的情况，后期需要储存在缓存中
+        public const int TIME_OUT = 10000;
 
         private static bool _isDebug = false;//TODO:需要考虑分布式的情况，后期需要储存在缓存中
 
@@ -61,11 +61,11 @@ namespace Senparc.Weixin
         /// <summary>
         /// JavaScriptSerializer 类接受的 JSON 字符串的最大长度
         /// </summary>
-        public static int MaxJsonLength = int.MaxValue;
+        public static int MaxJsonLength = int.MaxValue;//TODO:需要考虑分布式的情况，后期需要储存在缓存中
 
         /// <summary>
         /// 默认缓存键的第一级命名空间，默认值：DefaultCache
         /// </summary>
-        public static string DefaultCacheNamespace = "DefaultCache";
+        public static string DefaultCacheNamespace = "DefaultCache";//TODO:需要考虑分布式的情况，后期需要储存在缓存中,或进行全局配置
     }
 }

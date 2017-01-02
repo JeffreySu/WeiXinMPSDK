98529fde80d5b1078d2737346693c311634ba6dc
Jeffrey Su <www.jeffrey.su@gmail.com>
2017-01-02 17:14:11 +0800

    优化Demo代码和结构

--- src/Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample.CommonService/EventService.cs
+++ src/Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample.CommonService/EventService.cs
@@ -9,19 +9,30 @@
 ----------------------------------------------------------------*/
 
 using System;
+using System.Configuration;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web;
+using Senparc.Weixin.Exceptions;
 using Senparc.Weixin.MP.Entities;
 using Senparc.Weixin.MP.Helpers;
+using Senparc.Weixin.MP.Sample.CommonService.TemplateMessage;
 
 namespace Senparc.Weixin.MP.Sample.CommonService
 {
     /// <summary>
-    /// 事件处理程序，此代码的简化MessageHandler方法已由/CustomerMessageHandler/CustomerMessageHandler_Event.cs完成，
-    /// 此文件不再更新。
+    /// 全局微信事件有关的处理程序
     /// </summary>
     public class EventService
     {
+        /// <summary>
+        /// 微信MessageHandler事件处理，此代码的简化MessageHandler方法已由/CustomerMessageHandler/CustomerMessageHandler_Event.cs完成，
+        /// 此方法不再更新
+        /// </summary>
+        /// <param name="requestMessage"></param>
+        /// <returns></returns>
         public ResponseMessageBase GetResponseMessage(RequestMessageEventBase requestMessage)
         {
             ResponseMessageBase responseMessage = null;
@@ -67,6 +78,74 @@ namespace Senparc.Weixin.MP.Sample.CommonService
             }
 
             return responseMessage;         
+        }
+
+        public void ConfigOnWeixinExceptionFunc(WeixinException ex)
+        {
+            try
+            {
+                Task.Factory.StartNew(async () =>
+                {
+                    var appId = ConfigurationManager.AppSettings["WeixinAppId"];
+                    string openId = "olPjZjsXuQPJoV0HlruZkNzKc91E";//收到通知的管理员OpenId
+                    var host = "A1 / AccessTokenOrAppId：" + (ex.AccessTokenOrAppId ?? "null");
+                    string service = null;
+                    string message = null;
+                    var status = ex.GetType().Name;
+                    var remark = "\r\n这是一条通过OnWeixinExceptionFunc事件发送的异步模板消息";
+                    string url = "https://github.com/JeffreySu/WeiXinMPSDK/blob/24aca11630bf833f6a4b6d36dce80c5b171281d3/src/Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/Global.asax.cs#L246";//需要点击打开的URL
+
+                    var sendTemplateMessage = true;
+
+                    if (ex is ErrorJsonResultException)
+                    {
+                        var jsonEx = (ErrorJsonResultException)ex;
+                        service = jsonEx.Url;
+                        message = jsonEx.Message;
+
+                        //需要忽略的类型
+                        var ignoreErrorCodes = new[]
+                        {
+                                ReturnCode.获取access_token时AppSecret错误或者access_token无效,
+                                ReturnCode.template_id不正确,
+                                ReturnCode.缺少access_token参数,
+                                ReturnCode.api功能未授权,
+                                ReturnCode.用户未授权该api,
+                                ReturnCode.参数错误invalid_parameter,
+                                ReturnCode.接口调用超过限制,
+                                //其他更多可能的情况
+                            };
+                        if (ignoreErrorCodes.Contains(jsonEx.JsonResult.errcode))
+                        {
+                            sendTemplateMessage = false;//防止无限递归，这种请款那个下不发送消息
+                        }
+
+                        //TODO:防止更多的接口自身错误导致的无限递归。
+                    }
+                    else
+                    {
+                        if (ex.Message.StartsWith("openid:"))
+                        {
+                            openId = ex.Message.Split(':')[1];//发送给指定OpenId
+                        }
+                        service = "WeixinException";
+                        message = ex.Message;
+                    }
+
+                    if (sendTemplateMessage)
+                    {
+                        int sleepSeconds = 3;
+                        Thread.Sleep(sleepSeconds * 1000);
+                        var data = new WeixinTemplate_ExceptionAlert(string.Format("微信发生异常（延时{0}秒）", sleepSeconds), host, service, status, message, remark);
+                        var result = await Senparc.Weixin.MP.AdvancedAPIs.TemplateApi.SendTemplateMessageAsync(appId, openId, data.TemplateId,
+                                url, data);
+                    }
+                });
+            }
+            catch (Exception e)
+            {
+                Senparc.Weixin.WeixinTrace.SendCustomLog("OnWeixinExceptionFunc过程错误", e.Message);
+            }
         }
     }
 }
--- src/Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/Global.asax.cs
+++ src/Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/Global.asax.cs
@@ -17,6 +17,7 @@ using Senparc.Weixin.Cache.Redis;
 using Senparc.Weixin.Exceptions;
 using Senparc.Weixin.MP.CommonAPIs;
 using Senparc.Weixin.MP.Containers;
+using Senparc.Weixin.MP.Sample.CommonService;
 using Senparc.Weixin.MP.Sample.CommonService.TemplateMessage;
 using Senparc.Weixin.MP.TenPayLib;
 using Senparc.Weixin.MP.TenPayLibV3;
@@ -248,70 +249,8 @@ namespace Senparc.Weixin.MP.Sample
                 //加入每次触发WeixinExceptionLog后需要执行的代码
 
                 //发送模板消息给管理员
-                try
-                {
-                    Task.Factory.StartNew(async () =>
-                    {
-                        var appId = ConfigurationManager.AppSettings["WeixinAppId"];
-                        string openId = "olPjZjsXuQPJoV0HlruZkNzKc91E";//收到通知的管理员OpenId
-                        var host = "A1 / AccessTokenOrAppId：" + (ex.AccessTokenOrAppId ?? "null");
-                        string service = null;
-                        string message = null;
-                        var status = ex.GetType().Name;
-                        var remark = "\r\n这是一条通过OnWeixinExceptionFunc事件发送的异步模板消息";
-                        string url = "https://github.com/JeffreySu/WeiXinMPSDK/blob/24aca11630bf833f6a4b6d36dce80c5b171281d3/src/Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/Global.asax.cs#L246";//需要点击打开的URL
-
-                        var sendTemplateMessage = true;
-
-                        if (ex is ErrorJsonResultException)
-                        {
-                            var jsonEx = (ErrorJsonResultException)ex;
-                            service = jsonEx.Url;
-                            message = jsonEx.Message;
-
-                            //需要忽略的类型
-                            var ignoreErrorCodes = new[]
-                            {
-                                ReturnCode.获取access_token时AppSecret错误或者access_token无效,
-                                ReturnCode.template_id不正确,
-                                ReturnCode.缺少access_token参数,
-                                ReturnCode.api功能未授权,
-                                ReturnCode.用户未授权该api,
-                                ReturnCode.参数错误invalid_parameter,
-                                ReturnCode.接口调用超过限制,
-                                //其他更多可能的情况
-                            };
-                            if (ignoreErrorCodes.Contains(jsonEx.JsonResult.errcode))
-                            {
-                                sendTemplateMessage = false;//防止无限递归，这种请款那个下不发送消息
-                            }
-
-                            //TODO:防止更多的接口自身错误导致的无限递归。
-                        }
-                        else
-                        {
-                            if (ex.Message.StartsWith("openid:"))
-                            {
-                                openId = ex.Message.Split(':')[1];//发送给指定OpenId
-                            }
-                            service = "WeixinException";
-                            message = ex.Message;
-                        }
-
-                        if (sendTemplateMessage)
-                        {
-                            int sleepSeconds = 3;
-                            Thread.Sleep(sleepSeconds * 1000);
-                            var data = new WeixinTemplate_ExceptionAlert(string.Format("微信发生异常（延时{0}秒）", sleepSeconds), host, service, status, message, remark);
-                            var result = await Senparc.Weixin.MP.AdvancedAPIs.TemplateApi.SendTemplateMessageAsync(appId, openId, data.TemplateId,
-                                    url, data);
-                        }
-                    });
-                }
-                catch (Exception e)
-                {
-                    Senparc.Weixin.WeixinTrace.SendCustomLog("OnWeixinExceptionFunc过程错误", e.Message);
-                }
+                var eventService = new EventService();
+                eventService.ConfigOnWeixinExceptionFunc(ex);
             };
         }
     }

e5a852328dd22647fd218e299b57b7307a3729db
Jeffrey Su <www.jeffrey.su@gmail.com>
2017-01-07 00:35:59 +0800

    优化小程序Demo

--- src/Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/Controllers/WxOpen/WxOpenController.cs
+++ src/Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/Controllers/WxOpen/WxOpenController.cs
@@ -2,7 +2,7 @@
 using System.IO;
 using System.Web.Configuration;
 using System.Web.Mvc;
-using Senparc.Weixin.MP.Entities.Request;
+using Senparc.Weixin.WxOpen.Entities.Request;
 using Senparc.Weixin.MP.MvcExtension;
 using Senparc.Weixin.MP.Sample.CommonService.WxOpenMessageHandler;
 

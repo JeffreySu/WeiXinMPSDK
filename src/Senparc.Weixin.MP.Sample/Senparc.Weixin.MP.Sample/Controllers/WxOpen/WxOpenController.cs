22c7a6f6431b3bc632575a24d371cab6aa2291c9
Jeffrey Su <www.jeffrey.su@gmail.com>
2017-01-06 22:57:04 +0800

    调试小程序MessageHandler

--- src/Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/Controllers/WxOpen/WxOpenController.cs
+++ src/Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/Controllers/WxOpen/WxOpenController.cs
@@ -23,7 +23,6 @@ namespace Senparc.Weixin.MP.Sample.Controllers.WxOpen
         readonly Func<string> _getRandomFileName = () => DateTime.Now.ToString("yyyyMMdd-HHmmss") + Guid.NewGuid().ToString("n").Substring(0, 6);
 
 
-
         [HttpGet]
         [ActionName("Index")]
         public ActionResult Get(PostModel postModel, string echostr)
@@ -35,7 +34,7 @@ namespace Senparc.Weixin.MP.Sample.Controllers.WxOpen
             else
             {
                 return Content("failed:" + postModel.Signature + "," + MP.CheckSignature.GetSignature(postModel.Timestamp, postModel.Nonce, Token) + "。" +
-                    "如果你在浏览器中看到这句话，说明此地址可以被作为微信公众账号后台的Url，请注意保持Token一致。");
+                    "如果你在浏览器中看到这句话，说明此地址可以被作为微信小程序后台的Url，请注意保持Token一致。");
             }
         }
 

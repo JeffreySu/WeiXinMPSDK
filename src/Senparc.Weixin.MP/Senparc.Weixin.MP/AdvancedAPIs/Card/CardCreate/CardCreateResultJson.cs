ffa6db73cb320859a6d2a139386227d5452e6fcd
Jeffrey Su <www.jeffrey.su@gmail.com>
2017-01-10 22:08:41 +0800

    CreateQRResultJson添加url和show_qrcode_url属性

--- src/Senparc.Weixin.MP/Senparc.Weixin.MP/AdvancedAPIs/Card/CardCreate/CardCreateResultJson.cs
+++ src/Senparc.Weixin.MP/Senparc.Weixin.MP/AdvancedAPIs/Card/CardCreate/CardCreateResultJson.cs
@@ -25,8 +25,12 @@
     修改描述：添加上传logo返回结果
  
     创建标识：Senparc - 20160808
-    创建描述：添加创建标识：Senparc - 20160520
     创建描述：添加Card_UpdateResultJson
+
+    创建标识：Senparc - 20170110
+    创建描述：CreateQRResultJson添加url和show_qrcode_url属性
+
+
 ----------------------------------------------------------------*/
 
 using System.CodeDom;
@@ -264,9 +268,17 @@ namespace Senparc.Weixin.MP.AdvancedAPIs.Card
     public class CreateQRResultJson : WxJsonResult
     {
         /// <summary>
-        /// 获取的二维码ticket，凭借此ticket 可以在有效时间内换取二维码。
+        /// 获取的二维码ticket，凭借此ticket调用通过ticket换取二维码接口可以在有效时间内换取二维码。
         /// </summary>
         public string ticket { get; set; }
+        /// <summary>
+        /// 二维码图片解析后的地址，开发者可根据该地址自行生成需要的二维码图片
+        /// </summary>
+        public string url { get; set; }
+        /// <summary>
+        /// 二维码显示地址，点击后跳转二维码页面
+        /// </summary>
+        public string show_qrcode_url { get; set; }
     }
 
     /// <summary>

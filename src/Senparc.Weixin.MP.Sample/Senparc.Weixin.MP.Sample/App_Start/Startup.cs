02026b8676709c9e35b134ecfb4a930e6a4941de
Jeffrey Su <www.jeffrey.su@gmail.com>
2017-01-26 00:46:42 +0800

    add OwinStartup class

---
+++ src/Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/App_Start/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Senparc.Weixin.MP.Sample.App_Start.Startup))]
+
+namespace Senparc.Weixin.MP.Sample.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // 有关如何配置应用程序的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}

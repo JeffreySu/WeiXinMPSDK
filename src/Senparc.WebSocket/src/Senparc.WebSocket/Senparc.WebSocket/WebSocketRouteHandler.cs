cc07741d8f5e4e047e0bd507bb8570ae52e0d5a8
Jeffrey Su <www.jeffrey.su@gmail.com>
2017-01-26 13:29:57 +0800

    add Handlers for WebSocket

---
+++ src/Senparc.WebSocket/src/Senparc.WebSocket/Senparc.WebSocket/WebSocketRouteHandler.cs
@@ -0,0 +1,33 @@
+﻿/*----------------------------------------------------------------
+    Copyright (C) 2017 Senparc
+
+    文件名：WebSocketRouteHandler.cs
+    文件功能描述：WebSocketRouteHandler，处理WebSocket请求
+
+
+    创建标识：Senparc - 20170126
+
+----------------------------------------------------------------*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Routing;
+
+namespace Senparc.WebSocket
+{
+    /*Handler参考：http://www.cnblogs.com/lookbs/p/MVC-IMG.html */
+
+    /// <summary>
+    /// WebSocketHansler，处理WebSocket请求
+    /// </summary>
+    public class WebSocketRouteHandler: IRouteHandler
+    {
+        public IHttpHandler GetHttpHandler(RequestContext requestContext)
+        {
+            return new WebSocketHandler(requestContext);
+        }
+    }
+}

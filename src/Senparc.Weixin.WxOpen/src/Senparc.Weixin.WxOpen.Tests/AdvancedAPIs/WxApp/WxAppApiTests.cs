022a3242a77d4d0161217aeec0f52fbba1654ac5
Jeffrey Su <www.jeffrey.su@gmail.com>
2017-01-29 19:37:47 +0800

    通过小程序二维码单元测试

---
+++ src/Senparc.Weixin.WxOpen/src/Senparc.Weixin.WxOpen.Tests/AdvancedAPIs/WxApp/WxAppApiTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Senparc.Weixin.WxOpen.AdvancedAPIs.WxApp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Senparc.Weixin.Entities;
+using Senparc.Weixin.MP.Test.CommonAPIs;
+
+namespace Senparc.Weixin.WxOpen.AdvancedAPIs.WxApp.Tests
+{
+    [TestClass()]
+    public class WxAppApiTests : CommonApiTest
+    {
+        [TestMethod()]
+        public void CreateWxaQrCodeTest()
+        {
+            var ms = new MemoryStream();
+            var result = WxAppApi.CreateWxaQrCode(base._appId, ms, "pages/websocket", 400);
+            Assert.AreEqual(ReturnCode.请求成功, result.errcode);
+
+            ms.Seek(0, SeekOrigin.Begin);
+            //储存图片
+
+            var filePath = "../../Config/qr.jpg";
+            File.Delete(filePath);
+            using (var fs = new FileStream(filePath, FileMode.CreateNew))
+            {
+                ms.CopyTo(fs);
+                fs.Flush();
+            }
+
+            Assert.IsTrue(File.Exists(filePath));
+        }
+    }
+}

0c346dbcfbc6622392d9552c706c929dbe9845ad
Jeffrey Su <www.jeffrey.su@gmail.com>
2017-01-30 14:42:07 +0800

    add SessionHelper，SessionContainer

---
+++ src/Senparc.Weixin.WxOpen/src/Senparc.Weixin.WxOpen/Senparc.Weixin.WxOpen/Containers/SessionContainer.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Senparc.Weixin.Containers;
+using Senparc.Weixin.WxOpen.Helpers;
+
+namespace Senparc.Weixin.WxOpen.Containers
+{
+    /// <summary>
+    /// 第三方APP信息包
+    /// </summary>
+    [Serializable]
+    public class SessionBag : BaseContainerBag
+    {
+        /// <summary>
+        /// Session的Key
+        /// </summary>
+        public string Key
+        {
+            get { return _key; }
+            set { base.SetContainerProperty(ref _key, value); }
+        }
+
+        /// <summary>
+        /// OpenId
+        /// </summary>
+        public string OpenId
+        {
+            get { return _openId; }
+            set { base.SetContainerProperty(ref _openId, value); }
+        }
+
+        /// <summary>
+        /// SessionKey
+        /// </summary>
+        public string SessionKey
+        {
+            get { return _sessionKey; }
+            set { base.SetContainerProperty(ref _sessionKey, value); }
+        }
+
+        /// <summary>
+        /// 过期时间
+        /// </summary>
+        public DateTime ExpireTime
+        {
+            get { return _expireTime; }
+            set { base.SetContainerProperty(ref _expireTime, value); }
+        }
+
+        private string _key;
+        private string _openId;
+        private string _sessionKey;
+        private DateTime _expireTime;
+
+        /// <summary>
+        /// ComponentBag
+        /// </summary>
+        public SessionBag()
+        {
+            ExpireTime = DateTime.Now.AddDays(2);//有效期2天
+        }
+    }
+
+
+    /// <summary>
+    /// 3rdSession容器
+    /// </summary>
+    public class SessionContainer : BaseContainer<SessionBag>
+    {
+        #region 同步方法
+
+        /// <summary>
+        /// 获取Session
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static SessionBag GetSession(string key)
+        {
+            var bag = TryGetItem(key);
+            if (bag == null)
+            {
+                return null;
+            }
+
+            if (bag.ExpireTime < DateTime.Now)
+            {
+                //已经过期
+                Cache.RemoveFromCache(key);
+                return null;
+            }
+
+            bag.ExpireTime = DateTime.Now.AddDays(2);//滚动过期时间
+            Cache.UpdateContainerBag(key, bag);
+            return bag;
+        }
+
+        /// <summary>
+        /// 更新或插入SessionBag
+        /// </summary>
+        /// <param name="key">如果留空，则新建一条记录</param>
+        /// <param name="openId">OpenId</param>
+        /// <param name="sessionKey">SessionKey</param>
+        /// <returns></returns>
+        public static SessionBag UpdateSession(string key, string openId, string sessionKey)
+        {
+            key = key ?? SessionHelper.GetNewThirdSessionName();
+            var sessionBag = new SessionBag()
+            {
+                Key = key,
+                OpenId = openId,
+                SessionKey = sessionKey
+            };
+
+            Cache.InsertToCache(key, sessionBag);
+            return sessionBag;
+        }
+
+        #endregion
+
+    }
+}
---
+++ src/Senparc.Weixin.WxOpen/src/Senparc.Weixin.WxOpen/Senparc.Weixin.WxOpen/Helpers/SessionHelper.cs
@@ -0,0 +1,39 @@
+﻿/*----------------------------------------------------------------
+    Copyright (C) 2017 Senparc
+    
+    文件名：SessionHelper.cs
+    文件功能描述：Session帮助类
+    
+    
+    创建标识：Senparc - 20170129
+----------------------------------------------------------------*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.Weixin.WxOpen.Helpers
+{
+    /// <summary>
+    /// SessionHelper
+    /// </summary>
+    public class SessionHelper
+    {
+        /// <summary>
+        /// 获取新的3rdSession名称
+        /// </summary>
+        /// <param name="bSize">Session名称长度，单位：B，建议为16的倍数，通常情况下16B已经够用（32位GUID字符串）</param>
+        /// <returns></returns>
+        public static string GetNewThirdSessionName(int bSize = 16)
+        {
+            string key = null;
+            for (int i = 0; i < bSize / 16; i++)
+            {
+                key += Guid.NewGuid().ToString("n");
+            }
+            return key;
+        }
+    }
+}

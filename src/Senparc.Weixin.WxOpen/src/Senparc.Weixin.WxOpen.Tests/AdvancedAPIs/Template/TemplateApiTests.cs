3099e0d000a7d9c8e79e24cdc2e491167f66dc78
Jeffrey Su <www.jeffrey.su@gmail.com>
2017-02-01 16:22:27 +0800

    完善单元测试框架，针对小程序的单元测试优化

---
+++ src/Senparc.Weixin.WxOpen/src/Senparc.Weixin.WxOpen.Tests/AdvancedAPIs/Template/TemplateApiTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Senparc.Weixin.WxOpen.AdvancedAPIs.Template;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.Weixin.WxOpen.AdvancedAPIs.Template.Tests
+{
+    [TestClass()]
+    public class TemplateApiTests
+    {
+        [TestMethod()]
+        public void SendTemplateMessageTest()
+        {
+            Assert.Fail();
+        }
+    }
+}

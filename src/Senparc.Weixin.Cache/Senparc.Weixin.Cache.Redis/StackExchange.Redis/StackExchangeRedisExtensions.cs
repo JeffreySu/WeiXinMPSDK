5d83648f3bc8b1f1fa6f0b03ae0b6c16c79359ce
Jeffrey Su <www.jeffrey.su@gmail.com>
2017-02-04 21:52:49 +0800

    Redis 回到V1.1.0，采用二进制序列化

--- src/Senparc.Weixin.Cache/Senparc.Weixin.Cache.Redis/StackExchange.Redis/StackExchangeRedisExtensions.cs
+++ src/Senparc.Weixin.Cache/Senparc.Weixin.Cache.Redis/StackExchange.Redis/StackExchangeRedisExtensions.cs
@@ -68,18 +68,20 @@ namespace Senparc.Weixin.Cache.Redis
             }
 
             //二进制序列化方案
-            //BinaryFormatter binaryFormatter = new BinaryFormatter();
-            //using (MemoryStream memoryStream = new MemoryStream())
-            //{
-            //    binaryFormatter.Serialize(memoryStream, o);
-            //    byte[] objectDataAsStream = memoryStream.ToArray();
-            //    return objectDataAsStream;
-            //}
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                binaryFormatter.Serialize(memoryStream, o);
+                byte[] objectDataAsStream = memoryStream.ToArray();
+                return objectDataAsStream;
+            }
 
+
+            //使用JSON序列化，会在Get()方法反序列化到IContainerBag的过程中出错
             //JSON序列化方案
-            SerializerHelper serializerHelper = new SerializerHelper();
-            var jsonSetting = serializerHelper.GetJsonString(o);
-            return Encoding.UTF8.GetBytes(jsonSetting);
+            //SerializerHelper serializerHelper = new SerializerHelper();
+            //var jsonSetting = serializerHelper.GetJsonString(o);
+            //return Encoding.UTF8.GetBytes(jsonSetting);
         }
 
         /// <summary>
@@ -96,17 +98,17 @@ namespace Senparc.Weixin.Cache.Redis
             }
 
             //二进制序列化方案
-            //BinaryFormatter binaryFormatter = new BinaryFormatter();
-            //using (MemoryStream memoryStream = new MemoryStream(stream))
-            //{
-            //    T result = (T)binaryFormatter.Deserialize(memoryStream);
-            //    return result;
-            //}
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            using (MemoryStream memoryStream = new MemoryStream(stream))
+            {
+                T result = (T)binaryFormatter.Deserialize(memoryStream);
+                return result;
+            }
 
             //JSON序列化方案
-            SerializerHelper serializerHelper = new SerializerHelper();
-            T result = serializerHelper.GetObject<T>(Encoding.UTF8.GetString(stream));
-            return result;
+            //SerializerHelper serializerHelper = new SerializerHelper();
+            //T result = serializerHelper.GetObject<T>(Encoding.UTF8.GetString(stream));
+            //return result;
         }
     }
 }

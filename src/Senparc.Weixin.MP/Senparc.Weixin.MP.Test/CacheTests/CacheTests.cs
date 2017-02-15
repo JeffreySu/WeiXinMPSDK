a461993f6fb67e708ca3e8c276670c999768faa4
Jeffrey Su <www.jeffrey.su@gmail.com>
2017-02-15 18:46:17 +0800

    完善测试：https://github.com/JeffreySu/WeiXinMPSDK/issues/402 & https://github.com/JeffreySu/WeiXinMPSDK/issues/403

--- src/Senparc.Weixin.MP/Senparc.Weixin.MP.Test/CacheTests/CacheTests.cs
+++ src/Senparc.Weixin.MP/Senparc.Weixin.MP.Test/CacheTests/CacheTests.cs
@@ -6,6 +6,7 @@ using Senparc.Weixin.Cache;
 using Senparc.Weixin.Cache.Redis;
 using Senparc.Weixin.MP.AdvancedAPIs;
 using Senparc.Weixin.MP.Test.CommonAPIs;
+using System.Threading;
 
 namespace Senparc.Weixin.MP.Test.CacheTests
 {
@@ -14,18 +15,19 @@ namespace Senparc.Weixin.MP.Test.CacheTests
     {
         private void LocalCacheDeadLockTestThreadFun()
         {
-            UserApi.Info(base._appId, base._testOpenId);
+            var result = UserApi.Info(base._appId, base._testOpenId);
+            Console.WriteLine(Thread.CurrentThread.GetHashCode() + "：" + Newtonsoft.Json.JsonConvert.SerializeObject(result));
         }
 
         [TestMethod]
         public void LocalCacheDeadLockTest()
         {
-            //测试本地缓存死锁问题
+            //测试本地缓存死锁问题：https://github.com/JeffreySu/WeiXinMPSDK/issues/402
             CacheStrategyFactory.RegisterObjectCacheStrategy(() => LocalObjectCacheStrategy.Instance);//Local
 
             List<Task> taskList = new List<Task>();
             var dt1 = DateTime.Now;
-            for (int i = 0; i < 50; i++)
+            for (int i = 0; i < 200; i++)
             {
                 var lastTask = new Task(LocalCacheDeadLockTestThreadFun);
                 lastTask.Start();
@@ -39,17 +41,17 @@ namespace Senparc.Weixin.MP.Test.CacheTests
         [TestMethod]
         public void LocalCacheDeadLockInGetTest()
         {
-            //测试本地缓存死锁问题
+            //测试本地缓存死锁问题：https://github.com/JeffreySu/WeiXinMPSDK/issues/403
             CacheStrategyFactory.RegisterObjectCacheStrategy(() => LocalObjectCacheStrategy.Instance);//Local
 
             //测试死锁发生
             //Task.Factory.StartNew(() =>
             //{
             var dt1 = DateTime.Now;
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 200; i++)
             {
                 Console.WriteLine("开始循环：{0}", i);
-               var result = Senparc.Weixin.MP.AdvancedAPIs.UserApi.InfoAsync(base._appId, base._testOpenId);
+                var result = Senparc.Weixin.MP.AdvancedAPIs.UserApi.InfoAsync(base._appId, base._testOpenId);
                 Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(result.Result));
             }
 

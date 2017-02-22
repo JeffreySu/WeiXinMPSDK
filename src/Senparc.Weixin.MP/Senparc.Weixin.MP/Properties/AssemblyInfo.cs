e5603454fd7e37c698bce5b980e03e1efb85bf80
JeffreySu <www.jeffrey.su@gmail.com>
2017-02-22 19:09:33 +0800

    MP v14.3.127 重新编译并上传nuget

--- src/Senparc.Weixin.MP/Senparc.Weixin.MP/Properties/AssemblyInfo.cs
+++ src/Senparc.Weixin.MP/Senparc.Weixin.MP/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("14.3.126.*")]
+[assembly: AssemblyVersion("14.3.127.*")]
 //[assembly: AssemblyInformationalVersion("13.3.1-alpha")]
 //[assembly: AssemblyFileVersion("0.4.2.0")]

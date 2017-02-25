62da9357a9bbdc6f5c4ffc3912cf801e9febd351
JeffreySu <www.jeffrey.su@gmail.com>
2017-02-25 22:34:43 +0800

    整理文件，合并Demo中的Controller

--- src/Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/Controllers/MediaController.cs
+++ src/Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/Controllers/MediaController.cs
@@ -6,6 +6,9 @@
     
     
     创建标识：Senparc - 20150312
+    
+    修改标识：Senparc - 20150312
+    修改描述：TestUploadMediaFile() 方法专为Senparc.Weixin.MP.Test/HttpUtility/RequestUtilityTest.cs/HttpPostTest 提供上传测试目标
 ----------------------------------------------------------------*/
 
 using System;
@@ -31,6 +34,41 @@ namespace Senparc.Weixin.MP.Sample.Controllers
             AdvancedAPIs.MediaApi.Get(accessToken, mediaId, ms);
             ms.Seek(0, SeekOrigin.Begin);
             return File(ms, "audio/amr","voice.amr");
+        }
+
+        /// <summary>
+        /// 这个方法专为Senparc.Weixin.MP.Test/HttpUtility/RequestUtilityTest.cs/HttpPostTest() 提供上传测试目标
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="type"></param>
+        /// <param name="contentLength"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult TestUploadMediaFile(string token, UploadMediaFileType type, int contentLength /*, HttpPostedFileBase postedFile*/)
+        {
+            var inputStream = Request.InputStream;
+            if (contentLength != inputStream.Length)
+            {
+                return Content("ContentLength不正确，可能接收错误！");
+            }
+
+            if (token != "TOKEN")
+            {
+                return Content("TOKEN不正确！");
+            }
+
+            if (type != UploadMediaFileType.image)
+            {
+                return Content("UploadMediaFileType不正确！");
+            }
+
+            //储存文件，对比是否上传成功
+            using (FileStream ms = new FileStream(Server.MapPath("~/TestUploadMediaFile.jpg"), FileMode.OpenOrCreate))
+            {
+                inputStream.CopyTo(ms, 256);
+            }
+
+            return Content("{\"type\":\"image\",\"media_id\":\"MEDIA_ID\",\"created_at\":123456789}");
         }
     }
 }

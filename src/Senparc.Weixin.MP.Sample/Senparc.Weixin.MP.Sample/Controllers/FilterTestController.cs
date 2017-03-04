57e7d4a3cba0e83fd387fafc6c80d25359a7b42c
JeffreySu <www.jeffrey.su@gmail.com>
2017-03-04 13:23:27 +0800

    modify Demo

--- src/Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/Controllers/FilterTestController.cs
+++ src/Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/Controllers/FilterTestController.cs
@@ -34,5 +34,10 @@ namespace Senparc.Weixin.MP.Sample.Controllers
         {
             return Content("访问正常。当前地址：" + Request.Url.PathAndQuery + "<br />请点击右上角转发按钮，使用【在浏览器中打开】功能进行测试！<br />或者也可以直接在外部浏览器打开 http://sdk.weixin.senparc.com/FilterTest/Redirect 进行测试。");
         }
+
+        public ContentResult Agent()
+        {
+            return Content(Request.UserAgent);
+        }
     }
 }

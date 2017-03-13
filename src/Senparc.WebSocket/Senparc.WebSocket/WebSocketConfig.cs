c2c5e6bb083850affb8d01f1fac6528d1279ff65
JeffreySu <www.jeffrey.su@gmail.com>
2017-03-13 20:20:06 +0800

    Squashed 'src/Senparc.WebSocket/' changes from e266204..0cfd188
    0cfd188 modify
    403eb82 modify
    4ec5dfa 修改至.net 4.5.2
    2476bc6 尝试.net 4.6.2
    d7edf98 modify
    1e927d4 add Senparc.WebSocket.packages.csproj and modify .sln to link the new .csproj
    bf2836a add travis-ci
    a9ecfc8 add WebSocketMessageHandler基类
    537107a WS v0.1.1 重构WebSocket消息处理流程，创建WebSocketMessageHandler
    eeabf09 分离WebSocketConfig.OnReceiveMessage委托
    6c871cb 优化小程序Demo
    91fa175 add WebSocketRouteConfig
    dcbe637 add Handlers for WebSocket
    
    git-subtree-dir: src/Senparc.WebSocket
    git-subtree-split: 0cfd188558a468d670193f2238832d77cdd3f857

---
+++ src/Senparc.WebSocket/Senparc.WebSocket/WebSocketConfig.cs
@@ -0,0 +1,35 @@
+﻿/*----------------------------------------------------------------
+    Copyright (C) 2017 Senparc
+
+    文件名：WebSocketRouteConfig.cs
+    文件功能描述：自动配置WebSocket路由
+
+
+    创建标识：Senparc - 20170126
+
+----------------------------------------------------------------*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Routing;
+
+namespace Senparc.WebSocket
+{
+    public class WebSocketConfig
+    {
+        internal static Func<WebSocketMessageHandler> WebSocketMessageHandlerFunc { get; set; }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.Add("SenparcWebSocketRoute", new Route("SenparcWebSocket", new WebSocketRouteHandler()));//SenparcWebSocket/{app}
+        }
+
+        public static void RegisterMessageHandler<T>() where T : WebSocketMessageHandler, new()
+        {
+            WebSocketMessageHandlerFunc = () => new T();
+        }
+    }
+}

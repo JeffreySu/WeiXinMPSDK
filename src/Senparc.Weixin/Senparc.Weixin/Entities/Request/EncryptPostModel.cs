831feb0b4109b09f2beddf5fcb5d176b334e187d
JeffreySu <www.jeffrey.su@gmail.com>
2017-03-26 22:12:14 +0800

    Senparc.Weixin v4.11.6 修改注释，整理文件

--- src/Senparc.Weixin/Senparc.Weixin/Entities/Request/EncryptPostModel.cs
+++ src/Senparc.Weixin/Senparc.Weixin/Entities/Request/EncryptPostModel.cs
@@ -18,13 +18,32 @@ namespace Senparc.Weixin
     /// </summary>
     public interface IEncryptPostModel
     {
+        /// <summary>
+        /// Signature
+        /// </summary>
         string Signature { get; set; }
+        /// <summary>
+        /// Msg_Signature
+        /// </summary>
         string Msg_Signature { get; set; }
+        /// <summary>
+        /// Timestamp
+        /// </summary>
         string Timestamp { get; set; }
+        /// <summary>
+        /// Nonce
+        /// </summary>
         string Nonce { get; set; }
 
         //以下信息不会出现在微信发过来的信息中，都是企业号后台需要设置（获取的）的信息，用于扩展传参使用
+
+        /// <summary>
+        /// Token
+        /// </summary>
         string Token { get; set; }
+        /// <summary>
+        /// EncodingAESKey
+        /// </summary>
         string EncodingAESKey { get; set; }
     }
 
@@ -33,13 +52,32 @@ namespace Senparc.Weixin
     /// </summary>
     public class EncryptPostModel : IEncryptPostModel
     {
+        /// <summary>
+        /// Signature
+        /// </summary>
         public string Signature { get; set; }
+        /// <summary>
+        /// Msg_Signature
+        /// </summary>
         public string Msg_Signature { get; set; }
+        /// <summary>
+        /// Timestamp
+        /// </summary>
         public string Timestamp { get; set; }
+        /// <summary>
+        /// Nonce
+        /// </summary>
         public string Nonce { get; set; }
 
         //以下信息不会出现在微信发过来的信息中，都是企业号后台需要设置（获取的）的信息，用于扩展传参使用
+
+        /// <summary>
+        /// Token
+        /// </summary>
         public string Token { get; set; }
+        /// <summary>
+        /// EncodingAESKey
+        /// </summary>
         public string EncodingAESKey { get; set; }
 
         /// <summary>
@@ -47,7 +85,6 @@ namespace Senparc.Weixin
         /// </summary>
         /// <param name="token"></param>
         /// <param name="encodingAESKey"></param>
-        /// <param name="appId"></param>
         public virtual void SetSecretInfo(string token, string encodingAESKey)
         {
             Token = token;

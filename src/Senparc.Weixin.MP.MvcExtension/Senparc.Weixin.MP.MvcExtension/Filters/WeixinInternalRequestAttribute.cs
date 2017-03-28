741145758095c7a483bdfb9de213a88e548d7bc9
算神 <lishewen@gmail.com>
2017-03-28 17:23:19 +0800

    更新版本号，并使编译通过

--- src/Senparc.Weixin.MP.MvcExtension/Senparc.Weixin.MP.MvcExtension/Filters/WeixinInternalRequestAttribute.cs
+++ src/Senparc.Weixin.MP.MvcExtension/Senparc.Weixin.MP.MvcExtension/Filters/WeixinInternalRequestAttribute.cs
@@ -9,35 +9,35 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Senparc.Weixin.MP.MvcExtension
 {
-	/// <summary>
-	/// 过滤来自非微信客户端浏览器的请求
-	/// </summary>
-	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
-	public class WeixinInternalRequestAttribute : ActionFilterAttribute
-	{
-		private string _message;
-		private string _ignoreParameter;
+    /// <summary>
+    /// 过滤来自非微信客户端浏览器的请求
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class WeixinInternalRequestAttribute : ActionFilterAttribute
+    {
+        private string _message;
+        private string _ignoreParameter;
 
-		/// <summary>
-		/// 重定向地址，如果提供了redirectResult将忽略构造函数中的message
-		/// </summary>
-		public string RedirectUrl { get; set; }
+        /// <summary>
+        /// 重定向地址，如果提供了redirectResult将忽略构造函数中的message
+        /// </summary>
+        public string RedirectUrl { get; set; }
 
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="message">错误提示信息</param>
-		/// <param name="ignoreParameter">如果地址栏中提供改参数，则忽略浏览器判断，建议设置得复杂一些。如?abc=[任意字符]</param>
-		public WeixinInternalRequestAttribute(string message, string ignoreParameter = null)
-		{
-			_message = message;
-			_ignoreParameter = ignoreParameter;
-		}
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message">错误提示信息</param>
+        /// <param name="ignoreParameter">如果地址栏中提供改参数，则忽略浏览器判断，建议设置得复杂一些。如?abc=[任意字符]</param>
+        public WeixinInternalRequestAttribute(string message, string ignoreParameter = null)
+        {
+            _message = message;
+            _ignoreParameter = ignoreParameter;
+        }
 
 
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
-            if (string.IsNullOrEmpty(_ignoreParameter) || string.IsNullOrEmpty(filterContext.RequestContext.HttpContext.Request.QueryString[_ignoreParameter]))
+            if (string.IsNullOrEmpty(_ignoreParameter) || string.IsNullOrEmpty(filterContext.HttpContext.Request.Query[_ignoreParameter]))
             {
                 if (filterContext.HttpContext.SideInWeixinBrowser())
                 //if (!BrowserUtility.BrowserUtility.SideInWeixinBrowser(filterContext.HttpContext))
@@ -56,11 +56,11 @@ namespace Senparc.Weixin.MP.MvcExtension
                         };
                     }
 
-					filterContext.Result = actionResult;
-				}
-			}
+                    filterContext.Result = actionResult;
+                }
+            }
 
-			base.OnActionExecuting(filterContext);
-		}
-	}
+            base.OnActionExecuting(filterContext);
+        }
+    }
 }

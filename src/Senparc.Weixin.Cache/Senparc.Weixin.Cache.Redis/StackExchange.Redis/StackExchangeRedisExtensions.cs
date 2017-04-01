c45a7a8b26f41c8edd2fdb800dfde2ca53258031
JeffreySu <www.jeffrey.su@gmail.com>
2017-04-02 00:43:18 +0800

    完成移植 Redis .net core 版本

--- src/Senparc.Weixin.Cache/Senparc.Weixin.Cache.Redis/StackExchange.Redis/StackExchangeRedisExtensions.cs
+++ src/Senparc.Weixin.Cache/Senparc.Weixin.Cache.Redis/StackExchange.Redis/StackExchangeRedisExtensions.cs
@@ -15,7 +15,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.Serialization.Formatters.Binary;
+//using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using Senparc.Weixin.Helpers;

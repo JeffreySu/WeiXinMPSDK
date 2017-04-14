09524104ab05511892f509438f3eef63242d1591
算神 <lishewen@gmail.com>
2017-04-14 15:06:33 +0800

    修改了Sample项目的DI写法

--- src/Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/Controllers/HomeController.cs
+++ src/Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/Controllers/HomeController.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Senparc.Weixin.Entities;
+using Microsoft.Extensions.Options;
+using Senparc.Weixin.MP.Containers;
 
 namespace Senparc.Weixin.MP.Sample.Controllers
 {
     public class HomeController : Controller
     {
+        SenparcWeixinSetting setting;
+        public HomeController(IOptionsSnapshot<SenparcWeixinSetting> senparcWeixinSetting)
+        {
+            setting = senparcWeixinSetting.Value;
+        }
         public IActionResult Index()
         {
+            ViewData["AccessToken"] = AccessTokenContainer.TryGetAccessToken(setting.WeixinAppId, setting.WeixinAppSecret);
             return View();
         }
 
--- src/Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/Startup.cs
+++ src/Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/Startup.cs
@@ -46,8 +46,7 @@ namespace Senparc.Weixin.MP.Sample
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory,
-            IOptions<SenparcWeixinSetting> senparcWeixinSetting)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
@@ -72,7 +71,7 @@ namespace Senparc.Weixin.MP.Sample
             });
 
             //Senparc.Weixin SDK 配置
-            SenparcWeixinSetting setting = senparcWeixinSetting.Value;
+            //SenparcWeixinSetting setting = senparcWeixinSetting.Value;
             //app.UseMiddleware()
         }
     }

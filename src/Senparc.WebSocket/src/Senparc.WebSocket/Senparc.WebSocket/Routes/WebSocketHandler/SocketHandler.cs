aa9d01861c6926fb4f2d1da4f6315c6e45191566
Jeffrey Su <zsu@senparc.com>
2017-04-19 22:10:24 +0800

    移植WebSocket（未完成）

---
+++ src/Senparc.WebSocket/src/Senparc.WebSocket/Senparc.WebSocket/Routes/WebSocketHandler/SocketHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.WebSockets;
+using Microsoft.AspNetCore.WebSockets.Server;
+
+namespace Senparc.WebSocket.Routes.WebSocketHandler
+{
+   public class SocketHandler
+    {
+        public const int BufferSize = 4096;
+        WebSocket socket;
+
+        SocketHandler(WebSocket socket)
+        {
+            this.socket = socket;
+        }
+        async Task EchoLoop()
+        {
+            var buffer = new byte[BufferSize];
+            var seg = new ArraySegment<byte>(buffer);
+            while (this.socket.State == WebSocketState.Open)
+            {
+                var incoming = await this.socket.ReceiveAsync(seg, CancellationToken.None);
+                var outgoing = new ArraySegment<byte>(buffer, 0, incoming.Count);
+                await this.socket.SendAsync(outgoing, WebSocketMessageType.Text, true, CancellationToken.None);
+            }
+        }
+        static async Task Acceptor(HttpContext hc, Func<Task> n)
+        {
+            if (!hc.WebSockets.IsWebSocketRequest)
+                return;
+            var socket = await hc.WebSockets.AcceptWebSocketAsync();
+            var h = new SocketHandler(socket);
+            await h.EchoLoop();
+        }
+        /// <summary>
+        /// branches the request pipeline for this SocketHandler usage
+        /// </summary>
+        /// <param name="app"></param>
+        public static void Map(IApplicationBuilder app)
+        {
+            app.UseWebSockets();
+            app.Use(SocketHandler.Acceptor);
+        }
+    }
+}

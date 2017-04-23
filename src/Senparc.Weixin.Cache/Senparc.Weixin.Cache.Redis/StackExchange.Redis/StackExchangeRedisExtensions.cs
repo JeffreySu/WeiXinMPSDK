1ca41707fe4141662cbcb2d2d856a1a5cad87e83
JeffreySu <www.jeffrey.su@gmail.com>
2017-04-23 23:31:56 +0800

    完成Memcached移植

--- src/Senparc.Weixin.Cache/Senparc.Weixin.Cache.Redis/StackExchange.Redis/StackExchangeRedisExtensions.cs
+++ src/Senparc.Weixin.Cache/Senparc.Weixin.Cache.Redis/StackExchange.Redis/StackExchangeRedisExtensions.cs
@@ -15,10 +15,14 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.Serialization.Formatters.Binary;
+//using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using Senparc.Weixin.Helpers;
+
+#if NET461
+using System.Runtime.Serialization.Formatters.Binary;
+#endif
 
 namespace Senparc.Weixin.Cache.Redis
 {
@@ -67,7 +71,10 @@ namespace Senparc.Weixin.Cache.Redis
                 return null;
             }
 
-            //二进制序列化方案
+#if NET461
+            #region .net core后期可能会重新提供对 BinaryFormatter 的支持
+
+            ////二进制序列化方案
             BinaryFormatter binaryFormatter = new BinaryFormatter();
             using (MemoryStream memoryStream = new MemoryStream())
             {
@@ -75,8 +82,16 @@ namespace Senparc.Weixin.Cache.Redis
                 byte[] objectDataAsStream = memoryStream.ToArray();
                 return objectDataAsStream;
             }
-
-
+            #endregion
+#else
+            //二进制序列化方案
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                ProtoBuf.Serializer.Serialize(memoryStream, o);
+                byte[] objectDataAsStream = memoryStream.ToArray();
+                return objectDataAsStream;
+            }
+#endif
             //使用JSON序列化，会在Get()方法反序列化到IContainerBag的过程中出错
             //JSON序列化方案
             //SerializerHelper serializerHelper = new SerializerHelper();
@@ -97,6 +112,9 @@ namespace Senparc.Weixin.Cache.Redis
                 return default(T);
             }
 
+#if NET461
+            #region .net core后期可能会重新提供对 BinaryFormatter 的支持
+
             //二进制序列化方案
             BinaryFormatter binaryFormatter = new BinaryFormatter();
             using (MemoryStream memoryStream = new MemoryStream(stream))
@@ -104,6 +122,16 @@ namespace Senparc.Weixin.Cache.Redis
                 T result = (T)binaryFormatter.Deserialize(memoryStream);
                 return result;
             }
+
+            #endregion
+#else
+            using (MemoryStream memoryStream = new MemoryStream(stream))
+            {
+                T result = ProtoBuf.Serializer.Deserialize<T>(memoryStream);
+                return result;
+            }
+#endif
+
 
             //JSON序列化方案
             //SerializerHelper serializerHelper = new SerializerHelper();

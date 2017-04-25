88601377053fce1d5cd9f0f219fd29a260039880
JeffreySu <www.jeffrey.su@gmail.com>
2017-04-25 16:10:11 +0800

    添加原始csproj文件的条件编译

--- src/Senparc.Weixin.MP/Senparc.Weixin.MP/Properties/AssemblyInfo.cs
+++ src/Senparc.Weixin.MP/Senparc.Weixin.MP/Properties/AssemblyInfo.cs
@@ -26,14 +26,14 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-//[assembly: AssemblyTitle("Senparc.Weixin.MP")]
-//[assembly: AssemblyDescription("微信公众平台SDK")]
-//[assembly: AssemblyConfiguration("")]
-//[assembly: AssemblyCompany("Senparc")]
-//[assembly: AssemblyProduct("Senparc.Weixin.MP")]
-//[assembly: AssemblyCopyright("Copyright © Senparc 2017")]
-//[assembly: AssemblyTrademark("")]
-//[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("Senparc.Weixin.MP")]
+[assembly: AssemblyDescription("微信公众平台SDK")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Senparc")]
+[assembly: AssemblyProduct("Senparc.Weixin.MP")]
+[assembly: AssemblyCopyright("Copyright © Senparc 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
 // 将 ComVisible 设置为 false 使此程序集中的类型
 // 对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型，
@@ -53,6 +53,6 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
-//[assembly: AssemblyVersion("14.4.1.*")]
+[assembly: AssemblyVersion("14.4.1.*")]
 //[assembly: AssemblyInformationalVersion("13.3.1-alpha")]
 //[assembly: AssemblyFileVersion("0.4.2.0")]
--- src/Senparc.Weixin/Senparc.Weixin/Properties/AssemblyInfo.cs
+++ src/Senparc.Weixin/Senparc.Weixin/Properties/AssemblyInfo.cs
@@ -1,18 +1,20 @@
-﻿using System.Reflection;
+﻿#define NET45
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-//[assembly: AssemblyTitle("Senparc.Weixin")]
-//[assembly: AssemblyDescription("")]
-//[assembly: AssemblyConfiguration("")]
-//[assembly: AssemblyCompany("")]
-//[assembly: AssemblyProduct("Senparc.Weixin")]
-//[assembly: AssemblyCopyright("Copyright ©  2017")]
-//[assembly: AssemblyTrademark("")]
-//[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("Senparc.Weixin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Senparc.Weixin")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
 // 将 ComVisible 设置为 false 使此程序集中的类型
 // 对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型，
@@ -32,6 +34,6 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
-//[assembly: AssemblyVersion("4.11.11.*")]
+[assembly: AssemblyVersion("4.11.11.*")]
 //[assembly: AssemblyVersion("4.11.11")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]

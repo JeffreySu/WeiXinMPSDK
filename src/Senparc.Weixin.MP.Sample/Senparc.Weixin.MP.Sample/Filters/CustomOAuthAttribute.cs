07f4e9e7158eac632b1ca54e0ac9b84ca585e515
Jeffrey Su <zsu@senparc.com>
2017-05-09 13:16:35 +0800

    添加SenparcOAuthAttribute相关Demo

---
+++ src/Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/Filters/CustomOAuthAttribute.cs
@@ -0,0 +1,37 @@
+﻿using Senparc.Weixin.MP.MvcExtension;
+using Senparc.Weixin.MP.TenPayLibV3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Senparc.Weixin.MP.Sample.Filters
+{
+    public class CustomOAuthAttribute : SenparcOAuthAttribute
+    {
+        static TenPayV3Info _tenPayV3Info = null;
+        public static TenPayV3Info TenPayV3Info
+        {
+            get
+            {
+                if (_tenPayV3Info == null)
+                {
+                    _tenPayV3Info =
+                        TenPayV3InfoCollection.Data[System.Configuration.ConfigurationManager.AppSettings["TenPayV3_MchId"]];
+                }
+                return _tenPayV3Info;
+            }
+        }
+
+        public CustomOAuthAttribute(string appId, string oauthCallbackUrl) : base(appId, oauthCallbackUrl)
+        {
+            base._appId = base._appId ?? _tenPayV3Info.AppId;
+        }
+
+        public override bool IsLogined(HttpContextBase httpContext)
+        {
+            //也可以使用其他方法如Session验证用户登录
+            return httpContext != null && httpContext.User.Identity.IsAuthenticated;
+        }
+    }
+}

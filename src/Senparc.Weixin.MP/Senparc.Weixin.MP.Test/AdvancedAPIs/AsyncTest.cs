862cee40cd73b1002b9efdeb7bb2cc6af52a137d
Jeffrey Su <zsu@senparc.com>
2017-06-07 00:00:23 +0800

    完善 AsyncTest 单元测试方法

--- src/Senparc.Weixin.MP/Senparc.Weixin.MP.Test/AdvancedAPIs/AsyncTest.cs
+++ src/Senparc.Weixin.MP/Senparc.Weixin.MP.Test/AdvancedAPIs/AsyncTest.cs
@@ -14,7 +14,7 @@ namespace Senparc.Weixin.MP.Test.AdvancedAPIs
         public void TestMethod1()
         {
             var d1 = DateTime.Now;
-            Console.WriteLine("Start");
+            Console.WriteLine("1. Start");
 
             bool finished = false;
             Thread thread = new Thread(()=> {
@@ -22,19 +22,19 @@ namespace Senparc.Weixin.MP.Test.AdvancedAPIs
                 {
                     var d2 = DateTime.Now;
                     var tagJsonResult = await Senparc.Weixin.MP.AdvancedAPIs.UserTagApi.GetAsync(base._appId);
-                    Console.WriteLine("tagJsonResult 1：" + string.Join(",", tagJsonResult.tags.Select(z => z.name)));
-                    Console.WriteLine("用时：" + (DateTime.Now-d2).TotalMilliseconds+" ms");
+                    Console.WriteLine("3. tagJsonResult 1：" + string.Join(",", tagJsonResult.tags.Select(z => z.name)));
+                    Console.WriteLine("4. 用时：" + (DateTime.Now-d2).TotalMilliseconds+" ms");
 
                     return tagJsonResult;
                 }).ContinueWith(async task =>
                 {
                     var tagJsonResult = await task.Result;
-                    Console.WriteLine("tagJsonResult 2：" + string.Join(",", tagJsonResult.tags.Select(z => z.name)));
+                    Console.WriteLine("5. tagJsonResult 2：" + string.Join(",", tagJsonResult.tags.Select(z => z.name)));
                     finished = true;
                     //TODO：继续操作tagJsonResult
                 });
 
-                Console.WriteLine("StartNew Finished");
+                Console.WriteLine("2. StartNew Finished");
 
             });
 
@@ -45,7 +45,7 @@ namespace Senparc.Weixin.MP.Test.AdvancedAPIs
                 Thread.Sleep(5);
             }
 
-            Console.WriteLine("End："+(DateTime.Now -d1).TotalMilliseconds+" ms");
+            Console.WriteLine("6. End："+(DateTime.Now -d1).TotalMilliseconds+" ms");
 
         }
     }

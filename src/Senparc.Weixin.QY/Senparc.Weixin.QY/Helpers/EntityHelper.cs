c22105d8056d5cf1841983ba4695f13a3c3a61f5
JeffreySu <www.jeffrey.su@gmail.com>
2017-06-18 01:41:15 +0800

    modify

--- src/Senparc.Weixin.QY/Senparc.Weixin.QY/Helpers/EntityHelper.cs
+++ src/Senparc.Weixin.QY/Senparc.Weixin.QY/Helpers/EntityHelper.cs
@@ -382,13 +382,13 @@ namespace Senparc.Weixin.QY.Helpers
             return entity.ConvertEntityToXml().ToString();
         }
 
-		/// <summary>
-		/// ResponseMessageBase.CreateFromRequestMessage<T>(requestMessage)的扩展方法
-		/// </summary>
-		/// <typeparam name="T">需要生成的ResponseMessage类型</typeparam>
-		/// <param name="requestMessage">IRequestMessageBase接口下的接收信息类型</param>
-		/// <returns></returns>
-		public static T CreateResponseMessage<T>(this IRequestMessageBase requestMessage) where T : ResponseMessageBase
+        /// <summary>
+        /// ResponseMessageBase.CreateFromRequestMessage&lt;T&gt;(requestMessage)的扩展方法
+        /// </summary>
+        /// <typeparam name="T">需要生成的ResponseMessage类型</typeparam>
+        /// <param name="requestMessage">IRequestMessageBase接口下的接收信息类型</param>
+        /// <returns></returns>
+        public static T CreateResponseMessage<T>(this IRequestMessageBase requestMessage) where T : ResponseMessageBase
         {
             return ResponseMessageBase.CreateFromRequestMessage<T>(requestMessage);
         }

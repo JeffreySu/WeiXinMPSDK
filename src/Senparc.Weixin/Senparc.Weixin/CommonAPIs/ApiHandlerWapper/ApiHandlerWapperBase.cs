d632dc127fe79b51988880f5c483828c82da83eb
JeffreySu <www.jeffrey.su@gmail.com>
2017-07-02 17:29:13 +0800

    完成第一个AppApi.GetAppInfo()方法的ApiHandlerWapper.TryCommonApi改造（未测试）

--- src/Senparc.Weixin/Senparc.Weixin/CommonAPIs/ApiHandlerWapper/ApiHandlerWapperBase.cs
+++ src/Senparc.Weixin/Senparc.Weixin/CommonAPIs/ApiHandlerWapper/ApiHandlerWapperBase.cs
@@ -68,7 +68,7 @@ namespace Senparc.Weixin.CommonAPIs.ApiHandlerWapper
 
             //ApiHandlerWapperFactory.ApiHandlerWapperFactoryCollection["s"] = ()=> new Senparc.Weixin.MP.AdvancedAPIs.User.UserInfoJson();
 
-            var platform = ApiHandlerWapperFactory.CurrentPlatform;//当前平台
+            //var platform = ApiHandlerWapperFactory.CurrentPlatform;//当前平台
 
             /*
              * 对于企业微信来说，AppId = key = CorpId+CorpSecret
@@ -172,7 +172,7 @@ namespace Senparc.Weixin.CommonAPIs.ApiHandlerWapper
 
             //ApiHandlerWapperFactory.ApiHandlerWapperFactoryCollection["s"] = ()=> new Senparc.Weixin.MP.AdvancedAPIs.User.UserInfoJson();
 
-            var platform = ApiHandlerWapperFactory.CurrentPlatform;//当前平台
+            //var platform = ApiHandlerWapperFactory.CurrentPlatform;//当前平台
 
             string appId = null;
             string accessToken = null;

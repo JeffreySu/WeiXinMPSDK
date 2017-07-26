ff67033454008133e1b5c1ce683bb0881adc9bf7
chenSean1 <schen@senparc.com>
2017-07-26 15:27:51 +0800

    添加版权说明注释

--- src/Senparc.Weixin.WxOpen/src/Senparc.Weixin.WxOpen/Senparc.Weixin.WxOpen/AdvancedAPIs/WxApp/WxAppApi.cs
+++ src/Senparc.Weixin.WxOpen/src/Senparc.Weixin.WxOpen/Senparc.Weixin.WxOpen/AdvancedAPIs/WxApp/WxAppApi.cs
@@ -29,6 +29,14 @@ Detail: https://github.com/JeffreySu/WeiXinMPSDK/blob/master/license.md
     
     修改标识：Senparc - 20170129
     修改描述：v1.0.1 完善CreateWxaQrCode方法
+
+    修改标识：Senparc - 20170726
+
+    注意：此项目是《微信开发深度解析：微信公众号、小程序高效开发秘籍》图书中第5章的WeixinMarketing项目源代码。
+    本项目只包含了运行案例所必须的学习代码，以及精简的部分SenparcCore框架代码，不确保其他方面的稳定性、安全性，
+    因此，请勿直接用于商业项目，例如安全性、缓存等需要根据具体情况进行调试。
+
+    盛派网络保留所有权利。
 ----------------------------------------------------------------*/
 
 using System;

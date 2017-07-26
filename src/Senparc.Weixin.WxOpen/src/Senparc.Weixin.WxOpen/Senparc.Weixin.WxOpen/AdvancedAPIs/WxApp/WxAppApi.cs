b486ef92fc42edbe01aed808a12a149c5caf6664
LiJiangShan <930657@qq.com>
2017-07-26 11:24:02 +0800

    1

--- src/Senparc.Weixin.WxOpen/src/Senparc.Weixin.WxOpen/Senparc.Weixin.WxOpen/AdvancedAPIs/WxApp/WxAppApi.cs
+++ src/Senparc.Weixin.WxOpen/src/Senparc.Weixin.WxOpen/Senparc.Weixin.WxOpen/AdvancedAPIs/WxApp/WxAppApi.cs
@@ -55,6 +55,16 @@ namespace Senparc.Weixin.WxOpen.AdvancedAPIs.WxApp
     {
         #region 同步请求
 
+        /// <summary>
+        /// 获取小程序页面的小程序码
+        /// </summary>
+        /// <param name="accessTokenOrAppId">AccessToken或AppId（推荐使用AppId，需要先注册）</param>
+        /// <param name="stream">储存小程序码的流</param>
+        /// <param name="path">不能为空，最大长度 128 字节（如：pages/index?query=1。注：pages/index 需要在 app.json 的 pages 中定义）</param>
+        /// <param name="width">小程序码的宽度</param>
+        /// <param name="auto_color">自动配置线条颜色</param>
+        /// <param name="timeOut">请求超时时间</param>
+        /// <returns></returns>
         public static WxJsonResult GetWxaCode(string accessTokenOrAppId, Stream stream, string path, int width = 430, bool auto_color = false , int timeOut = Config.TIME_OUT)
         {
             return ApiHandlerWapper.TryCommonApi(accessToken =>
@@ -73,6 +83,16 @@ namespace Senparc.Weixin.WxOpen.AdvancedAPIs.WxApp
             }, accessTokenOrAppId);
         }
 
+        /// <summary>
+        /// 获取小程序页面的小程序码
+        /// </summary>
+        /// <param name="accessTokenOrAppId">AccessToken或AppId（推荐使用AppId，需要先注册）</param>
+        /// <param name="filePath">储存图片的物理路径</param>
+        /// <param name="path">不能为空，最大长度 128 字节（如：pages/index?query=1。注：pages/index 需要在 app.json 的 pages 中定义）</param>
+        /// <param name="width">二维码的宽度</param>
+        /// <param name="auto_color">自动配置线条颜色</param>
+        /// <param name="timeOut">请求超时时间</param>
+        /// <returns></returns>
         public static WxJsonResult GetWxaCode(string accessTokenOrAppId, string filePath, string path, int width = 430, bool auto_color = false, int timeOut = Config.TIME_OUT)
         {
             using (var ms = new MemoryStream())
@@ -146,6 +166,61 @@ namespace Senparc.Weixin.WxOpen.AdvancedAPIs.WxApp
         #endregion
 
         #region 异步请求
+
+        /// <summary>
+        /// 获取小程序页面的小程序码
+        /// </summary>
+        /// <param name="accessTokenOrAppId">AccessToken或AppId（推荐使用AppId，需要先注册）</param>
+        /// <param name="stream">储存小程序码的流</param>
+        /// <param name="path">不能为空，最大长度 128 字节（如：pages/index?query=1。注：pages/index 需要在 app.json 的 pages 中定义）</param>
+        /// <param name="width">小程序码的宽度</param>
+        /// <param name="auto_color">自动配置线条颜色</param>
+        /// <param name="timeOut">请求超时时间</param>
+        /// <returns></returns>
+        public static async Task<WxJsonResult> GetWxaCodeAsync(string accessTokenOrAppId, Stream stream, string path, int width = 430, bool auto_color = false, int timeOut = Config.TIME_OUT)
+        {
+            return await ApiHandlerWapper.TryCommonApiAsync(async accessToken =>
+            {
+                const string urlFormat = "https://api.weixin.qq.com/wxa/getwxacode?access_token={0}";
+                var url = string.Format(urlFormat, accessToken);
+
+                var data = new { path = path, width = width };
+                SerializerHelper serializerHelper = new SerializerHelper();
+                await Post.DownloadAsync(url, serializerHelper.GetJsonString(data), stream);
+
+                return new WxJsonResult()
+                {
+                    errcode = ReturnCode.请求成功
+                };
+            }, accessTokenOrAppId);
+        }
+
+        /// <summary>
+        /// 获取小程序页面的小程序码
+        /// </summary>
+        /// <param name="accessTokenOrAppId">AccessToken或AppId（推荐使用AppId，需要先注册）</param>
+        /// <param name="filePath">储存图片的物理路径</param>
+        /// <param name="path">不能为空，最大长度 128 字节（如：pages/index?query=1。注：pages/index 需要在 app.json 的 pages 中定义）</param>
+        /// <param name="width">二维码的宽度</param>
+        /// <param name="auto_color">自动配置线条颜色</param>
+        /// <param name="timeOut">请求超时时间</param>
+        /// <returns></returns>
+        public static async Task<WxJsonResult> GetWxaCodeAsync(string accessTokenOrAppId, string filePath, string path, int width = 430, bool auto_color = false, int timeOut = Config.TIME_OUT)
+        {
+            using (var ms = new MemoryStream())
+            {
+                var result = await WxAppApi.GetWxaCodeAsync(accessTokenOrAppId, ms, path, width);
+                ms.Seek(0, SeekOrigin.Begin);
+                //储存图片
+                File.Delete(filePath);
+                using (var fs = new FileStream(filePath, FileMode.CreateNew))
+                {
+                    await ms.CopyToAsync(fs);
+                    await fs.FlushAsync();
+                }
+                return result;
+            }
+        }
 
         /// <summary>
         /// 【异步方法】获取小程序页面二维码

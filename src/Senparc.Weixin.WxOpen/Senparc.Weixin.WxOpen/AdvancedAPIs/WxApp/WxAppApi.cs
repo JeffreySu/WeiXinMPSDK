99dbc35a3a5800e8467ae622c5c6f9c1ee2bc46b
JeffreySu <www.jeffrey.su@gmail.com>
2017-08-31 18:02:47 +0800

    Squashed 'src/Senparc.Weixin.WxOpen/' changes from 9d291874..ea504a73
    ea504a73 update sln file
    1828ebd6 Merge pull request #15 from JeffreySu/Developer
    d1462bf0 remove unlinked file
    4e4929d3 Merge pull request #14 from JeffreySu/Developer
    12a6dd97 Merge pull request #734 from jiehanlin/master
    0ae3ea43 修改引用命名空间名称
    02b12eff Revert "修改引用命名空间名称"
    09e4fdf2 修改引用命名空间名称
    b727d3ac 小程序 WxOpen v1.4.3 添加JsCode2JsonResult.unionid 感谢@kevinleptin #711
    025a6e54 调整Tencent命名空间
    679ace00 为Tencent命名空间添加Senparc.Weixin.MP/WxOpen/Open/QY/Work前缀 感谢@jiehanlin #727
    2cf763a7 小程序WxOpen v1.4.1  增加GetWxaCodeUnlimit接口 感谢 @mojinxun https://github.com/JeffreySu/WxOpen/pull/13
    
    git-subtree-dir: src/Senparc.Weixin.WxOpen
    git-subtree-split: ea504a73ed3a3cc760553bd21fb4b6380eda6a4d

--- src/Senparc.Weixin.WxOpen/Senparc.Weixin.WxOpen/AdvancedAPIs/WxApp/WxAppApi.cs
+++ src/Senparc.Weixin.WxOpen/Senparc.Weixin.WxOpen/AdvancedAPIs/WxApp/WxAppApi.cs
@@ -171,6 +171,8 @@ namespace Senparc.Weixin.WxOpen.AdvancedAPIs.WxApp
             }
         }
 
+
+
         /// <summary>
         /// 获取小程序页面二维码
         /// </summary>
@@ -227,34 +229,6 @@ namespace Senparc.Weixin.WxOpen.AdvancedAPIs.WxApp
         #endregion
 
         #region 异步请求
-
-        /// <summary>
-        /// 获取小程序页面的小程序码
-        /// </summary>
-        /// <param name="accessTokenOrAppId">AccessToken或AppId（推荐使用AppId，需要先注册）</param>
-        /// <param name="stream">储存小程序码的流</param>
-        /// <param name="path">不能为空，最大长度 128 字节（如：pages/index?query=1。注：pages/index 需要在 app.json 的 pages 中定义）</param>
-        /// <param name="width">小程序码的宽度</param>
-        /// <param name="auto_color">自动配置线条颜色</param>
-        /// <param name="timeOut">请求超时时间</param>
-        /// <returns></returns>
-        public static async Task<WxJsonResult> GetWxaCodeAsync(string accessTokenOrAppId, Stream stream, string path, int width = 430, bool auto_color = false, int timeOut = Config.TIME_OUT)
-        {
-            return await ApiHandlerWapper.TryCommonApiAsync(async accessToken =>
-            {
-                const string urlFormat = "https://api.weixin.qq.com/wxa/getwxacode?access_token={0}";
-                var url = string.Format(urlFormat, accessToken);
-
-                var data = new { path = path, width = width };
-                SerializerHelper serializerHelper = new SerializerHelper();
-                await Post.DownloadAsync(url, serializerHelper.GetJsonString(data), stream);
-
-                return new WxJsonResult()
-                {
-                    errcode = ReturnCode.请求成功
-                };
-            }, accessTokenOrAppId);
-        }
 
         /// <summary>
         /// 获取小程序页面的小程序码
@@ -339,6 +313,34 @@ namespace Senparc.Weixin.WxOpen.AdvancedAPIs.WxApp
                 }
                 return result;
             }
+        }
+
+        /// <summary>
+        /// 获取小程序页面的小程序码
+        /// </summary>
+        /// <param name="accessTokenOrAppId">AccessToken或AppId（推荐使用AppId，需要先注册）</param>
+        /// <param name="stream">储存小程序码的流</param>
+        /// <param name="path">不能为空，最大长度 128 字节（如：pages/index?query=1。注：pages/index 需要在 app.json 的 pages 中定义）</param>
+        /// <param name="width">小程序码的宽度</param>
+        /// <param name="auto_color">自动配置线条颜色</param>
+        /// <param name="timeOut">请求超时时间</param>
+        /// <returns></returns>
+        public static async Task<WxJsonResult> GetWxaCodeAsync(string accessTokenOrAppId, Stream stream, string path, int width = 430, bool auto_color = false, int timeOut = Config.TIME_OUT)
+        {
+            return await ApiHandlerWapper.TryCommonApiAsync(async accessToken =>
+            {
+                const string urlFormat = "https://api.weixin.qq.com/wxa/getwxacode?access_token={0}";
+                var url = string.Format(urlFormat, accessToken);
+
+                var data = new { path = path, width = width };
+                SerializerHelper serializerHelper = new SerializerHelper();
+                await Post.DownloadAsync(url, serializerHelper.GetJsonString(data), stream);
+
+                return new WxJsonResult()
+                {
+                    errcode = ReturnCode.请求成功
+                };
+            }, accessTokenOrAppId);
         }
 
         /// <summary>

f94b6f8e1b533fb04c5cd3d3f1badd06314a6b95
Null <hetaoos@gmail.com>
2017-09-14 21:17:33 +0800

    更新 DateTimeHelper 的时间戳转换方式。原代码强制将本地时间当作东八区来使用。

--- src/Senparc.Weixin/Senparc.Weixin/Helpers/DateTimeHelper.cs
+++ src/Senparc.Weixin/Senparc.Weixin/Helpers/DateTimeHelper.cs
@@ -42,7 +42,7 @@ namespace Senparc.Weixin.Helpers
     /// </summary>
     public static class DateTimeHelper
     {
-        public static DateTime BaseTime = new DateTime(1970, 1, 1);//Unix起始时间
+        public readonly static DateTime BaseTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);//Unix起始时间
 
         /// <summary>
         /// 转换微信DateTime时间到C#时间
@@ -51,7 +51,7 @@ namespace Senparc.Weixin.Helpers
         /// <returns></returns>
         public static DateTime GetDateTimeFromXml(long dateTimeFromXml)
         {
-            return BaseTime.AddTicks((dateTimeFromXml + 8 * 60 * 60) * 10000000);
+            return BaseTime.AddSeconds(dateTimeFromXml).ToLocalTime();
         }
         /// <summary>
         /// 转换微信DateTime时间到C#时间
@@ -70,7 +70,7 @@ namespace Senparc.Weixin.Helpers
         /// <returns></returns>
         public static long GetWeixinDateTime(DateTime dateTime)
         {
-            return (dateTime.Ticks - BaseTime.Ticks) / 10000000 - 8 * 60 * 60;
+            return (long)(dateTime.ToUniversalTime() - BaseTime).TotalSeconds;
         }
     }
 }

5b7441692d455c6c500b0f9d6ff0564132dbc659
Jeffrey Su <zsu@senparc.com>
2017-09-21 18:25:19 +0800

    升级ClearDotNetCoreBadFiles.exe

--- Tools/ClearDotNetCoreBadFiles/ClearDotNetCoreBadFiles/Program.cs
+++ Tools/ClearDotNetCoreBadFiles/ClearDotNetCoreBadFiles/Program.cs
@@ -11,11 +11,20 @@ namespace ClearDotNetCoreBadFiles
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("当前工具用于清理项目在.NET Core（包括.NET Standard）编译后，");
-            Console.WriteLine("生成的中间文件会导致.NET Framework编译失败的情况。");
-            Console.WriteLine(@"例如：Your project.json doesn't have a runtimes section. You should add '""runtimes"": { ""win"": { } }' to your project.json and then re-run NuGet restore.	Senparc.Weixin.Work
+            Console.WriteLine(@"ClearDotNetCoreBadFiles v1.0
+Copyrignt 2017 Senparc
+
+当前工具用于清理项目在.NET Core（包括.NET Standard）编译后，
+生成的中间文件会导致.NET Framework编译失败的情况。
+
+例如遇到错误信息：
+---------------------------------------------
+Your project.json doesn't have a runtimes section. You should add '""runtimes"": { ""win"": { } }' to your project.json and then re-run NuGet restore.
+---------------------------------------------
+
+工作原理为删除项目obj文件夹，弥补“清理解决方案”功能无法解决的问题。
+请了解原理后再使用本工具，以免发生意外！点击任意键开始进行清理...
 ");
-            Console.WriteLine("点击任意键进行清理");
             Console.ReadKey();
             Console.WriteLine("清理开始……");
 

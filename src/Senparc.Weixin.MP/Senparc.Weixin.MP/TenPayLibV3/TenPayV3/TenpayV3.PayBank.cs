4e0799af4f6677ab2b7cf15d788a60057200e238
JeffreySu <www.jeffrey.su@gmail.com>
2017-11-29 23:02:07 +0800

    add TenPayV3.PayBank.cs 用于存放“付款到银行卡”接口

---
+++ src/Senparc.Weixin.MP/Senparc.Weixin.MP/TenPayLibV3/TenPayV3/TenpayV3.PayBank.cs
@@ -0,0 +1,46 @@
+﻿#region Apache License Version 2.0
+/*----------------------------------------------------------------
+
+Copyright 2017 Jeffrey Su & Suzhou Senparc Network Technology Co.,Ltd.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+except in compliance with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the
+License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+either express or implied. See the License for the specific language governing permissions
+and limitations under the License.
+
+Detail: https://github.com/JeffreySu/WeiXinMPSDK/blob/master/license.md
+
+----------------------------------------------------------------*/
+#endregion Apache License Version 2.0
+
+/*----------------------------------------------------------------
+    Copyright (C) 2017 Senparc
+ 
+    文件名：TenPayV3.PayBank.cs
+    文件功能描述：微信支付V3接口：付款到银行卡
+    
+    
+    创建标识：Senparc - 20171129
+
+----------------------------------------------------------------*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.Weixin.MP.TenPayLibV3
+{
+    /// <summary>
+    /// 付款到银行卡，文档：https://pay.weixin.qq.com/wiki/doc/api/tools/mch_pay.php?chapter=24_2
+    /// </summary>
+    public partial class TenpayV3
+    {
+    }
+}

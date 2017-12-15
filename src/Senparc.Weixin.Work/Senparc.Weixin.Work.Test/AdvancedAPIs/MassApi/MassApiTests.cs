7b046595dacf79cec794e13e59f66f4430becce7
JeffreySu <www.jeffrey.su@gmail.com>
2017-12-15 23:22:13 +0800

    完善单元测试

--- src/Senparc.Weixin.Work/Senparc.Weixin.Work.Test/AdvancedAPIs/MassApi/MassApiTests.cs
+++ src/Senparc.Weixin.Work/Senparc.Weixin.Work.Test/AdvancedAPIs/MassApi/MassApiTests.cs
@@ -17,15 +17,22 @@ namespace Senparc.Weixin.Work.Test.AdvancedAPIs.MassApi
 
             try
             {
-                var accessToken = "fM5CruClicaXYpz9vai-nl2lB2V-S25Yed_BDmd8sl6P1vBvExBnfoYbOrYAMEawhZc5bhX1mu0nVJilrBxXyeAs-7y70gkwJIEXVrk4JvfFaDJP6GdWBDYq5l6tqfL8megghLqDHLXPsdokIJ6UmXrb8buPEUgdKPNWXNJKVeq32uqB54OaAnxNkQVK-MtbI_QgNT4grhUhuIbYak_7Gg";
+                var accessToken =
+                    "fM5CruClicaXYpz9vai-nl2lB2V-S25Yed_BDmd8sl6P1vBvExBnfoYbOrYAMEawhZc5bhX1mu0nVJilrBxXyeAs-7y70gkwJIEXVrk4JvfFaDJP6GdWBDYq5l6tqfL8megghLqDHLXPsdokIJ6UmXrb8buPEUgdKPNWXNJKVeq32uqB54OaAnxNkQVK-MtbI_QgNT4grhUhuIbYak_7Gg";
 
                 var result = Work.AdvancedAPIs.MassApi.SendNews(accessToken, "agentId", new List<Article>());
                 Assert.Fail();
             }
+            catch (Senparc.Weixin.Exceptions.UnRegisterAppIdException e)
+            {
+                Assert.Fail();//出现反馈问题中的“AppId未注册”错误
+            }
             catch (Senparc.Weixin.Exceptions.ErrorJsonResultException e)
             {
                 //请求发生错误！错误代码：40014，说明：invalid access_token”
-                Assert.AreEqual(40014, (int)e.JsonResult.errcode);
+                Assert.AreEqual(40014, (int) e.JsonResult.errcode);
+
+                //没有发生AppId未注册的错误
             }
 
         }

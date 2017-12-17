723131ccf6ad73eac902b26a5a5320bf8cd65278
JeffreySu <www.jeffrey.su@gmail.com>
2017-12-17 18:23:40 +0800

    添加 BaseGroupMessageDataByFilter，为所有此文件夹下其他类的基类，用于提供具有筛选条件群发（GroupId、TagId）数据的基类

---
+++ src/Senparc.Weixin.MP/Senparc.Weixin.MP/AdvancedAPIs/GroupMessage/GroupMessageJson/ByFilter/BaseGroupMessageDataByFilter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.Weixin.MP.AdvancedAPIs.GroupMessage
+{
+    public class BaseGroupMessageDataByFilter
+    {
+        public string msgtype { get; set; }
+
+        /// <summary>
+        /// 群发接口新增 send_ignore_reprint 参数，开发者可以对群发接口的 send_ignore_reprint 参数进行设置，指定待群发的文章被判定为转载时，是否继续群发。
+        /// 当 send_ignore_reprint 参数设置为1时，文章被判定为转载时，且原创文允许转载时，将继续进行群发操作。
+        /// 当 send_ignore_reprint 参数设置为0时，文章被判定为转载时，将停止群发操作。
+        /// send_ignore_reprint 默认为0。
+        /// </summary>
+        public int send_ignore_reprint { get; set; }
+    }
+}
--- src/Senparc.Weixin.MP/Senparc.Weixin.MP/AdvancedAPIs/GroupMessage/GroupMessageJson/ByFilter/BaseGroupMessageDataByGroupId.cs
+++ src/Senparc.Weixin.MP/Senparc.Weixin.MP/AdvancedAPIs/GroupMessage/GroupMessageJson/ByFilter/BaseGroupMessageDataByGroupId.cs
@@ -17,18 +17,13 @@
 
 namespace Senparc.Weixin.MP.AdvancedAPIs.GroupMessage
 {
-    public class BaseGroupMessageDataByGroupId
+    /// <summary>
+    /// 根据GroupId群发消息筛选
+    /// </summary>
+    public class BaseGroupMessageDataByGroupId : BaseGroupMessageDataByFilter
     {
         public GroupMessageByGroupId_GroupId filter { get; set; }
-        public string msgtype { get; set; }
 
-        /// <summary>
-        /// 群发接口新增 send_ignore_reprint 参数，开发者可以对群发接口的 send_ignore_reprint 参数进行设置，指定待群发的文章被判定为转载时，是否继续群发。
-        /// 当 send_ignore_reprint 参数设置为1时，文章被判定为转载时，且原创文允许转载时，将继续进行群发操作。
-        /// 当 send_ignore_reprint 参数设置为0时，文章被判定为转载时，将停止群发操作。
-        /// send_ignore_reprint 默认为0。
-        /// </summary>
-        public int send_ignore_reprint { get; set; }
     }
 
     public class GroupMessageByGroupId_GroupId

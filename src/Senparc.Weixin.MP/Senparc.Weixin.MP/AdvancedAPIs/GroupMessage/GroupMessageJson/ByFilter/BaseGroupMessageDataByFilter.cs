5c721852f4523df986a5c0095d0e3f8d774ca911
JeffreySu <www.jeffrey.su@gmail.com>
2017-12-18 12:59:13 +0800

    添加注释

--- src/Senparc.Weixin.MP/Senparc.Weixin.MP/AdvancedAPIs/GroupMessage/GroupMessageJson/ByFilter/BaseGroupMessageDataByFilter.cs
+++ src/Senparc.Weixin.MP/Senparc.Weixin.MP/AdvancedAPIs/GroupMessage/GroupMessageJson/ByFilter/BaseGroupMessageDataByFilter.cs
@@ -1,4 +1,36 @@
-﻿using System;
+﻿#region Apache License Version 2.0
+/*----------------------------------------------------------------
+
+Copyright 2017 Jeffrey Su & Suzhou Senparc Network Technology Co.,Ltd.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+except in compliance with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the
+License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+either express or implied. See the License for the specific language governing permissions
+and limitations under the License.
+
+Detail: https://github.com/JeffreySu/WeiXinMPSDK/blob/master/license.md
+
+----------------------------------------------------------------*/
+#endregion Apache License Version 2.0
+
+
+/*----------------------------------------------------------------
+    Copyright (C) 2017 Senparc
+    
+    文件名：BaseGroupMessageByFilter.cs
+    文件功能描述：根据筛选条件（GroupId、TagId）群发消息数据的基类
+    
+    
+    创建标识：Senparc - 20171217
+    
+----------------------------------------------------------------*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +38,9 @@ using System.Threading.Tasks;
 
 namespace Senparc.Weixin.MP.AdvancedAPIs.GroupMessage
 {
+    /// <summary>
+    /// 根据筛选条件（GroupId、TagId）群发消息数据的基类
+    /// </summary>
     public abstract class BaseGroupMessageByFilter
     {
         public bool is_to_all { get; set; }

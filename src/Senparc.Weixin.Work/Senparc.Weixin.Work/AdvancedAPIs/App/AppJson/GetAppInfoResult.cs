1d8e52d9d5b8ba30027e5d26258a963717ea024d
JeffreySu <www.jeffrey.su@gmail.com>
2017-12-30 14:12:29 +0800

    v1.2.8 调整“获取应用”接口，重新释放allow_tags属性。 感谢 @kkistudio #1000

--- src/Senparc.Weixin.Work/Senparc.Weixin.Work/AdvancedAPIs/App/AppJson/GetAppInfoResult.cs
+++ src/Senparc.Weixin.Work/Senparc.Weixin.Work/AdvancedAPIs/App/AppJson/GetAppInfoResult.cs
@@ -46,10 +46,10 @@ namespace Senparc.Weixin.Work.AdvancedAPIs.App
         /// 企业应用可见范围（部门）
         /// </summary>
         public GetAppInfo_AllowPartys allow_partys { get; set; }
-        ///// <summary>
-        ///// 企业应用可见范围（标签）
-        ///// </summary>
-        //public GetAppInfo_AllowTags allow_tags { get; set; }
+        /// <summary>
+        /// 企业应用可见范围（标签）
+        /// </summary>
+        public GetAppInfo_AllowTags allow_tags { get; set; }
         /// <summary>
         /// 企业应用是否被禁用
         /// </summary>
@@ -89,7 +89,7 @@ namespace Senparc.Weixin.Work.AdvancedAPIs.App
 
     public class GetAppInfo_AllowPartys
     {
-        public int[] partyid { get; set; }
+        public long[] partyid { get; set; }
     }
 
     public class GetAppInfo_AllowTags

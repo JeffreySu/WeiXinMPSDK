843d5a0fefce50d51cd8ba0b596aba845c0d1db1
JeffreySu <www.jeffrey.su@gmail.com>
2018-01-02 21:03:50 +0800

    修改 Copyright 2018

--- src/Senparc.Weixin.Work/Senparc.Weixin.Work/Helpers/MD5UtilHelper.cs
+++ src/Senparc.Weixin.Work/Senparc.Weixin.Work/Helpers/MD5UtilHelper.cs
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------
-    Copyright (C) 2017 Senparc
+    Copyright (C) 2018 Senparc
     
     文件名：MD5UtilHelper.cs
     文件功能描述：获取大写的MD5签名结果

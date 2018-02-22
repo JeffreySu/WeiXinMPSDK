1c767fd447953acc053486183cc636d837f471f3
JeffreySu <www.jeffrey.su@gmail.com>
2018-02-22 16:25:09 +0800

    add RegisterService

---
+++ src/Senparc.Weixin/Senparc.Weixin/Register/RegisterService.cs
@@ -0,0 +1,38 @@
+﻿/*----------------------------------------------------------------
+    Copyright (C) 2018 Senparc
+
+    文件名：RegisterService.cs
+    文件功能描述：Senparc.Weixin SDK 快捷注册流程
+
+
+    创建标识：Senparc - 20180222
+
+----------------------------------------------------------------*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.Weixin.Register
+{
+    /// <summary>
+    /// 快捷注册接口
+    /// </summary>
+    public interface IRegisterService
+    {
+
+    }
+
+    /// <summary>
+    /// 快捷注册类，IRegisterService的默认实现
+    /// </summary>
+    public class RegisterService : IRegisterService
+    {
+        public static RegisterService Start()
+        {
+            return new RegisterService();
+        }
+    }
+}

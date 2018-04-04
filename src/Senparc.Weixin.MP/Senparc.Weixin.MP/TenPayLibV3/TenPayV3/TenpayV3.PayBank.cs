2746e35334f7988ceb9305b690c76e89642b1ee4
JeffreySu <www.jeffrey.su@gmail.com>
2018-04-04 16:37:27 +0800

    modify

--- src/Senparc.Weixin.MP/Senparc.Weixin.MP/TenPayLibV3/TenPayV3/TenpayV3.PayBank.cs
+++ src/Senparc.Weixin.MP/Senparc.Weixin.MP/TenPayLibV3/TenPayV3/TenpayV3.PayBank.cs
@@ -46,18 +46,18 @@ namespace Senparc.Weixin.MP.TenPayLibV3
     {
         #region 同步方法
 
-        public static PayBankResult PayBank(TenPayV3PayBankRequestData dataInfo)
-        {
-            var urlFormat = ReurnPayApiUrl("https://api.mch.weixin.qq.com/{0}mmpaysptrans/pay_bank");
+        //public static PayBankResult PayBank(TenPayV3PayBankRequestData dataInfo)
+        //{
+        //    var urlFormat = ReurnPayApiUrl("https://api.mch.weixin.qq.com/{0}mmpaysptrans/pay_bank");
 
-            var data = dataInfo.PackageRequestHandler.ParseXML();//获取XML
-            var formDataBytes = data == null ? new byte[0] : Encoding.UTF8.GetBytes(data);
-            MemoryStream ms = new MemoryStream();
-            ms.Write(formDataBytes, 0, formDataBytes.Length);
-            ms.Seek(0, SeekOrigin.Begin);//设置指针读取位置
-            var resultXml = RequestUtility.HttpPost(urlFormat, null, ms);
-            return new PayBankResult(resultXml);
-        }
+        //    var data = dataInfo.PackageRequestHandler.ParseXML();//获取XML
+        //    var formDataBytes = data == null ? new byte[0] : Encoding.UTF8.GetBytes(data);
+        //    MemoryStream ms = new MemoryStream();
+        //    ms.Write(formDataBytes, 0, formDataBytes.Length);
+        //    ms.Seek(0, SeekOrigin.Begin);//设置指针读取位置
+        //    var resultXml = RequestUtility.HttpPost(urlFormat, null, ms);
+        //    return new PayBankResult(resultXml);
+        //}
 
         #endregion
     }

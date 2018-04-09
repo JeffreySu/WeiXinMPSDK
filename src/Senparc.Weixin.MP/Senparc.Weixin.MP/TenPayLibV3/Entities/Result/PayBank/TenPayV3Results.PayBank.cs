f6d427035084946da7ab92261418b050462a9401
JeffreySu <www.jeffrey.su@gmail.com>
2018-04-09 20:09:46 +0800

    TenPayV3Result.PayBank.cs 更名为 TenPayV3Results.PayBank.cs

--- src/Senparc.Weixin.MP/Senparc.Weixin.MP/TenPayLibV3/Entities/Result/PayBank/TenPayV3Results.PayBank.cs
+++ src/Senparc.Weixin.MP/Senparc.Weixin.MP/TenPayLibV3/Entities/Result/PayBank/TenPayV3Results.PayBank.cs
@@ -21,12 +21,15 @@ Detail: https://github.com/JeffreySu/WeiXinMPSDK/blob/master/license.md
 /*----------------------------------------------------------------
     Copyright (C) 2018 Senparc
  
-    文件名：TenPayV3Result.PayBank.cs
+    文件名：TenPayV3Results.PayBank.cs
     文件功能描述：微信支付V3返回结果 - 付款到银行卡
     
     
     创建标识：Senparc - 20180409
     
+    修改标识：Senparc - 20180409
+    修改描述：TenPayV3Result.PayBank.cs 更名为 TenPayV3Results.PayBank.cs
+
 ----------------------------------------------------------------*/
 
 using System.Collections.Generic;
@@ -135,4 +138,129 @@ namespace Senparc.Weixin.MP.TenPayLibV3
         }
     }
 
+
+    /// <summary>
+    /// 查询付款到银行卡返回结果
+    /// </summary>
+    public class QueryBankResult : TenPayV3Result
+    {
+        #region 以下字段在return_code为SUCCESS的时候有返回
+
+        /// <summary>
+        /// <para>业务结果</para>
+        /// <para>SUCCESS/FAIL，注意：当状态为FAIL时，存在业务结果未明确的情况，所以如果状态y为FAIL，请务必通过查询接口确认此次付款的结果（关注错误码err_code字段）。如果要继续进行这笔付款，请务必用原商户订单号和原参数来重入此接口。</para>
+        /// </summary>
+        public string result_code { get; set; }
+
+        /// <summary>
+        /// <para>错误代码</para>
+        /// <para>错误码信息，注意：出现未明确的错误码时，如（SYSTEMERROR）等，请务必用原商户订单号重试，或通过查询接口确认此次付款的结果</para>
+        /// </summary>
+        public string err_code { get; set; }
+
+        /// <summary>
+        /// 错误代码描述
+        /// </summary>
+        public string err_code_des { get; set; }
+
+        #endregion
+
+        #region 以下字段在return_code 和result_code都为SUCCESS的时候有返回
+
+        /// <summary>
+        /// 商户号
+        /// </summary>
+        public string mch_id { get; set; }
+        /// <summary>
+        /// 商户企业付款单号，需要保持唯一
+        /// </summary>
+        public string partner_trade_no { get; set; }
+        /// <summary>
+        /// 微信企业付款单号
+        /// </summary>
+        public string payment_no { get; set; }
+        /// <summary>
+        /// 收款用户银行卡号(MD5加密)
+        /// </summary>
+        public string bank_no_md5 { get; set; }
+        /// <summary>
+        /// 收款人真实姓名（MD5加密）
+        /// </summary>
+        public string true_name_md5 { get; set; }
+        /// <summary>
+        /// 代付订单金额RMB：分
+        /// </summary>
+        public int amount { get; set; }
+        /// <summary>
+        /// <para>代付单状态</para>
+        /// <para>代付订单状态：PROCESSING（处理中，如有明确失败，则返回额外失败原因；否则没有错误原因）</para>
+        /// <para>SUCCESS（付款成功）</para>
+        /// <para>FAILED（付款失败,需要替换付款单号重新发起付款）</para>
+        /// <para>BANK_FAIL（银行退票，订单状态由付款成功流转至退票,退票时付款金额和手续费会自动退还）</para>
+        /// </summary>
+        public string status { get; set; }
+        /// <summary>
+        /// 手续费订单金额 RMB：分
+        /// </summary>
+        public int cmms_amt { get; set; }
+        /// <summary>
+        /// 商户下单时间（微信侧订单创建时间）
+        /// </summary>
+        public string create_time { get; set; }
+        /// <summary>
+        /// 成功付款时间（但无法保证银行不会退票）
+        /// </summary>
+        public string pay_succ_time { get; set; }
+        /// <summary>
+        /// 订单失败原因（如：余额不足）
+        /// </summary>
+        public string reason { get; set; }
+
+        #endregion
+
+        /*
+         * 错误码
+        错误代码	描述	解决方案
+        INVALID_REQUEST	无效的请求，商户系统异常导致，商户权限异常、证书错误、频率限制等	使用原单号以及原请求参数重试
+        PARAM_ERROR	参数错误	按照err_msg指定参数错误信息，修改相应参数
+        SIGNERROR	签名错误	签名前没有按照要求进行排序。没有使用商户平台设置的密钥进行签名，参数有空格或者进行了encode后进行签名
+        ORDERNOTEXIST	订单不存在	确认订单号是否发起过请求
+        SYSTEMERROR	系统繁忙，请稍后重试	使用原单号以及原请求参数重试
+        SUCCESS	Wx侧查询成功	 
+        */
+
+        /// <summary>
+        /// 付款到银行卡返回结果 构造函数
+        /// </summary>
+        /// <param name="resultXml"></param>
+        public QueryBankResult(string resultXml) : base(resultXml)
+        {
+            if (base.IsReturnCodeSuccess())
+            {
+                result_code = GetXmlValue("result_code") ?? "";
+                err_code = GetXmlValue("err_code") ?? "";
+                err_code_des = GetXmlValue("err_code_des") ?? "";
+
+                if (this.IsResultCodeSuccess())
+                {
+                    mch_id = GetXmlValue("mch_id") ?? "";
+                    partner_trade_no = GetXmlValue("partner_trade_no") ?? "";
+                    payment_no = GetXmlValue("payment_no") ?? "";
+                    bank_no_md5 = GetXmlValue("bank_no_md5") ?? "";
+                    true_name_md5 = GetXmlValue("true_name_md5") ?? "";
+                    amount = int.Parse(GetXmlValue("amount"));//必填
+                    status = GetXmlValue("status") ?? "";
+                    cmms_amt = int.Parse(GetXmlValue("cmms_amt"));//必填
+                    create_time = GetXmlValue("create_time") ?? "";
+                    pay_succ_time = GetXmlValue("pay_succ_time") ?? "";
+                    reason = GetXmlValue("reason") ?? "";
+                }
+            }
+        }
+
+        public bool IsResultCodeSuccess()
+        {
+            return result_code == "SUCCESS";
+        }
+    }
 }

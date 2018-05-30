698889199c71a12ad03bc4c61a6bb32edab86484
zhcnlei <zhcnlei@live.com>
2018-05-30 10:01:03 +0800

    获取用户基本信息（包括UnionID机制）API 的 ResultJson 模型增加 subscribe_scene（用户关注的渠道来源）字段。

--- src/Senparc.Weixin.MP/Senparc.Weixin.MP/AdvancedAPIs/User/UserJson/UserInfoJson.cs
+++ src/Senparc.Weixin.MP/Senparc.Weixin.MP/AdvancedAPIs/User/UserJson/UserInfoJson.cs
@@ -93,6 +93,10 @@ namespace Senparc.Weixin.MP.AdvancedAPIs.User
         /// </summary>
         public int[] tagid_list { get; set; }
         /// <summary>
+        /// 返回用户关注的渠道来源，ADD_SCENE_SEARCH 公众号搜索，ADD_SCENE_ACCOUNT_MIGRATION 公众号迁移，ADD_SCENE_PROFILE_CARD 名片分享，ADD_SCENE_QR_CODE 扫描二维码，ADD_SCENEPROFILE LINK 图文页内名称点击，ADD_SCENE_PROFILE_ITEM 图文页右上角菜单，ADD_SCENE_PAID 支付后关注，ADD_SCENE_OTHERS 其他
+        /// </summary>
+        public string subscribe_scene { get; set; }
+        /// <summary>
         /// 二维码扫码场景（开发者自定义）
         /// </summary>
         public int qr_scene { get; set; }

332b63a95d8a77ea251d3d3e2422b4a41896ab3d
JeffreySu <www.jeffrey.su@gmail.com>
2018-06-01 18:23:11 +0800

    移植 DateTimeHelper

--- src/Senparc.Weixin/Senparc.Weixin/Helpers/DateTimeHelper.cs
+++ src/Senparc.Weixin/Senparc.Weixin/Helpers/DateTimeHelper.cs
@@ -40,37 +40,9 @@ namespace Senparc.Weixin.Helpers
     /// <summary>
     /// 微信日期处理帮助类
     /// </summary>
-    public static class DateTimeHelper
+    [Obsolete("请使用 Senparc.CO2NET.Helpers.DateTimeHelper 类")]
+    public class DateTimeHelper : CO2NET.Helpers.DateTimeHelper
     {
-        public readonly static DateTime BaseTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);//Unix起始时间
-
-        /// <summary>
-        /// 转换微信DateTime时间到C#时间
-        /// </summary>
-        /// <param name="dateTimeFromXml">微信DateTime</param>
-        /// <returns></returns>
-        public static DateTime GetDateTimeFromXml(long dateTimeFromXml)
-        {
-            return BaseTime.AddSeconds(dateTimeFromXml).ToLocalTime();
-        }
-        /// <summary>
-        /// 转换微信DateTime时间到C#时间
-        /// </summary>
-        /// <param name="dateTimeFromXml">微信DateTime</param>
-        /// <returns></returns>
-        public static DateTime GetDateTimeFromXml(string dateTimeFromXml)
-        {
-            return GetDateTimeFromXml(long.Parse(dateTimeFromXml));
-        }
-
-        /// <summary>
-        /// 获取微信DateTime（UNIX时间戳）
-        /// </summary>
-        /// <param name="dateTime">时间</param>
-        /// <returns></returns>
-        public static long GetWeixinDateTime(DateTime dateTime)
-        {
-            return (long)(dateTime.ToUniversalTime() - BaseTime).TotalSeconds;
-        }
+        
     }
 }

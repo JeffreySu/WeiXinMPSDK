db558dd250f8ec0c42c21d7d092a221f43c6174e
JeffreySu <www.jeffrey.su@gmail.com>
2018-06-02 18:55:01 +0800

    使用CO2NET.Config.TIME_OUT

--- src/Senparc.Weixin/Senparc.Weixin/Config.cs
+++ src/Senparc.Weixin/Senparc.Weixin/Config.cs
@@ -48,16 +48,10 @@ using System.Threading;
 namespace Senparc.Weixin
 {
     /// <summary>
-    /// 全局设置
+    /// Senparc.Weixin 全局设置
     /// </summary>
     public static class Config
     {
-        /// <summary>
-        /// 请求超时设置（以毫秒为单位），默认为10秒。
-        /// 说明：此处常量专为提供给方法的参数的默认值，不是方法内所有请求的默认超时时间。
-        /// </summary>
-        public const int TIME_OUT = 10000;
-
         private static bool _isDebug = false;//TODO:需要考虑分布式的情况，后期需要储存在缓存中
 
         /// <summary>

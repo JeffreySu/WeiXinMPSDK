a60b61558fcc191d1680dd90a4813ad0864bb6de
JeffreySu <www.jeffrey.su@gmail.com>
2018-06-21 21:10:35 +0800

    add Register.RegisterDomainCache()

--- src/Senparc.Weixin.Cache/Senparc.Weixin.Cache.Redis/Register.cs
+++ src/Senparc.Weixin.Cache/Senparc.Weixin.Cache.Redis/Register.cs
@@ -1,19 +1,31 @@
-﻿#if !NET45
+﻿#if NETCOREAPP2_0 || NETCOREAPP2_1
 using Microsoft.AspNetCore.Builder;
+#endif
 
 namespace Senparc.Weixin.Cache.Redis
 {
     public static class Register
     {
+#if NETCOREAPP2_0 || NETCOREAPP2_1
+
         /// <summary>
         /// 注册 Senparc.Weixin.Cache.Memcached
         /// </summary>
         /// <param name="app"></param>
         public static IApplicationBuilder UseSenparcWeixinCacheRedis(this IApplicationBuilder app)
         {
-            var cache = RedisContainerCacheStrategy.Instance;
+            RegisterDomainCache();
             return app;
+        }
+#endif
+
+
+        /// <summary>
+        /// 注册领域缓存
+        /// </summary>
+        public static void RegisterDomainCache()
+        {
+            var cache = RedisContainerCacheStrategy.Instance;
         }
     }
 }
-#endif

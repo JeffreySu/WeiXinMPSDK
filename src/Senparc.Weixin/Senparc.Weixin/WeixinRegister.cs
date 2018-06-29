067a6b4376de5fb566c9e7579513cfd3d0e84cdd
JeffreySu <www.jeffrey.su@gmail.com>
2018-06-29 10:12:13 +0800

    v5.0.3.1 修复 WeixinRegister.UseSenparcWeixin() 方法的 IsDebug 设置问题 感谢@lishewen #1253

--- src/Senparc.Weixin/Senparc.Weixin/WeixinRegister.cs
+++ src/Senparc.Weixin/Senparc.Weixin/WeixinRegister.cs
@@ -14,6 +14,9 @@
     修改标识：Senparc - 20180607
     修改描述：Register 改名为 WeixinRegister
 
+    修改标识：Senparc - 201806029
+    修改描述：v5.0.3.1 修复 WeixinRegister.UseSenparcWeixin() 方法的 IsDebug 设置问题
+
 ----------------------------------------------------------------*/
 
 #if NETCOREAPP2_0 || NETCOREAPP2_1
@@ -40,12 +43,12 @@ namespace Senparc.Weixin
         /// <param name="registerService"></param>
         /// <param name="senparcWeixinSetting"></param>
         /// <param name="isDebug"></param>
-        /// <param name="containerCacheStrategies">需要注册的扩展Container缓存策略（LocalContainerCacheStrategy已经自动注册）</param>
+        /// <param name="containerCacheStrategiesFunc">需要注册的扩展Container缓存策略（LocalContainerCacheStrategy已经自动注册）</param>
         /// <returns></returns>
         public static IRegisterService UseSenparcWeixin(this IRegisterService registerService, SenparcWeixinSetting senparcWeixinSetting, bool isDebug, Func<IList<IDomainExtensionCacheStrategy>> containerCacheStrategiesFunc)
         {
             //Senparc.Weixin SDK 配置
-            Senparc.Weixin.Config.IsDebug = true;
+            Senparc.Weixin.Config.IsDebug = isDebug;
             Senparc.Weixin.Config.DefaultSenparcWeixinSetting = senparcWeixinSetting;
 
             // 微信的 本地 缓存

0d23752cf5db43dc3a7c43eeb3aaa300ca4d2954
JeffreySu <www.jeffrey.su@gmail.com>
2018-07-07 19:50:20 +0800

    Open v3.0.9 Container 的 Register() 的微信参数自动添加到 Config.SenparcWeixinSetting.Items 下；单元测试通过。

--- src/Senparc.Weixin.MP/Senparc.Weixin.MP/TenPayLibV3/TenPayV3Info.cs
+++ src/Senparc.Weixin.MP/Senparc.Weixin.MP/TenPayLibV3/TenPayV3Info.cs
@@ -87,7 +87,7 @@ namespace Senparc.Weixin.MP.TenPayLibV3
         /// 微信支付 V3 参数 构造函数
         /// </summary>
         /// <param name="senparcWeixinSetting">已经填充过微信支付（旧版本）参数的 SenparcWeixinSetting 对象</param>
-        public TenPayInfo(SenparcWeixinSetting senparcWeixinSetting)
+        public TenPayV3Info(SenparcWeixinSetting senparcWeixinSetting)
             : this(senparcWeixinSetting.TenPayV3_AppId,
                   senparcWeixinSetting.TenPayV3_AppSecret,
                   senparcWeixinSetting.TenPayV3_MchId,

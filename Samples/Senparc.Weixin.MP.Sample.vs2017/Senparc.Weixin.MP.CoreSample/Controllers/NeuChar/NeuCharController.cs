63c26b8b683a382f74fe72ed787d6f24e6fee472
JeffreySu <www.jeffrey.su@gmail.com>
2018-08-15 23:29:22 +0800

    添加NeuChar同步接口：NeuCharController

---
+++ Samples/Senparc.Weixin.MP.Sample.vs2017/Senparc.Weixin.MP.CoreSample/Controllers/NeuChar/NeuCharController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senparc.Weixin.MP.CoreSample.Controllers.NeuChar
+{
+    /// <summary>
+    /// NeuChar 处理程序
+    /// </summary>
+    public class NeuCharController : Controller
+    {
+        [HttpPost]
+        public IActionResult Sync(string json)
+        {
+            //TODO:json可加密
+
+            //TODO：进行保存
+
+            return Json(new { msg = "ok" });
+        }
+    }
+}

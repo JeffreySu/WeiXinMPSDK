132a6ae12218b989cba26c5420e5eca7853414d8
JeffreySu <www.jeffrey.su@gmail.com>
2018-08-15 21:55:44 +0800

    初始化NeuChar SDK

---
+++ NeuChar/Senparc.NeuChar/BaseModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Senparc.NeuChar
+{
+    /// <summary>
+    /// 基础模型
+    /// </summary>
+    public class BaseModel
+    {
+        public string Version { get; set; }
+        public object ApiData { get; set; }
+        public object ApiDataKey { get; set; }
+        public object ExtData { get; set; }
+        public object ExtDataKey { get; set; }
+        public DataType DataType { get; set; }
+        public ApiType ApiType { get; set; }
+    }
+}
---
+++ NeuChar/Senparc.NeuChar/Enums.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Senparc.NeuChar
+{
+    public enum DataType
+    {
+        List,
+        Unique
+    }
+
+    public enum ApiType
+    {
+        AccessToken,
+        Normal
+    }
+}

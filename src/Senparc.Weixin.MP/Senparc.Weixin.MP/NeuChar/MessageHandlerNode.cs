22d6cc230d448b798d9818bbd800281adf5a88fb
JeffreySu <www.jeffrey.su@gmail.com>
2018-08-19 22:25:12 +0800

    在MP中创建MessageHandlerNode神经节点，用于MessageHandler的处理

---
+++ src/Senparc.Weixin.MP/Senparc.Weixin.MP/NeuChar/MessageHandlerNode.cs
@@ -0,0 +1,18 @@
+﻿using Senparc.NeuChar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.Weixin.MP.NeuChar
+{
+    /// <summary>
+    /// MessageHandler 的神经节点
+    /// </summary>
+    public class MessageHandlerNode : BaseNeuralNode
+    {
+        public override string Version { get; set; }
+
+    }
+}

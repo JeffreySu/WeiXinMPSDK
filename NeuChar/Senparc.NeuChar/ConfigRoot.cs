c74773f975a22b64ccb2ff05c049d1006773cafd
Jeffrey Su <zsu@senparc.com>
2018-08-29 22:04:33 +0800

    add ConfigRoot

---
+++ NeuChar/Senparc.NeuChar/ConfigRoot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.NeuChar
+{
+    /// <summary>
+    /// 用于传入设置的基础模型
+    /// </summary>
+    public class ConfigRoot
+    {
+        public string Version { get; set; }
+        public string Name { get; set; }
+        public string Guid { get; set; }
+        public string Key { get; set; }
+
+        public string Configs { get; set; }
+    }
+
+}

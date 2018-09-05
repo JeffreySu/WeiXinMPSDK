498036ccceafb93582600ce90bd622ce9b954aa0
JeffreySu <www.jeffrey.su@gmail.com>
2018-09-05 17:24:07 +0800

    支持 NeuChar v0.0.6.1，提供API自动处理能力

---
+++ src/Senparc.Weixin.MP/Senparc.Weixin.MP/AdvancedAPIs/MpApiEnlighten.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Senparc.NeuChar.ApiHandlers;
+
+
+namespace Senparc.Weixin.MP.AdvancedAPIs
+{
+    public class MpApiEnlighten : ApiEnlighten
+    {
+    }
+}

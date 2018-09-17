35d0814a9c0874c0744443f4d2bf6a4d516d65b8
JeffreySu <www.jeffrey.su@gmail.com>
2018-09-17 19:34:30 +0800

    更新到NeuChar 0.0.9.6（测试）

--- src/Senparc.Weixin/Senparc.Weixin/WeixinRegister.cs
+++ src/Senparc.Weixin/Senparc.Weixin/WeixinRegister.cs
@@ -124,6 +124,10 @@ namespace Senparc.Weixin
 
             /* 扩展缓存注册结束 */
 
+            //ApiBind 自动扫描
+            Senparc.NeuChar.Register.RegisterApiBind();
+
+
             return registerService;
         }
     }

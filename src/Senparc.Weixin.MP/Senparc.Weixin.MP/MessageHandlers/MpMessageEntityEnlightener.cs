c9132dd040fcc10a2b35fcfec29a734ff11e51f0
JeffreySu <www.jeffrey.su@gmail.com>
2018-09-23 23:57:44 +0800

    MP 更新 NeuChar

--- src/Senparc.Weixin.MP/Senparc.Weixin.MP/MessageHandlers/MpMessageEntityEnlightener.cs
+++ src/Senparc.Weixin.MP/Senparc.Weixin.MP/MessageHandlers/MpMessageEntityEnlightener.cs
@@ -1,4 +1,5 @@
 ﻿using Senparc.NeuChar.Entities;
+using Senparc.NeuChar.Exceptions;
 using Senparc.NeuChar.MessageHandlers;
 using Senparc.Weixin.MP.Entities;
 using System;
@@ -12,34 +13,100 @@ namespace Senparc.Weixin.MP.MessageHandlers
     /// <summary>
     /// 公众号 MessageEntityEnlightener
     /// </summary>
-    public class MpMessageEntityEnlightener
+    public class MpMessageEntityEnlightener : MessageEntityEnlightener
     {
         //说明：此处不需要强制使用单例模式
 
         /// <summary>
         /// MpMessageEntityEnlightener 全局对象
         /// </summary>
-        public static MessageEntityEnlightener Instance = new MessageEntityEnlightener(NeuChar.PlatformType.WeChat_OfficialAccount)
+        public static MessageEntityEnlightener Instance = new MpMessageEntityEnlightener(NeuChar.PlatformType.WeChat_OfficialAccount);
+
+        MpMessageEntityEnlightener(NeuChar.PlatformType platformType) : base(platformType) { }
+
+        public override IRequestMessageEvent NewRequestMessageEvent()
         {
-            NewRequestMessageText = () => new RequestMessageText(),
-            NewRequestMessageLocation = () => new RequestMessageLocation(),
-            NewRequestMessageImage = () => new RequestMessageImage(),
-            NewRequestMessageVoice = () => new RequestMessageVoice(),
-            NewRequestMessageVideo = () => new RequestMessageVideo(),
-            NewRequestMessageLink = () => new RequestMessageLink(),
-            NewRequestMessageShortVideo = () => new RequestMessageShortVideo(),
-            NewRequestMessageEvent = () => new RequestMessageEventBase(),
-            NewRequestMessageFile = () => new RequestMessageFile(),
+            return new RequestMessageEventBase();
+        }
 
+        public override IRequestMessageFile NewRequestMessageFile()
+        {
+            return new RequestMessageFile();
+        }
 
-            NewResponseMessageText = () => new ResponseMessageText(),
-            NewResponseMessageNews = () => new ResponseMessageNews(),
-            NewResponseMessageMusic = () => new ResponseMessageMusic(),
-            NewResponseMessageImage = () => new ResponseMessageImage(),
-            NewResponseMessageVoice = () => new ResponseMessageVoice(),
-            NewResponseMessageVideo = () => new ResponseMessageVideo(),
-            NewResponseMessageTransfer_Customer_Service = () => new ResponseMessageTransfer_Customer_Service(),
+        public override IRequestMessageImage NewRequestMessageImage()
+        {
+            return new RequestMessageImage();
+        }
 
-        };
+        public override IRequestMessageLink NewRequestMessageLink()
+        {
+            return new RequestMessageLink();
+        }
+
+        public override IRequestMessageLocation NewRequestMessageLocation()
+        {
+            return new RequestMessageLocation();
+        }
+
+        public override IRequestMessageShortVideo NewRequestMessageShortVideo()
+        {
+            return new RequestMessageShortVideo();
+        }
+
+        public override IRequestMessageText NewRequestMessageText()
+        {
+            return new RequestMessageText();
+        }
+
+        public override IRequestMessageVideo NewRequestMessageVideo()
+        {
+            return new RequestMessageVideo();
+        }
+
+        public override IRequestMessageVoice NewRequestMessageVoice()
+        {
+            return new RequestMessageVoice();
+        }
+
+        public override IResponseMessageImage NewResponseMessageImage()
+        {
+            return new ResponseMessageImage();
+        }
+
+        public override IResponseMessageMpNews NewResponseMessageMpNews()
+        {
+            throw new MessageHandlerException("微信公众号不支持 IResponseMessageMpNews 响应类型");
+        }
+
+        public override IResponseMessageMusic NewResponseMessageMusic()
+        {
+            return new ResponseMessageMusic();
+        }
+
+        public override IResponseMessageNews NewResponseMessageNews()
+        {
+            return new ResponseMessageNews();
+        }
+
+        public override IResponseMessageText NewResponseMessageText()
+        {
+            return new ResponseMessageText();
+        }
+
+        public override IResponseMessageTransfer_Customer_Service NewResponseMessageTransfer_Customer_Service()
+        {
+            return new ResponseMessageTransfer_Customer_Service();
+        }
+
+        public override IResponseMessageVideo NewResponseMessageVideo()
+        {
+            return new ResponseMessageVideo();
+        }
+
+        public override IResponseMessageVoice NewResponseMessageVoice()
+        {
+            return new ResponseMessageVoice();
+        }
     }
 }

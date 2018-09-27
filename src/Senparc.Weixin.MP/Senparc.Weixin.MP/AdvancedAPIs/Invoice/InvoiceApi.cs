8ea2b6635343ad7bdddd12ab1d3caf4921d3b8c3
晨璇 韩 <chan@senparc.com>
2018-09-27 18:20:33 +0800

    添加 UpdateStatusAsync

--- src/Senparc.Weixin.MP/Senparc.Weixin.MP/AdvancedAPIs/Invoice/InvoiceApi.cs
+++ src/Senparc.Weixin.MP/Senparc.Weixin.MP/AdvancedAPIs/Invoice/InvoiceApi.cs
@@ -15,6 +15,9 @@ namespace Senparc.Weixin.MP.AdvancedAPIs
     /// </summary>
     public static class InvoiceApi
     {
+        #region 同步方法
+
+
         #region 非税票据 https://mp.weixin.qq.com/wiki?t=resource/res_main&id=21530623533CgUdj
 
         /// <summary>
@@ -47,7 +50,41 @@ namespace Senparc.Weixin.MP.AdvancedAPIs
 
 
         #endregion
+        #endregion
 
+#if !NET35 && !NET40
+        #region 异步方法
+
+        /// <summary>
+        /// 【异步方法】更新电子票据状态
+        /// </summary>
+        /// <param name="accessTokenOrAppId"></param>
+        /// <param name="cardId"></param>
+        /// <param name="code"></param>
+        /// <param name="reimburseStatus"></param>
+        /// <param name="timeOut"></param>
+        /// <returns></returns>
+        [ApiBind(NeuChar.PlatformType.WeChat_OfficialAccount, "CardApi.UpdateStatusAsync", true)]
+        public static async Task<WxJsonResult> UpdateStatusAsync(string accessTokenOrAppId, string cardId, string code, string reimburseStatus, int timeOut = Config.TIME_OUT)
+        {
+            return await ApiHandlerWapper.TryCommonApiAsync(async accessToken =>
+            {
+                var urlFormat = string.Format(Config.ApiMpHost + "/card/invoice/platform/updatestatus?access_token={0}", accessToken.AsUrlData());
+
+                var data = new
+                {
+                    card_id = cardId,
+                    code = code,
+                    reimburse_status = reimburseStatus
+                };
+
+                return await CommonJsonSend.SendAsync<WxJsonResult>(null, urlFormat, data, timeOut: timeOut);
+
+            }, accessTokenOrAppId);
+        }
+
+        #endregion
+#endif
 
     }
 }

6eeceb7927df0a79440734b35094a95c1add5eb2
Jeffrey Su <zsu@senparc.com>
2018-11-08 23:29:36 +0800

    modify

--- Samples/Senparc.Weixin.MP.Sample.vs2017/Senparc.Weixin.MP.CoreSample/Controllers/MenuController.cs
+++ Samples/Senparc.Weixin.MP.Sample.vs2017/Senparc.Weixin.MP.CoreSample/Controllers/MenuController.cs
@@ -8,7 +8,6 @@
     创建标识：Senparc - 20150312
 ----------------------------------------------------------------*/
 
-//PDBMARK_FILE MP
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;

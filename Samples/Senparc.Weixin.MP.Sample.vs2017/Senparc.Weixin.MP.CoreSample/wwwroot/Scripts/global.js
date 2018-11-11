d9e2fe32c5c7a788b616bd4e10b85544315d817f
Jeffrey Su <zsu@senparc.com>
2018-11-11 17:39:10 +0800

    修改Demo

--- Samples/Senparc.Weixin.MP.Sample.vs2017/Senparc.Weixin.MP.CoreSample/wwwroot/Scripts/global.js
+++ Samples/Senparc.Weixin.MP.Sample.vs2017/Senparc.Weixin.MP.CoreSample/wwwroot/Scripts/global.js
@@ -6,6 +6,16 @@
     }, function () {
         $(this).find('ul.nav-sub-catalog').hide();
     });
+
+    var noticeareaHeight = $('#noticearea').height();
+    var noticeareaHeight_shrink = noticeareaHeight * 2 / 3;
+    $('#noticearea').height(noticeareaHeight_shrink);
+    $('#noticearea').hover(function () {
+        $('#noticearea').animate({ height: noticeareaHeight });
+        //$('#noticearea').css('position', 'absolute');
+    }, function () {
+        $('#noticearea').animate({ height: noticeareaHeight_shrink });
+    });
 });
 
 $(function () {

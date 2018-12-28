caaa9be8dedff25f668a950a7b7b3c1b567f20c0
JeffreySu <www.jeffrey.su@gmail.com>
2018-12-28 18:01:00 +0800

    update nuget

--- src/Senparc.Weixin.MP/Senparc.WeixinTests/WeixinRegisterTests.cs
+++ src/Senparc.Weixin.MP/Senparc.WeixinTests/WeixinRegisterTests.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Senparc.WeixinTests
@@ -8,5 +9,13 @@ namespace Senparc.WeixinTests
     [TestClass]
     public class WeixinRegisterTests
     {
+        [TestMethod]
+        public void MyTestMethod()
+        {
+            var pathSeparator = Path.DirectorySeparatorChar.ToString();
+            var altPathSeparator = Path.AltDirectorySeparatorChar.ToString();
+            Console.WriteLine(pathSeparator);
+            Console.WriteLine(altPathSeparator);
+        }
     }
 }

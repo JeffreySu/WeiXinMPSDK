5b623039f09a8fd61f2583ab38c2b31ecc65f06b
Jeffrey Su <zsu@senparc.com>
2019-01-08 21:04:09 +0800

    创建Console Demo

---
+++ Samples/Senparc.Weixin.MP.Sample.Consoles/Senparc.Weixin.MP.Sample.Consoles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Senparc.Weixin.MP.Sample.Consoles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

1fda503b66db2836f574786bf22aeb165fa781ba
JeffreySu <www.jeffrey.su@gmail.com>
2019-01-23 22:17:54 +0800

    .net 4.5 通过“子程序 Oauth”单元测试 #1552

--- src/Senparc.Weixin.MP/Senparc.WeixinTests/Utilities/HttpUtility/UrlUtilityTests.cs
+++ src/Senparc.Weixin.MP/Senparc.WeixinTests/Utilities/HttpUtility/UrlUtilityTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using Senparc.Weixin.HttpUtility;
 using System;
 using System.Collections.Generic;
@@ -22,12 +23,25 @@ namespace Senparc.Weixin.HttpUtility.Tests
         public void GenerateOAuthCallbackUrlTest()
         {
 #if NET45
-            Func<string, string, HttpContextBase> getHttpContextBase = (url, queryString) =>
-            {
-                var httpContext = new HttpContext(new HttpRequest("Senparc", url, queryString), new HttpResponse(null));
-                var httpContextBase = new HttpContextWrapper(httpContext);
-                return httpContextBase;
-            };
+            Func<string, string, string, HttpContextBase> getHttpContextBase = (url, queryString, baseUrl) =>
+             {
+                 var context = new Mock<HttpContextBase>();
+                 var request = new Mock<HttpRequestBase>();
+                 var response = new Mock<HttpResponseBase>();
+
+                 request.Setup(r => r.Url).Returns(new Uri(url));
+                 request.Setup(r => r.ApplicationPath).Returns(baseUrl);
+
+                 context.Setup(c => c.Request).Returns(request.Object);
+                 context.Setup(c => c.Response).Returns(response.Object);
+                 context.Setup(c => c.Response).Returns(response.Object);
+                 return context.Object;
+
+                 var httpContext = new HttpContext(new HttpRequest("Senparc", url, queryString), new HttpResponse(null));
+                 var httpContextBase = new HttpContextWrapper(httpContext);
+
+                 return httpContextBase;
+             };
 #else
             Func<string, string, string, HttpContext> getHttpContextBase = (url, queryString, baseUrl) =>
              {
@@ -108,13 +122,17 @@ namespace Senparc.Weixin.HttpUtility.Tests
             {
                 virtualPath = "/VirtualPath";
                 var httpContext = getHttpContextBase("https://sdk.weixin.senparc.com:1433/VirtualPath/Home/Index?a=1&b=2&c=3-1", "a=1&b=2&c=3-1", virtualPath);
-                //Console.WriteLine(httpContext.Request.AbsoluteUri());
+                //Console.WriteLine("\r\nhttpContext.Request.AbsoluteUri():"+httpContext.Request.AbsoluteUri());
                 var callbackUrl = UrlUtility.GenerateOAuthCallbackUrl(httpContext, "/TenpayV3/OAuthCallback");
                 Console.WriteLine("\r\n带参数、带端口 HTTPS Url：" + callbackUrl);
 
-                //因为模拟会存在偏差（无法自动识别Url中的VirtualPath为特殊的虚拟目录，所以会出现重复），因此以下结果是可以接受的
-                Assert.AreEqual(
-    "https://sdk.weixin.senparc.com:1433/VirtualPath/TenpayV3/OAuthCallback?returnUrl=https%3a%2f%2fsdk.weixin.senparc.com%3a1433%2fVirtualPath%2fVirtualPath%2fHome%2fIndex%3fa%3d1%26b%3d2%26c%3d3-1",
+#if NETSTANDARD2_0
+                //.NET Standard(.NET Core)下因为模拟会存在偏差（无法自动识别Url中的VirtualPath为特殊的虚拟目录，所以会出现重复），因此以下结果是可以接受的
+                var expectedUrl = "https://sdk.weixin.senparc.com:1433/VirtualPath/TenpayV3/OAuthCallback?returnUrl=https%3a%2f%2fsdk.weixin.senparc.com%3a1433%2fVirtualPath%2fVirtualPath%2fHome%2fIndex%3fa%3d1%26b%3d2%26c%3d3-1";
+#else
+                var expectedUrl = "https://sdk.weixin.senparc.com:1433/VirtualPath/TenpayV3/OAuthCallback?returnUrl=https%3a%2f%2fsdk.weixin.senparc.com%3a1433%2fVirtualPath%2fHome%2fIndex%3fa%3d1%26b%3d2%26c%3d3-1";
+#endif
+                Assert.AreEqual(expectedUrl,
     callbackUrl, true);
             }
         }

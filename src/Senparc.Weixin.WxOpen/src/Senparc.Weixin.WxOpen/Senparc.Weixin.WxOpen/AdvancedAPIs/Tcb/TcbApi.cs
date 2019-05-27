eea92630bd3e5bad05d008732ad20391a72649f0
算神 <lishewen@msn.com>
2019-05-27 14:57:36 +0800

    云数据库迁移相关HTTP API

--- src/Senparc.Weixin.WxOpen/src/Senparc.Weixin.WxOpen/Senparc.Weixin.WxOpen/AdvancedAPIs/Tcb/TcbApi.cs
+++ src/Senparc.Weixin.WxOpen/src/Senparc.Weixin.WxOpen/Senparc.Weixin.WxOpen/AdvancedAPIs/Tcb/TcbApi.cs
@@ -36,6 +36,88 @@ namespace Senparc.Weixin.WxOpen.AdvancedAPIs.Tcb
 
             }, accessTokenOrAppId);
         }
+        /// <summary>
+        /// 数据库导入
+        /// </summary>
+        /// <param name="accessTokenOrAppId">接口调用凭证</param>
+        /// <param name="env">云环境ID</param>
+        /// <param name="collection">导入collection名</param>
+        /// <param name="file_path">导入文件路径(导入文件需先上传到同环境的存储中，可使用开发者工具或 HTTP API的上传文件 API上传）</param>
+        /// <param name="file_type">导入文件类型，文件格式参考数据库导入指引中的文件格式部分</param>
+        /// <param name="stop_on_error">是否在遇到错误时停止导入</param>
+        /// <param name="conflict_mode">冲突处理模式</param>
+        /// <param name="timeOut"></param>
+        /// <returns></returns>
+        [ApiBind(NeuChar.PlatformType.WeChat_MiniProgram, "TcbApi.DatabaseMigrateImport", true)]
+        public static WxDatabaseMigrateJsonResult DatabaseMigrateImport(string accessTokenOrAppId, string env, string collection, string file_path, FileType file_type, bool stop_on_error,
+            ConflictMode conflict_mode, int timeOut = Config.TIME_OUT)
+        {
+            return WxOpenApiHandlerWapper.TryCommonApi(accessToken =>
+            {
+                string urlFormat = Config.ApiMpHost + "/tcb/databasemigrateimport?access_token={0}";
+                var postBody = new
+                {
+                    env,
+                    collection,
+                    file_path,
+                    file_type,
+                    stop_on_error,
+                    conflict_mode
+                };
+                return CommonJsonSend.Send<WxDatabaseMigrateJsonResult>(accessToken, urlFormat, postBody, timeOut: timeOut);
+
+            }, accessTokenOrAppId);
+        }
+        /// <summary>
+        /// 数据库导出
+        /// </summary>
+        /// <param name="accessTokenOrAppId">接口调用凭证</param>
+        /// <param name="env">云环境ID</param>
+        /// <param name="file_path">导出文件路径（文件会导出到同环境的云存储中，可使用获取下载链接 API 获取下载链接）</param>
+        /// <param name="file_type">导出文件类型，文件格式参考数据库导入指引中的文件格式部分</param>
+        /// <param name="query">导出条件</param>
+        /// <param name="timeOut"></param>
+        /// <returns></returns>
+        [ApiBind(NeuChar.PlatformType.WeChat_MiniProgram, "TcbApi.DatabaseMigrateExport", true)]
+        public static WxDatabaseMigrateJsonResult DatabaseMigrateExport(string accessTokenOrAppId, string env, string file_path, FileType file_type, string query, int timeOut = Config.TIME_OUT)
+        {
+            return WxOpenApiHandlerWapper.TryCommonApi(accessToken =>
+            {
+                string urlFormat = Config.ApiMpHost + "/tcb/databasemigrateexport?access_token={0}";
+                var postBody = new
+                {
+                    env,
+                    file_path,
+                    file_type,
+                    query
+                };
+                return CommonJsonSend.Send<WxDatabaseMigrateJsonResult>(accessToken, urlFormat, postBody, timeOut: timeOut);
+
+            }, accessTokenOrAppId);
+        }
+        /// <summary>
+        /// 数据库迁移状态查询
+        /// </summary>
+        /// <param name="accessTokenOrAppId">接口调用凭证</param>
+        /// <param name="env">云环境ID</param>
+        /// <param name="job_id">迁移任务ID</param>
+        /// <param name="timeOut"></param>
+        /// <returns></returns>
+        [ApiBind(NeuChar.PlatformType.WeChat_MiniProgram, "TcbApi.DatabaseMigrateQueryInfo", true)]
+        public static WxDatabaseMigrateQueryInfoJsonResult DatabaseMigrateQueryInfo(string accessTokenOrAppId, string env, int job_id, int timeOut = Config.TIME_OUT)
+        {
+            return WxOpenApiHandlerWapper.TryCommonApi(accessToken =>
+            {
+                string urlFormat = Config.ApiMpHost + "/tcb/databasemigratequeryinfo?access_token={0}";
+                var postBody = new
+                {
+                    env,
+                    job_id
+                };
+                return CommonJsonSend.Send<WxDatabaseMigrateQueryInfoJsonResult>(accessToken, urlFormat, postBody, timeOut: timeOut);
+
+            }, accessTokenOrAppId);
+        }
         #endregion
 
         #region 异步方法
@@ -55,6 +137,88 @@ namespace Senparc.Weixin.WxOpen.AdvancedAPIs.Tcb
             {
                 string urlFormat = Config.ApiMpHost + "/tcb/invokecloudfunction?access_token={0}&env=" + env + "&name=" + name;
                 return await CommonJsonSend.SendAsync<WxCloudFunctionJsonResult>(accessToken, urlFormat, postBody, timeOut: timeOut).ConfigureAwait(false);
+
+            }, accessTokenOrAppId).ConfigureAwait(false);
+        }
+        /// <summary>
+        /// 【异步方法】数据库导入
+        /// </summary>
+        /// <param name="accessTokenOrAppId">接口调用凭证</param>
+        /// <param name="env">云环境ID</param>
+        /// <param name="collection">导入collection名</param>
+        /// <param name="file_path">导入文件路径(导入文件需先上传到同环境的存储中，可使用开发者工具或 HTTP API的上传文件 API上传）</param>
+        /// <param name="file_type">导入文件类型，文件格式参考数据库导入指引中的文件格式部分</param>
+        /// <param name="stop_on_error">是否在遇到错误时停止导入</param>
+        /// <param name="conflict_mode">冲突处理模式</param>
+        /// <param name="timeOut"></param>
+        /// <returns></returns>
+        [ApiBind(NeuChar.PlatformType.WeChat_MiniProgram, "TcbApi.DatabaseMigrateImportAsync", true)]
+        public static async Task<WxDatabaseMigrateJsonResult> DatabaseMigrateImportAsync(string accessTokenOrAppId, string env, string collection, string file_path, FileType file_type, bool stop_on_error,
+            ConflictMode conflict_mode, int timeOut = Config.TIME_OUT)
+        {
+            return await WxOpenApiHandlerWapper.TryCommonApiAsync(async accessToken =>
+            {
+                string urlFormat = Config.ApiMpHost + "/tcb/databasemigrateimport?access_token={0}";
+                var postBody = new
+                {
+                    env,
+                    collection,
+                    file_path,
+                    file_type,
+                    stop_on_error,
+                    conflict_mode
+                };
+                return await CommonJsonSend.SendAsync<WxDatabaseMigrateJsonResult>(accessToken, urlFormat, postBody, timeOut: timeOut).ConfigureAwait(false);
+
+            }, accessTokenOrAppId).ConfigureAwait(false);
+        }
+        /// <summary>
+        /// 【异步方法】数据库导出
+        /// </summary>
+        /// <param name="accessTokenOrAppId">接口调用凭证</param>
+        /// <param name="env">云环境ID</param>
+        /// <param name="file_path">导出文件路径（文件会导出到同环境的云存储中，可使用获取下载链接 API 获取下载链接）</param>
+        /// <param name="file_type">导出文件类型，文件格式参考数据库导入指引中的文件格式部分</param>
+        /// <param name="query">导出条件</param>
+        /// <param name="timeOut"></param>
+        /// <returns></returns>
+        [ApiBind(NeuChar.PlatformType.WeChat_MiniProgram, "TcbApi.DatabaseMigrateExportAsync", true)]
+        public static async Task<WxDatabaseMigrateJsonResult> DatabaseMigrateExportAsync(string accessTokenOrAppId, string env, string file_path, FileType file_type, string query, int timeOut = Config.TIME_OUT)
+        {
+            return await WxOpenApiHandlerWapper.TryCommonApiAsync(async accessToken =>
+            {
+                string urlFormat = Config.ApiMpHost + "/tcb/databasemigrateexport?access_token={0}";
+                var postBody = new
+                {
+                    env,
+                    file_path,
+                    file_type,
+                    query
+                };
+                return await CommonJsonSend.SendAsync<WxDatabaseMigrateJsonResult>(accessToken, urlFormat, postBody, timeOut: timeOut).ConfigureAwait(false);
+
+            }, accessTokenOrAppId).ConfigureAwait(false);
+        }
+        /// <summary>
+        /// 【异步方法】数据库迁移状态查询
+        /// </summary>
+        /// <param name="accessTokenOrAppId">接口调用凭证</param>
+        /// <param name="env">云环境ID</param>
+        /// <param name="job_id">迁移任务ID</param>
+        /// <param name="timeOut"></param>
+        /// <returns></returns>
+        [ApiBind(NeuChar.PlatformType.WeChat_MiniProgram, "TcbApi.DatabaseMigrateQueryInfoAsync", true)]
+        public static async Task<WxDatabaseMigrateQueryInfoJsonResult> DatabaseMigrateQueryInfoAsync(string accessTokenOrAppId, string env, int job_id, int timeOut = Config.TIME_OUT)
+        {
+            return await WxOpenApiHandlerWapper.TryCommonApiAsync(async accessToken =>
+            {
+                string urlFormat = Config.ApiMpHost + "/tcb/databasemigratequeryinfo?access_token={0}";
+                var postBody = new
+                {
+                    env,
+                    job_id
+                };
+                return await CommonJsonSend.SendAsync<WxDatabaseMigrateQueryInfoJsonResult>(accessToken, urlFormat, postBody, timeOut: timeOut).ConfigureAwait(false);
 
             }, accessTokenOrAppId).ConfigureAwait(false);
         }
--- src/Senparc.Weixin.WxOpen/src/Senparc.Weixin.WxOpen/Senparc.Weixin.WxOpen/AdvancedAPIs/Tcb/TcbJson/WxDatabaseMigrateJsonResult.cs
+++ src/Senparc.Weixin.WxOpen/src/Senparc.Weixin.WxOpen/Senparc.Weixin.WxOpen/AdvancedAPIs/Tcb/TcbJson/WxDatabaseMigrateJsonResult.cs
@@ -14,4 +14,28 @@ namespace Senparc.Weixin.WxOpen.AdvancedAPIs.Tcb
         /// </summary>
         public int job_id { get; set; }
     }
+
+    public class WxDatabaseMigrateQueryInfoJsonResult : WxJsonResult
+    {
+        /// <summary>
+        /// 导出状态
+        /// </summary>
+        public string status { get; set; }
+        /// <summary>
+        /// 导出成功记录数
+        /// </summary>
+        public int record_success { get; set; }
+        /// <summary>
+        /// 导出失败记录数
+        /// </summary>
+        public int record_fail { get; set; }
+        /// <summary>
+        /// 导出错误信息
+        /// </summary>
+        public string err_msg { get; set; }
+        /// <summary>
+        /// 导出文件下载地址
+        /// </summary>
+        public string file_url { get; set; }
+    }
 }

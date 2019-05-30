b167002b21fe8b31d4444e52cbeb1148d8cb68d4
Jeffrey Su <zsu@senparc.com>
2019-05-30 20:00:40 +0800

    v3.4.6 小程序云开发 HTTP API 相关封装；完善注释 #1787 感谢 @lishewen

--- src/Senparc.Weixin.WxOpen/src/Senparc.Weixin.WxOpen/Senparc.Weixin.WxOpen/AdvancedAPIs/Tcb/TcbJson/WxDatabaseMigrateJsonResult.cs
+++ src/Senparc.Weixin.WxOpen/src/Senparc.Weixin.WxOpen/Senparc.Weixin.WxOpen/AdvancedAPIs/Tcb/TcbJson/WxDatabaseMigrateJsonResult.cs
@@ -1,4 +1,35 @@
-﻿using Senparc.Weixin.Entities;
+﻿#region Apache License Version 2.0
+/*----------------------------------------------------------------
+
+Copyright 2019 Jeffrey Su & Suzhou Senparc Network Technology Co.,Ltd.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+except in compliance with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the
+License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+either express or implied. See the License for the specific language governing permissions
+and limitations under the License.
+
+Detail: https://github.com/JeffreySu/WeiXinMPSDK/blob/master/license.md
+
+----------------------------------------------------------------*/
+#endregion Apache License Version 2.0
+
+/*----------------------------------------------------------------
+    Copyright (C) 2019 Senparc
+    
+    文件名：WxDatabaseMigrateJsonResult.cs
+    文件功能描述：数据库操作记录的各类返回结果
+    
+    
+    创建标识：lishewen - 20190530
+   
+----------------------------------------------------------------*/
+
+using Senparc.Weixin.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +38,9 @@ using System.Threading.Tasks;
 
 namespace Senparc.Weixin.WxOpen.AdvancedAPIs.Tcb
 {
+    /// <summary>
+    /// 数据库导入/导出 返回结果
+    /// </summary>
     public class WxDatabaseMigrateJsonResult : WxJsonResult
     {
         /// <summary>
@@ -15,6 +49,9 @@ namespace Senparc.Weixin.WxOpen.AdvancedAPIs.Tcb
         public int job_id { get; set; }
     }
 
+    /// <summary>
+    /// 数据库迁移状态查询 返回结果
+    /// </summary>
     public class WxDatabaseMigrateQueryInfoJsonResult : WxJsonResult
     {
         /// <summary>

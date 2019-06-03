be4400bf0e4af37ebe2816a4400d599ef9e8c93f
Mc7246 <jiangshans@163.com>
2019-06-03 16:23:05 +0800

    增加小程序名称审核事件，修复小程序添加类目接口参数错误

---
+++ src/Senparc.Weixin.Open/Senparc.Weixin.Open/Entities/Request/RequestMessageNicknameAudit.cs
@@ -0,0 +1,39 @@
+﻿/*----------------------------------------------------------------
+    Copyright (C) 2019 Senparc
+    
+    文件名：RequestMessageUnauthorized.cs
+    文件功能描述：推送取消授权通知
+    
+    
+    创建标识：Senparc - 20150430
+----------------------------------------------------------------*/
+
+namespace Senparc.Weixin.Open
+{
+    public class RequestMessageNicknameAudit : RequestMessageBase
+    {
+        public override RequestInfoType InfoType
+        {
+            get { return RequestInfoType.unauthorized; }
+        }
+
+        public string ToUserName { get; set; }
+
+        public string FromUserName { get; set; }
+
+        /// <summary>
+        /// 结果2失败，3成功
+        /// </summary>
+        public int ret { get; set; }
+
+        /// <summary>
+        /// 小程序名称
+        /// </summary>
+        public string nickname { get; set; }
+
+        /// <summary>
+        /// 驳回原因
+        /// </summary>
+        public string reason { get; set; }
+    }
+}

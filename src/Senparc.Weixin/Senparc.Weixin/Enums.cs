29d928b19f7f04cfd84d42fbf3d939d7bb14b110
webwang <474196974@qq.com>
2019-06-29 16:53:07 +0800

    增加 创建标签 错误返回信息

--- src/Senparc.Weixin/Senparc.Weixin/Enums.cs
+++ src/Senparc.Weixin/Senparc.Weixin/Enums.cs
@@ -222,6 +222,14 @@ namespace Senparc.Weixin
         参数错误invalid_parameter = 61451,
         无效客服账号invalid_kf_account = 61452,
         客服帐号已存在kf_account_exsited = 61453,
+    
+        //创建标签 错误返回信息
+        标签名非法请注意不能和其他标签重名 = 45157,
+        标签名长度超过30个字节 = 45158,
+        创建的标签数过多请注意不能超过100个 = 45056,
+
+        
+
         /// <summary>
         /// 客服帐号名长度超过限制(仅允许10个英文字符，不包括@及@后的公众号的微信号)(invalid kf_acount length)
         /// </summary>

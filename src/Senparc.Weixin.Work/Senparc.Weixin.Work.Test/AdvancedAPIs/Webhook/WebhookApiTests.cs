812d8132d031548f17e2f410c217c66f5e93a809
算神 <lishewen@msn.com>
2019-07-01 10:59:29 +0800

    WebhookApi单元测试 TODO: 测试运行将使用为框架 .NETCoreApp,Version=v1.0 和平台 X86 生成的 DLL。以下 DLL 将不是运行的一部分: Senparc.Weixin.Work.Test.vs2017.dll 适用于 2.2 框架和 AnyCPU 平台。

---
+++ src/Senparc.Weixin.Work/Senparc.Weixin.Work.Test/AdvancedAPIs/Webhook/WebhookApiTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Senparc.Weixin.Work.Test.vs2017.AdvancedAPIs.Webhook
+{
+    [TestClass]
+    public class WebhookApiTests
+    {
+        const string key = "[your key]";
+        [TestMethod]
+        public void SendTestTest()
+        {
+            try
+            {
+                Work.AdvancedAPIs.Webhook.WebhookApi.SendText(key, "测试消息");
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}

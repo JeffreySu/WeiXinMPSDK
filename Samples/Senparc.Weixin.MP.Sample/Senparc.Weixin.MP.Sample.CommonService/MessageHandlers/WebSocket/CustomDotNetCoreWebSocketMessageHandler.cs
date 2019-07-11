093d5483a13c8f2f707aad91692846da4d671ba8
JeffreySu <www.jeffrey.su@gmail.com>
2019-07-11 23:56:08 +0800

    修改小程序Demo

--- Samples/Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample.CommonService/MessageHandlers/WebSocket/CustomDotNetCoreWebSocketMessageHandler.cs
+++ Samples/Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample.CommonService/MessageHandlers/WebSocket/CustomDotNetCoreWebSocketMessageHandler.cs
@@ -64,7 +64,7 @@ namespace Senparc.Weixin.MP.Sample.CommonService.MessageHandlers.WebSocket
                 //await webSocketHandler.SendMessage("FormId：" + formId);
 
                 //群发
-                await webSocketHandler.SendMessage($"[群发消息] [来自 OpenId：{openId}]：{message}" , webSocketHandler.WebSocket.Clients.Others);
+                await webSocketHandler.SendMessage($"[群发消息] [来自 OpenId：{openId}]：{message}" , webSocketHandler.WebSocket.Clients.All);
 
 
                 if (sessionBag == null)

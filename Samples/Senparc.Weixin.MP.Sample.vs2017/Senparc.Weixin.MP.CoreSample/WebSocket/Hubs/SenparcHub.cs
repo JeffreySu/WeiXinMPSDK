f1fac627d64f9b401f23fe0b863319d883cee323
JeffreySu <www.jeffrey.su@gmail.com>
2019-07-11 09:04:41 +0800

    完成WebSocket小程序测试

--- Samples/Senparc.Weixin.MP.Sample.vs2017/Senparc.Weixin.MP.CoreSample/WebSocket/Hubs/SenparcHub.cs
+++ Samples/Senparc.Weixin.MP.Sample.vs2017/Senparc.Weixin.MP.CoreSample/WebSocket/Hubs/SenparcHub.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Senparc.WebSocket.SignalR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,10 +7,10 @@ using System.Threading.Tasks;
 
 namespace Senparc.Weixin.MP.CoreSample.WebSocket.Hubs
 {
-    public class SenparcHub : Hub
+    public class SenparcHub : SenparcWebSocketHubBase
     {
         /// <summary>
-        /// 给网页用 /WebScoket
+        /// 给普通网页用的自定义扩展方法 /WebScoket
         /// </summary>
         /// <param name="user"></param>
         /// <param name="message"></param>
@@ -19,21 +20,21 @@ namespace Senparc.Weixin.MP.CoreSample.WebSocket.Hubs
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
 
-        /// <summary>
-        /// 给小程序用
-        /// </summary>
-        /// <param name="message"></param>
-        /// <returns></returns>
-        public async Task SendText(string message)
-        {
-            await Clients.Caller.SendAsync("ReceiveMessage", "您发送了文字：" + message);
-            await Clients.Caller.SendAsync("ReceiveMessage", "正在处理中...");
+        ///// <summary>
+        ///// 给小程序用
+        ///// </summary>
+        ///// <param name="message"></param>
+        ///// <returns></returns>
+        //public async Task SendText(string message)
+        //{
+        //    await Clients.Caller.SendAsync("ReceiveMessage", "您发送了文字：" + message);
+        //    await Clients.Caller.SendAsync("ReceiveMessage", "正在处理中...");
 
-            await Task.Delay(1000);//模拟停顿1秒
+        //    await Task.Delay(1000);//模拟停顿1秒
 
-            //处理文字
-            var result = string.Concat(message.Reverse());
-            await Clients.Caller.SendAsync("ReceiveMessage", result);
-        }
+        //    //处理文字
+        //    var result = string.Concat(message.Reverse());
+        //    await Clients.Caller.SendAsync("ReceiveMessage", result);
+        //}
     }
 }

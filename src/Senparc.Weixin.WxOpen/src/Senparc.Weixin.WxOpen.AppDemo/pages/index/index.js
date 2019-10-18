73eb7d8af81f6fb346f499f1ef02e31605fab606
JeffreySu <www.jeffrey.su@gmail.com>
2019-10-18 19:17:43 +0800

    Sample v4.6.3 优化小程序 Demo

--- src/Senparc.Weixin.WxOpen/src/Senparc.Weixin.WxOpen.AppDemo/pages/index/index.js
+++ src/Senparc.Weixin.WxOpen/src/Senparc.Weixin.WxOpen.AppDemo/pages/index/index.js
@@ -3,7 +3,7 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Senparc.Weixin SDK Demo v2019.8.19',
+    motto: 'Senparc.Weixin SDK Demo v2019.10.19',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')

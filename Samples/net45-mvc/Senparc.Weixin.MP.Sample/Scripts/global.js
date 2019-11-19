3ddd38e559bae5d504ef378db6e402e119db217c
JeffreySu <www.jeffrey.su@gmail.com>
2019-11-19 14:45:07 +0800

    优化 .net 4.5 Sample，重新提供线上 Demo

--- Samples/net45-mvc/Senparc.Weixin.MP.Sample/Scripts/global.js
+++ Samples/net45-mvc/Senparc.Weixin.MP.Sample/Scripts/global.js
@@ -9,7 +9,7 @@ $(function () {
     });
 
     var noticeareaHeight = $('#noticearea').height();
-    var noticeareaHeight_shrink = noticeareaHeight * 2 / 4;
+    var noticeareaHeight_shrink = noticeareaHeight * 2 / 5;
     $('#noticearea').animate({ height: noticeareaHeight_shrink }, 1000);
     $('#noticearea').hover(function () {
         if (noticeareaHeightChanging) {

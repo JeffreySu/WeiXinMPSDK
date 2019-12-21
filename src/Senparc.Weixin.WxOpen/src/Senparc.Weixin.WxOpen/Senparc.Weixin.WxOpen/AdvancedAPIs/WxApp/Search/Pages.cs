baa0b9602cd24faf819fa35c071ca9592b4d2a48
算神 <lishewen@msn.com>
2019-12-21 14:50:33 +0800

    小程序搜索 - 页面路径收录推送接口 文档地址：https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/search/search.submitPages.html

---
+++ src/Senparc.Weixin.WxOpen/src/Senparc.Weixin.WxOpen/Senparc.Weixin.WxOpen/AdvancedAPIs/WxApp/Search/Pages.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.Weixin.WxOpen.AdvancedAPIs.WxApp.Search
+{
+    public class Page
+    {
+        /// <summary>
+        /// 页面路径
+        /// </summary>
+        public string path { get; set; }
+        /// <summary>
+        /// 页面参数
+        /// </summary>
+        public string query { get; set; }
+    }
+}

b91b8e1701b73dc2cb154821abad1e217c0525c8
算神 <lishewen@msn.com>
2019-12-30 08:57:23 +0800

    新增客户群管理API 文档地址：https://work.weixin.qq.com/api/doc/90000/90135/92119

---
+++ src/Senparc.Weixin.Work/Senparc.Weixin.Work/AdvancedAPIs/External/GroupChatListResult.cs
@@ -0,0 +1,36 @@
+﻿using Senparc.Weixin.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.Weixin.Work.AdvancedAPIs.External
+{
+    /// <summary>
+    /// 获取客户群列表 返回结果
+    /// </summary>
+    public class GroupChatListResult : WorkJsonResult
+    {
+        /// <summary>
+        /// 客户群列表
+        /// </summary>
+        public Group_Chat_List[] group_chat_list { get; set; }
+    }
+
+    public class Group_Chat_List
+    {
+        /// <summary>
+        /// 客户群IDs
+        /// </summary>
+        public string chat_id { get; set; }
+        /// <summary>
+        /// 客户群状态。
+        /// 0 - 正常
+        /// 1 - 跟进人离职
+        /// 2 - 离职继承中
+        /// 3 - 离职继承完成
+        /// </summary>
+        public int status { get; set; }
+    }
+}

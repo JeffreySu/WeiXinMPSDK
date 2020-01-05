456c646f14c806d94dff3b3ec916a7ffaf07b51c
JeffreySu <www.jeffrey.su@gmail.com>
2020-01-05 21:53:45 +0800

    优化AspNet分离

--- src/Senparc.Weixin/Senparc.Weixin/WeixinRegister.cs
+++ src/Senparc.Weixin/Senparc.Weixin/WeixinRegister.cs
@@ -162,7 +162,7 @@ namespace Senparc.Weixin
         }
 
 
-        #region v6.6.102+ 新方法 
+        #region v6.6.102+ 新方法
 
         /// <summary>
         /// 开始 Senparc.Weixin SDK 初始化参数流程
@@ -182,33 +182,34 @@ namespace Senparc.Weixin
             return register;
         }
 
-#if NETSTANDARD2_0 || NETSTANDARD2_1 || NETCOREAPP3_0
-        /// <summary>
-        /// <para>开始包含 CO2NET 注册在内的 Senparc.Weixin SDK 初始化参数流程</para>
-        /// <para>注意：本方法集成了 CON2ET 全局注册以及 Senparc.Weixin SDK 微信注册过程，提供给对代码行数有极限追求的开发者使用，常规情况下为了提高代码可读性和可维护性，并不推荐使用此方法。</para>
-        /// </summary>
-        /// <param name="registerService"></param>
-        /// <param name="senparcWeixinSetting"></param>
-        /// <param name="registerConfigure"></param>
-        /// <returns></returns>
-        public static IRegisterService UseSenparcWeixin(this IApplicationBuilder app,
-#if NETSTANDARD2_0 || NETSTANDARD2_1
-            IHostingEnvironment env,
-#else
-            IWebHostEnvironment env,
-#endif
-            SenparcSetting senparcSetting, SenparcWeixinSetting senparcWeixinSetting, Action<IRegisterService> globalRegisterConfigure, Action<IRegisterService> weixinRegisterConfigure,
-             //CO2NET 全局设置
-             bool autoScanExtensionCacheStrategies = false, Func<IList<IDomainExtensionCacheStrategy>> extensionCacheStrategiesFunc = null
-            )
-        {
-            //注册 CO2NET 全局
-            var register = app.UseSenparcGlobal(env, senparcSetting, globalRegisterConfigure, autoScanExtensionCacheStrategies, extensionCacheStrategiesFunc);
-            //注册微信
-            register.UseSenparcWeixin(senparcWeixinSetting, weixinRegisterConfigure);
-            return register;
-        }
-#endif
+        //从 v6.7.100 开始分离到 Senparc.Weixin.AspNet
+//#if NETSTANDARD2_0 || NETSTANDARD2_1 || NETCOREAPP3_0
+//        /// <summary>
+//        /// <para>开始包含 CO2NET 注册在内的 Senparc.Weixin SDK 初始化参数流程</para>
+//        /// <para>注意：本方法集成了 CON2ET 全局注册以及 Senparc.Weixin SDK 微信注册过程，提供给对代码行数有极限追求的开发者使用，常规情况下为了提高代码可读性和可维护性，并不推荐使用此方法。</para>
+//        /// </summary>
+//        /// <param name="registerService"></param>
+//        /// <param name="senparcWeixinSetting"></param>
+//        /// <param name="registerConfigure"></param>
+//        /// <returns></returns>
+//        public static IRegisterService UseSenparcWeixin(this IApplicationBuilder app,
+//#if NETSTANDARD2_0 || NETSTANDARD2_1
+//            IHostingEnvironment env,
+//#else
+//            IWebHostEnvironment env,
+//#endif
+//            SenparcSetting senparcSetting, SenparcWeixinSetting senparcWeixinSetting, Action<IRegisterService> globalRegisterConfigure, Action<IRegisterService> weixinRegisterConfigure,
+//             //CO2NET 全局设置
+//             bool autoScanExtensionCacheStrategies = false, Func<IList<IDomainExtensionCacheStrategy>> extensionCacheStrategiesFunc = null
+//            )
+//        {
+//            //注册 CO2NET 全局
+//            var register = app.UseSenparcGlobal(env, senparcSetting, globalRegisterConfigure, autoScanExtensionCacheStrategies, extensionCacheStrategiesFunc);
+//            //注册微信
+//            register.UseSenparcWeixin(senparcWeixinSetting, weixinRegisterConfigure);
+//            return register;
+//        }
+//#endif
         #endregion
     }
 }

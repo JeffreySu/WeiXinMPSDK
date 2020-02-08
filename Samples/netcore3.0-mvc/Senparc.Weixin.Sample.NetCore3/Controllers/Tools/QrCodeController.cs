00e2fa2c9720d32403eb287051ad8279f3cf2a96
JeffreySu <www.jeffrey.su@gmail.com>
2020-02-09 01:29:55 +0800

    Sample v5.203.0，添加二维码生成器

--- Samples/netcore3.0-mvc/Senparc.Weixin.Sample.NetCore3/Controllers/Tools/QrCodeController.cs
+++ Samples/netcore3.0-mvc/Senparc.Weixin.Sample.NetCore3/Controllers/Tools/QrCodeController.cs
@@ -1,14 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Senparc.CO2NET.Extensions;
 using Senparc.CO2NET.Trace;
-using Senparc.Weixin.MP.Sample.CommonService.Utilities;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
 using ZXing;
 using ZXing.Common;
 
@@ -37,6 +34,9 @@ namespace Senparc.Weixin.Sample.NetCore3.Controllers
                 codes = codes.Take(100).ToArray();
             }
 
+            var dt0 = SystemTime.Now;
+
+            //QrCode 根目录
             var qrCodeDir = Path.Combine(CO2NET.Config.RootDictionaryPath, "App_Data", "QrCode");
             if (!Directory.Exists(qrCodeDir))
             {
@@ -44,13 +44,14 @@ namespace Senparc.Weixin.Sample.NetCore3.Controllers
             }
 
             //定义文件名和路径
-            var tempId = SystemTime.Now.ToString("yyyy-MM-dd-HHmmss");
-            var tempDirName = $"{tempId}_{Guid.NewGuid().ToString("n")}";
-            var tempZipFileName = $"{tempDirName}.zip";
-            var tempZipFileFullPath = Path.Combine(qrCodeDir, tempZipFileName);
+            var tempId = SystemTime.Now.ToString("yyyy-MM-dd-HHmmss");//本次生成唯一Id
+            var tempDirName = $"{tempId}_{Guid.NewGuid().ToString("n")}";//临时文件夹名
+            var tempZipFileName = $"{tempDirName}.zip";//临时压缩文件名
+            var tempZipFileFullPath = Path.Combine(qrCodeDir, tempZipFileName);//临时压缩文件完整路径
             var tempDir = Path.Combine(qrCodeDir, tempDirName);
             Directory.CreateDirectory(tempDir);//创建临时目录
 
+            //说明文件
             var readmeFile = Path.Combine(qrCodeDir, "readme.txt");
             System.IO.File.Copy(readmeFile, Path.Combine(tempDir, "readme.txt"));
 
@@ -59,12 +60,14 @@ namespace Senparc.Weixin.Sample.NetCore3.Controllers
             {
                 if (code.IsNullOrEmpty())
                 {
-                    continue;
+                    continue;//过滤为空的段落
                 }
-                i++;
+                i++;//计数器
 
+                
                 var finalCode = code.Length > 100 ? code.Substring(0, 100) : code;//约束长度
 
+                //二维码生成开始
                 BitMatrix bitMatrix;
                 bitMatrix = new MultiFormatWriter().encode(finalCode, BarcodeFormat.QR_CODE, 600, 600);
                 var bw = new ZXing.BarcodeWriterPixelData();
@@ -82,16 +85,18 @@ namespace Senparc.Weixin.Sample.NetCore3.Controllers
                 {
                     bitmap.UnlockBits(bitmapData);
                 }
+                //二维码生成结束
 
-                var fileName = Path.Combine(tempDir, $"{i}.jpg");
+                var fileName = Path.Combine(tempDir, $"{i}.jpg");//二维码文件名
 
+                //保存二维码
                 var fileStream = new FileStream(fileName, FileMode.Create, FileAccess.ReadWrite);
                 {
                     bitmap.Save(fileStream, System.Drawing.Imaging.ImageFormat.Png);
                     fileStream.Close();
                 }
             }
-            SenparcTrace.SendCustomLog("二维码生成结束", tempZipFileFullPath);
+            SenparcTrace.SendCustomLog("二维码生成结束", $"耗时：{SystemTime.DiffTotalMS(dt0)} ms，临时文件：{tempZipFileFullPath}");//记录日志
 
             var dt1 = SystemTime.Now;
             while (Directory.GetFiles(tempDir).Length < i + 1/* readme.txt */ && SystemTime.NowDiff(dt1) < TimeSpan.FromSeconds(30)/*最多等待时间*/)
@@ -117,7 +122,8 @@ namespace Senparc.Weixin.Sample.NetCore3.Controllers
 
                 if (fs != null)
                 {
-                    return File(fs/*$"~/App_Data/QrCode/{tempZipFileName}"*/ /*tempZipFileFullPath*/, "application/x-zip-compressed", $"SenparcQrCode_{tempId}.zip");
+                    return File(fs, "application/x-zip-compressed", $"SenparcQrCode_{tempId}.zip");
+                    //TODO:删除临时文件
                 }
             }
 

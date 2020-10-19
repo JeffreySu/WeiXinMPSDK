214522c34698e7f5529dbbc62d63dbd5f8377a75
Jimmy Wu <wjy307@qq.com>
2020-10-19 13:46:12 +0800

    修改appId和appSecret参数名，以防混淆

--- src/Senparc.Weixin.WxOpen/src/Senparc.Weixin.WxOpen/Senparc.Weixin.WxOpen/Containers/AccessTokenContainer.cs
+++ src/Senparc.Weixin.WxOpen/src/Senparc.Weixin.WxOpen/Senparc.Weixin.WxOpen/Containers/AccessTokenContainer.cs
@@ -31,17 +31,17 @@ namespace Senparc.Weixin.WxOpen.Containers
         /// <summary>
         /// 注册应用凭证信息，此操作只是注册，不会马上获取Token，并将清空之前的Token
         /// </summary>
-        /// <param name="appId">微信小程序后台的【开发】>【基本配置】中的“AppID(应用ID)”</param>
-        /// <param name="appSecret">微信小程序后台的【开发】>【基本配置】中的“AppSecret(应用密钥)”</param>
+        /// <param name="wxOpenAppId">微信小程序后台的【开发】>【基本配置】中的“AppID(应用ID)”</param>
+        /// <param name="wxOpenAppSecret">微信小程序后台的【开发】>【基本配置】中的“AppSecret(应用密钥)”</param>
         /// <param name="name">标记AccessToken名称（如微信小程序名称），帮助管理员识别。当 name 不为 null 和 空值时，本次注册内容将会被记录到 Senparc.Weixin.Config.SenparcWeixinSetting.Items[name] 中，方便取用。</param>
         [Obsolete("请使用 RegisterAsync() 方法")]
-        public static void Register(string appId, string appSecret, string name = null)
+        public static void Register(string wxOpenAppId, string wxOpenAppSecret, string name = null)
         {
-            var task = RegisterAsync(appId, appSecret, name);
+            var task = RegisterAsync(wxOpenAppId, wxOpenAppSecret, name);
             Task.WaitAll(new[] { task }, 10000);
             //Task.Factory.StartNew(() =>
             //{
-            //    RegisterAsync(appId, appSecret, name).ConfigureAwait(false);
+            //    RegisterAsync(wxOpenAppId, wxOpenAppSecret, name).ConfigureAwait(false);
             //}).ConfigureAwait(false);
         }
 
@@ -50,46 +50,46 @@ namespace Senparc.Weixin.WxOpen.Containers
         /// <summary>
         /// 使用完整的应用凭证获取Token，如果不存在将自动注册
         /// </summary>
-        /// <param name="appId"></param>
-        /// <param name="appSecret"></param>
+        /// <param name="wxOpenAppId"></param>
+        /// <param name="wxOpenAppSecret"></param>
         /// <param name="getNewToken"></param>
         /// <returns></returns>
-        public static string TryGetAccessToken(string appId, string appSecret, bool getNewToken = false)
+        public static string TryGetAccessToken(string wxOpenAppId, string wxOpenAppSecret, bool getNewToken = false)
         {
-            if (!CheckRegistered(appId) || getNewToken)
+            if (!CheckRegistered(wxOpenAppId) || getNewToken)
             {
-                Register(appId, appSecret);
+                Register(wxOpenAppId, wxOpenAppSecret);
             }
-            return GetAccessToken(appId, getNewToken);
+            return GetAccessToken(wxOpenAppId, getNewToken);
         }
 
         /// <summary>
         /// 获取可用Token
         /// </summary>
-        /// <param name="appId"></param>
+        /// <param name="wxOpenAppId"></param>
         /// <param name="getNewToken">是否强制重新获取新的Token</param>
         /// <returns></returns>
-        public static string GetAccessToken(string appId, bool getNewToken = false)
+        public static string GetAccessToken(string wxOpenAppId, bool getNewToken = false)
         {
-            return GetAccessTokenResult(appId, getNewToken).access_token;
+            return GetAccessTokenResult(wxOpenAppId, getNewToken).access_token;
         }
 
         /// <summary>
         /// 获取可用AccessTokenResult对象
         /// </summary>
-        /// <param name="appId"></param>
+        /// <param name="wxOpenAppId"></param>
         /// <param name="getNewToken">是否强制重新获取新的Token</param>
         /// <returns></returns>
-        public static AccessTokenResult GetAccessTokenResult(string appId, bool getNewToken = false)
+        public static AccessTokenResult GetAccessTokenResult(string wxOpenAppId, bool getNewToken = false)
         {
-            if (!CheckRegistered(appId))
+            if (!CheckRegistered(wxOpenAppId))
             {
-                throw new UnRegisterAppIdException(appId, string.Format("此appId（{0}）尚未注册，请先使用AccessTokenContainer.Register完成注册（全局执行一次即可）！", appId));
+                throw new UnRegisterAppIdException(wxOpenAppId, string.Format("此wxOpenAppId（{0}）尚未注册，请先使用AccessTokenContainer.Register完成注册（全局执行一次即可）！", wxOpenAppId));
             }
 
-            var accessTokenBag = TryGetItem(appId);
+            var accessTokenBag = TryGetItem(wxOpenAppId);
 
-            using (Cache.BeginCacheLock(LockResourceName, appId))//同步锁
+            using (Cache.BeginCacheLock(LockResourceName, wxOpenAppId))//同步锁
             {
                 if (getNewToken || accessTokenBag.AccessTokenExpireTime <= SystemTime.Now)
                 {
@@ -111,36 +111,36 @@ namespace Senparc.Weixin.WxOpen.Containers
         /// <summary>
         /// 【异步方法】注册应用凭证信息，此操作只是注册，不会马上获取Token，并将清空之前的Token
         /// </summary>
-        /// <param name="appId">微信小程序后台的【开发】>【基本配置】中的N“AppID(应用ID)”</param>
-        /// <param name="appSecret">微信小程序后台的【开发】>【基本配置】中的“AppSecret(应用密钥)”</param>
+        /// <param name="wxOpenAppId">微信小程序后台的【开发】>【基本配置】中的N“AppID(应用ID)”</param>
+        /// <param name="wxOpenAppSecret">微信小程序后台的【开发】>【基本配置】中的“AppSecret(应用密钥)”</param>
         /// <param name="name">标记AccessToken名称（如微信小程序名称），帮助管理员识别。当 name 不为 null 和 空值时，本次注册内容将会被记录到 Senparc.Weixin.Config.SenparcWeixinSetting.Items[name] 中，方便取用。</param>
-        public static async Task RegisterAsync(string appId, string appSecret, string name = null)
+        public static async Task RegisterAsync(string wxOpenAppId, string wxOpenAppSecret, string name = null)
         {
             //记录注册信息，RegisterFunc委托内的过程会在缓存丢失之后自动重试
-            RegisterFuncCollection[appId] = async () =>
+            RegisterFuncCollection[wxOpenAppId] = async () =>
             {
                 //using (FlushCache.CreateInstance())
                 //{
                 var bag = new AccessTokenBag()
                 {
-                    //Key = appId,
+                    //Key = wxOpenAppId,
                     Name = name,
-                    WxOpenAppId = appId,
-                    WxOpenAppSecret = appSecret,
+                    WxOpenAppId = wxOpenAppId,
+                    WxOpenAppSecret = wxOpenAppSecret,
                     AccessTokenExpireTime = DateTimeOffset.MinValue,
                     AccessTokenResult = new AccessTokenResult()
                 };
-                await UpdateAsync(appId, bag, null).ConfigureAwait(false);//第一次添加，此处已经立即更新
+                await UpdateAsync(wxOpenAppId, bag, null).ConfigureAwait(false);//第一次添加，此处已经立即更新
                 return bag;
                 //}
             };
 
-            var registerTask = RegisterFuncCollection[appId]();
+            var registerTask = RegisterFuncCollection[wxOpenAppId]();
 
             if (!name.IsNullOrEmpty())
             {
-                Senparc.Weixin.Config.SenparcWeixinSetting.Items[name].WxOpenAppId = appId;
-                Senparc.Weixin.Config.SenparcWeixinSetting.Items[name].WxOpenAppSecret = appSecret;
+                Senparc.Weixin.Config.SenparcWeixinSetting.Items[name].WxOpenAppId = wxOpenAppId;
+                Senparc.Weixin.Config.SenparcWeixinSetting.Items[name].WxOpenAppSecret = wxOpenAppSecret;
             }
 
             await Task.WhenAll(new[] { registerTask });//等待所有任务完成
@@ -152,47 +152,47 @@ namespace Senparc.Weixin.WxOpen.Containers
         /// <summary>
         /// 【异步方法】使用完整的应用凭证获取Token，如果不存在将自动注册
         /// </summary>
-        /// <param name="appId"></param>
-        /// <param name="appSecret"></param>
+        /// <param name="wxOpenAppId"></param>
+        /// <param name="wxOpenAppSecret"></param>
         /// <param name="getNewToken"></param>
         /// <returns></returns>
-        public static async Task<string> TryGetAccessTokenAsync(string appId, string appSecret, bool getNewToken = false)
+        public static async Task<string> TryGetAccessTokenAsync(string wxOpenAppId, string wxOpenAppSecret, bool getNewToken = false)
         {
-            if (!await CheckRegisteredAsync(appId).ConfigureAwait(false) || getNewToken)
+            if (!await CheckRegisteredAsync(wxOpenAppId).ConfigureAwait(false) || getNewToken)
             {
-                await RegisterAsync(appId, appSecret).ConfigureAwait(false);
+                await RegisterAsync(wxOpenAppId, wxOpenAppSecret).ConfigureAwait(false);
             }
-            return await GetAccessTokenAsync(appId, getNewToken).ConfigureAwait(false);
+            return await GetAccessTokenAsync(wxOpenAppId, getNewToken).ConfigureAwait(false);
         }
 
         /// <summary>
         /// 【异步方法】获取可用Token
         /// </summary>
-        /// <param name="appId"></param>
+        /// <param name="wxOpenAppId"></param>
         /// <param name="getNewToken">是否强制重新获取新的Token</param>
         /// <returns></returns>
-        public static async Task<string> GetAccessTokenAsync(string appId, bool getNewToken = false)
+        public static async Task<string> GetAccessTokenAsync(string wxOpenAppId, bool getNewToken = false)
         {
-            var result = await GetAccessTokenResultAsync(appId, getNewToken).ConfigureAwait(false);
+            var result = await GetAccessTokenResultAsync(wxOpenAppId, getNewToken).ConfigureAwait(false);
             return result.access_token;
         }
 
         /// <summary>
         /// 获取可用AccessTokenResult对象
         /// </summary>
-        /// <param name="appId"></param>
+        /// <param name="wxOpenAppId"></param>
         /// <param name="getNewToken">是否强制重新获取新的Token</param>
         /// <returns></returns>
-        public static async Task<IAccessTokenResult> GetAccessTokenResultAsync(string appId, bool getNewToken = false)
+        public static async Task<IAccessTokenResult> GetAccessTokenResultAsync(string wxOpenAppId, bool getNewToken = false)
         {
-            if (!await CheckRegisteredAsync(appId).ConfigureAwait(false))
+            if (!await CheckRegisteredAsync(wxOpenAppId).ConfigureAwait(false))
             {
-                throw new UnRegisterAppIdException(appId, string.Format("此appId（{0}）尚未注册，请先使用AccessTokenContainer.Register完成注册（全局执行一次即可）！", appId));
+                throw new UnRegisterAppIdException(wxOpenAppId, string.Format("此wxOpenAppId（{0}）尚未注册，请先使用AccessTokenContainer.Register完成注册（全局执行一次即可）！", wxOpenAppId));
             }
 
-            var accessTokenBag = await TryGetItemAsync(appId).ConfigureAwait(false);
+            var accessTokenBag = await TryGetItemAsync(wxOpenAppId).ConfigureAwait(false);
 
-            using (await Cache.BeginCacheLockAsync(LockResourceName, appId).ConfigureAwait(false))//同步锁
+            using (await Cache.BeginCacheLockAsync(LockResourceName, wxOpenAppId).ConfigureAwait(false))//同步锁
             {
                 if (getNewToken || accessTokenBag.AccessTokenExpireTime <= SystemTime.Now)
                 {

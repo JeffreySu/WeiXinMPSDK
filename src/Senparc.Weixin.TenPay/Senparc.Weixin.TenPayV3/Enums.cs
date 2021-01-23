f9defc7a89be64fb6caeffad00eb3f7ed8a0832c
JeffreySu <www.jeffrey.su@gmail.com>
2021-01-24 02:23:08 +0800

    添加 Senparc.Weixin.TenPayV3 模块

---
+++ src/Senparc.Weixin.TenPay/Senparc.Weixin.TenPayV3/Enums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Senparc.Weixin.TenPayV3
+{
+    /// <summary>
+    /// 应答的语种
+    /// </summary>
+    public class AcceptLanguage
+    {
+        public const string EN = "en";
+        public const string ZH_CN = "zh-CN";
+        public const string ZH_HK = "zh-HK";
+        public const string ZH_TW = "zh-TW";
+    }
+
+}
---
+++ src/Senparc.Weixin.TenPay/Senparc.Weixin.TenPayV3/Helpers/TenPayDateTimeHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.Weixin.TenPayV3.Helpers
+{
+    public static class TenPayDateTimeHelper
+    {
+        /// <summary>
+        /// 输出微信 V3 指定的符合 ISO 8601 要求格式的时间字符串
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static string ToTenPayDateTime(this DateTime dateTime)
+        {
+            return dateTime.ToString("szzz");//).ToString("yyyy-MM-ddTHH:mm:fff");
+        }
+    }
+}

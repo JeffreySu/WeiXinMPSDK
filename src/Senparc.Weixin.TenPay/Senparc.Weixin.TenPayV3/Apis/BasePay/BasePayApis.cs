91696305775a5ead7afba35a2b3a2fa484c309e1
JeffreySu <www.jeffrey.su@gmail.com>
2021-01-31 00:16:23 +0800

    开始创建第一批TenpayV接口，先创建JSAPI基础支付接口

---
+++ src/Senparc.Weixin.TenPay/Senparc.Weixin.TenPayV3/Apis/BasePay/BasePayApis.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.Weixin.TenPayV3.Apis
+{
+    public static class BasePayApis
+    {
+
+
+    }
+}

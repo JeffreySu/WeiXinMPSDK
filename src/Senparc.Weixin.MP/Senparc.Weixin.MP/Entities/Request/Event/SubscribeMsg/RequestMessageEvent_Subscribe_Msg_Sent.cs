14eaaf1a8a011491ff14ac0e307c0506d723aa6a
ccccccmd <ccccccmd@163.com>
2021-03-30 11:01:52 +0800

    增加服务号订阅通知事件

---
+++ src/Senparc.Weixin.MP/Senparc.Weixin.MP/Entities/Request/Event/SubscribeMsg/RequestMessageEvent_Subscribe_Msg_Sent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Senparc.NeuChar.Entities;
+
+namespace Senparc.Weixin.MP.Entities
+{
+    public class RequestMessageEvent_Subscribe_Msg_Sent : RequestMessageEventBase, IRequestMessageEventBase
+    {
+        public override Event Event => Event.subscribe_msg_sent_event;
+
+        public List<SubscribeMsgSentEvent> SubscribeMsgSentEventList { get; set; }
+
+        public RequestMessageEvent_Subscribe_Msg_Sent()
+        {
+            SubscribeMsgSentEventList = new List<SubscribeMsgSentEvent>();
+        }
+
+    }
+}

40ca4e663e03dbac5c823cc0377ad7a44f7b4ba4
RockRockWhite <1834309151@qq.com>
2021-09-22 11:22:58 +0800

    对微信支付V3的MarkingApis使用partial类

---
+++ src/Senparc.Weixin.TenPay/Senparc.Weixin.TenPayV3.Test/Apis/Marketing/MarketingPartnershipsApisTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Senparc.CO2NET.Extensions;
+using Senparc.Weixin.Helpers;
+using Senparc.Weixin.TenPayV3.Apis.Marketing;
+using Senparc.Weixin.TenPayV3.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.Weixin.TenPayV3.Apis.Tests
+{
+    public partial class MarketingApisTests
+    {
+        #region 委托营销接口
+
+
+        #endregion
+
+    }
+}

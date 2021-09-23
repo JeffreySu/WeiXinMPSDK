214028eb768907f4fc2784c2e79c24b2721f6339
mc7246 <jiangshans@163.com>
2021-09-23 18:19:28 +0800

    添加第三方代码提审资料上传接口

---
+++ src/Senparc.Weixin.Open/Senparc.Weixin.Open/WxaAPIs/Code/CodeJson/UploadMediaResult.cs
@@ -0,0 +1,16 @@
+﻿using Senparc.Weixin.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.Weixin.Open.WxaAPIs
+{
+    public class UploadMediaResult : WxJsonResult
+    {
+        public string type { get; set; }
+
+        public string mediaid { get; set; }
+    }
+}

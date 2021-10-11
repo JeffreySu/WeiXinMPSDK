be08031b6b5b270432bb9db9b18df18913045ea2
JeffreySu <www.jeffrey.su@gmail.com>
2021-10-11 16:57:01 +0800

    修改注释

--- src/Senparc.Weixin.TenPay/Senparc.Weixin.TenPayV3/Helpers/TenPaySignHelper.cs
+++ src/Senparc.Weixin.TenPay/Senparc.Weixin.TenPayV3/Helpers/TenPaySignHelper.cs
@@ -65,13 +65,14 @@ namespace Senparc.Weixin.TenPayV3.Helpers
 
             byte[] keyData = Convert.FromBase64String(privateKey);
 
-            //以下方法不兼容 Linux
+            #region 以下方法不兼容 Linux
             //using (CngKey cngKey = CngKey.Import(keyData, CngKeyBlobFormat.Pkcs8PrivateBlob))
             //using (RSACng rsa = new RSACng(cngKey))
             //{
             //    byte[] data = System.Text.Encoding.UTF8.GetBytes(message);
             //    return Convert.ToBase64String(rsa.SignData(data, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1));
             //}
+            #endregion
 
             using (var rsa = System.Security.Cryptography.RSA.Create())
             {

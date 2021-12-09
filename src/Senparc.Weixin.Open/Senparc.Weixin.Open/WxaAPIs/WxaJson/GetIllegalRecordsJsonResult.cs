2e2b6b33697d77a275e3cd9c65ed4842d4e52da7
mc7246 <jiangshans@163.com>
2021-12-09 11:36:08 +0800

    开放平台-小程序违规和申诉管理 小程序-违规和申诉记录事件推送

---
+++ src/Senparc.Weixin.Open/Senparc.Weixin.Open/WxaAPIs/WxaJson/GetIllegalRecordsJsonResult.cs
@@ -0,0 +1,42 @@
+﻿using Senparc.Weixin.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.Weixin.Open.WxaAPIs
+{
+    public class GetIllegalRecordsJsonResult : WxJsonResult
+    {
+        /// <summary>
+        /// 违规处罚记录id
+        /// </summary>
+        public string illegal_record_id { get; set; }
+
+        /// <summary>
+        /// 违规处罚时间
+        /// </summary>
+        public string create_time { get; set; }
+
+        /// <summary>
+        /// 违规原因
+        /// </summary>
+        public string illegal_reason { get; set; }
+
+        /// <summary>
+        /// 违规内容
+        /// </summary>
+        public string illegal_content { get; set; }
+
+        /// <summary>
+        /// 规则链接
+        /// </summary>
+        public string rule_url { get; set; }
+
+        /// <summary>
+        /// 违反的规则名称
+        /// </summary>
+        public string rule_name { get; set; }
+    }
+}

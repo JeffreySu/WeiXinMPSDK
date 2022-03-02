889a1201163ac92547776dee40bfa85deaf7a39b
JeffreySu <www.jeffrey.su@gmail.com>
2022-03-02 20:30:58 +0800

    更新Sample

--- Samples/MP/Senparc.Weixin.Sample.MP/Controllers/HomeController.cs
+++ Samples/MP/Senparc.Weixin.Sample.MP/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace Senparc.Weixin.Sample.MP.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {

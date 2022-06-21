3b59304afd0d0cce6b91b13031acb7bd2d9375e2
JeffreySu <www.jeffrey.su@gmail.com>
2022-06-21 22:24:14 +0800

    Revert "更新Nuget版本信息"
    This reverts commit a63d1b1e9611a31fdaef49d335ff2852bfe82f3f.

--- src/Senparc.Weixin.MP/Senparc.WeixinTests/Entities/Request/Extensions/RequestMessageTextExtensionTests.cs
+++ src/Senparc.Weixin.MP/Senparc.WeixinTests/Entities/Request/Extensions/RequestMessageTextExtensionTests.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Senparc.Weixin.MP.Entities;
-using Senparc.CO2NET.Extensions;
 
 namespace Senparc.NeuChar.Entities.Request.Tests
 {
@@ -22,14 +21,14 @@ namespace Senparc.NeuChar.Entities.Request.Tests
             {
                 //大小写不敏感
                 requestMessage.Content = "Case1";
-                result = requestMessage.StartHandler(false)
-                   .Keyword("CASe1", () => null)
-                   .MatchSuccessed;
+                 result = requestMessage.StartHandler(false)
+                    .Keyword("CASe1", () => null)
+                    .MatchSuccessed;
                 Assert.IsTrue(result);
 
                 requestMessage.Content = "Case1";
                 result = requestMessage.StartHandler(false)
-                   .Keywords(new[] { "CaSe1", "case2" }, () => null)
+                   .Keywords(new []{"CaSe1","case2"}, () => null)
                    .MatchSuccessed;
                 Assert.IsTrue(result);
 
@@ -58,30 +57,6 @@ namespace Senparc.NeuChar.Entities.Request.Tests
                 .Regex(@"case\d+", () => null)
                 .MatchSuccessed;
                 Assert.IsFalse(result);
-            }
-
-            {
-                //异步方法
-                requestMessage.Content = "case123";
-                var handler = requestMessage.StartHandler(true)
-                 .RegexAsync(@"case\d+", async () =>
-                 {
-                     Console.WriteLine(SystemTime.Now.ToString("mm:ss.ffff"));
-                     await Task.Delay(10);
-                     Console.WriteLine(SystemTime.Now.ToString("mm:ss.ffff"));
-                     var responseMessage = new ResponseMessageText();
-                     responseMessage.Content = @"case\d+";
-                     return responseMessage;
-                 }).Result
-                 .KeywordAsync("CaseA", async () =>
-                 {
-                     var responseMessage = new ResponseMessageText();
-                     responseMessage.Content = @"CaseA";
-                     Console.WriteLine(responseMessage.ToJson(true));
-                     return responseMessage;
-                 }).Result;
-                result = handler.MatchSuccessed;
-                Assert.IsTrue(result);
             }
         }
     }

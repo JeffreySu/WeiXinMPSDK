610ded87516a122281780e5e3029b85522aa296a
JeffreySu <www.jeffrey.su@gmail.com>
2022-07-31 01:40:43 +0800

    MP v16.18.5 新增 OpenApi 接口，包括：“查询 openAPI 调用quota”“查询 rid 信息”接口 单元测试已通过 #2657 感谢 @liuyanruidx

---
+++ src/Senparc.Weixin.MP/Senparc.Weixin.MP/OpenAPIs/OpenApiJson/RidGetJsonResult.cs
@@ -0,0 +1,45 @@
+﻿using Senparc.Weixin.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.Weixin.MP.OpenAPIs.OpenApiJson
+{
+    public class RidGetJsonResult: WxJsonResult
+    {
+        public Request request { get; set; }
+    }
+
+    public class Request
+    {
+        /// <summary>
+        /// 发起请求的时间戳
+        /// </summary>
+        public long invoke_time { get; set; }
+        /// <summary>
+        /// 请求毫秒级耗时
+        /// </summary>
+        public int cost_in_ms { get; set; }
+        /// <summary>
+        /// 请求的 URL 参数
+        /// </summary>
+        public string request_url { get; set; }
+        /// <summary>
+        /// post请求的请求参数
+        /// </summary>
+        public string request_body { get; set; }
+        /// <summary>
+        /// 接口请求返回参数
+        /// </summary>
+        public string response_body { get; set; }
+        /// <summary>
+        /// 接口请求的客户端ip
+        /// </summary>
+        public string client_ip { get; set; }
+    }
+
+
+
+}

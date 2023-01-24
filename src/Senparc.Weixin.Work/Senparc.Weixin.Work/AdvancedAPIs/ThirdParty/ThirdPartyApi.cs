3aec9a3682c8e95837d88a4a5795eb820cbca435
JeffreySu <www.jeffrey.su@gmail.com>
2023-01-24 23:42:33 +0800

    企业微信 开始添加第三方应用接口

---
+++ src/Senparc.Weixin.Work/Senparc.Weixin.Work/AdvancedAPIs/ThirdParty/ThirdPartyApi.cs
@@ -0,0 +1,31 @@
+﻿/*----------------------------------------------------------------
+    Copyright (C) 2023 Senparc
+    
+    文件名：ThirdPartyApi.cs
+    文件功能描述：第三方应用接口
+    
+    
+    创建标识：Senparc - 20220124
+    
+
+----------------------------------------------------------------*/
+
+/*
+    官方文档：https://developer.work.weixin.qq.com/document/path/91201
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.Weixin.Work.AdvancedAPIs.ThirdParty
+{
+    /// <summary>
+    /// 第三方应用接口
+    /// </summary>
+    public static class ThirdPartyApi
+    {
+    }
+}

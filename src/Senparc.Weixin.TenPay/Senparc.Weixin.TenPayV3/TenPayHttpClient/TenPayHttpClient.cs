44a549f2f04cfa04f74b56a59efaa1fe31171af1
RockRockWhite <1834309151@qq.com>
2023-02-14 11:26:43 +0800

    修复TenPayHttpClient遇到statusCode为204时会验证签名错误的问题

--- src/Senparc.Weixin.TenPay/Senparc.Weixin.TenPayV3/TenPayHttpClient/TenPayHttpClient.cs
+++ src/Senparc.Weixin.TenPay/Senparc.Weixin.TenPayV3/TenPayHttpClient/TenPayHttpClient.cs
@@ -238,6 +238,12 @@ namespace Senparc.Weixin.TenPayV3.TenPayHttpClient
 
         protected async Task<bool> VerifyResponseMessage(HttpResponseMessage responseMessage, string content)
         {
+            // 注意：在 StatusCode 为 204（NoContent）时，验证签名直接成功。
+            if (responseMessage.StatusCode == System.Net.HttpStatusCode.NoContent)
+            {
+                return true;
+            }
+            
             var wechatpayTimestamp = responseMessage.Headers.GetValues("Wechatpay-Timestamp").First();
             var wechatpayNonce = responseMessage.Headers.GetValues("Wechatpay-Nonce").First();
             var wechatpaySignatureBase64 = responseMessage.Headers.GetValues("Wechatpay-Signature").First();//后续需要base64解码

7afb9473dbe85955b9a582775a2399e794e2b89b
JeffreySu <www.jeffrey.su@gmail.com>
2023-02-26 22:30:49 +0800

    Work v3.15.16 添加“审批”接口 （PR #2795，Issue #2791） 感谢 @mojinxun @ssjun

--- src/Senparc.Weixin.Work/Senparc.Weixin.Work/AdvancedAPIs/OA/OaApi.cs
+++ src/Senparc.Weixin.Work/Senparc.Weixin.Work/AdvancedAPIs/OA/OaApi.cs
@@ -1,4 +1,35 @@
-﻿using Senparc.NeuChar;
+﻿#region Apache License Version 2.0
+/*----------------------------------------------------------------
+
+Copyright 2023 Jeffrey Su & Suzhou Senparc Network Technology Co.,Ltd.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+except in compliance with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the
+License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+either express or implied. See the License for the specific language governing permissions
+and limitations under the License.
+
+Detail: https://github.com/JeffreySu/WeiXinMPSDK/blob/master/license.md
+
+----------------------------------------------------------------*/
+#endregion Apache License Version 2.0
+
+/*----------------------------------------------------------------
+    Copyright (C) 2023 Senparc
+    
+    文件名：OaApi.cs
+    文件功能描述：审批接口
+    
+    
+    创建标识：mojinxun - 20230226
+    
+----------------------------------------------------------------*/
+
+using Senparc.NeuChar;
 using Senparc.Weixin.CommonAPIs;
 using Senparc.Weixin.Entities;
 using Senparc.Weixin.Work.AdvancedAPIs.Mobile;
@@ -11,6 +42,9 @@ using System.Threading.Tasks;
 
 namespace Senparc.Weixin.Work.AdvancedAPIs.OA
 {
+    /// <summary>
+    /// 
+    /// </summary>
     [NcApiBind(NeuChar.PlatformType.WeChat_Work, true)]
     public static class OaApi
     {

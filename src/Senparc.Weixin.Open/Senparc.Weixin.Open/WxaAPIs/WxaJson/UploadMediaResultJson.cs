f65d6e98c745baba2ed11d9a136abf991a949d56
姚烽 <yaofeng@fengwall.com>
2023-04-18 17:37:16 +0800

    第三方平台API：代商家管理小程序 /小程序代码管理 /上传提审素材

---
+++ src/Senparc.Weixin.Open/Senparc.Weixin.Open/WxaAPIs/WxaJson/UploadMediaResultJson.cs
@@ -0,0 +1,19 @@
+﻿using Senparc.Weixin.Entities;
+
+namespace Senparc.Weixin.MP.AdvancedAPIs.Media
+{
+    /// <summary>
+    /// 上传提审素材
+    /// </summary>
+    public class UploadMediaResultJson : WxJsonResult
+    {
+        /// <summary>
+        /// 类型
+        /// </summary>
+        public string type { get; set; }
+        /// <summary>
+        /// 媒体id
+        /// </summary>
+        public string mediaid { get; set; }
+    }
+}

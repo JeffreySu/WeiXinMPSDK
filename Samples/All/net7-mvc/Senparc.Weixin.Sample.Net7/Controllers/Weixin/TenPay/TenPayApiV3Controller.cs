09880bfa4f1cd27e958f00a830e34976a0034db4
JeffreySu <www.jeffrey.su@gmail.com>
2023-04-23 09:51:58 -0700

    更新 TenPayV3 Sample

--- Samples/All/net7-mvc/Senparc.Weixin.Sample.Net7/Controllers/Weixin/TenPay/TenPayApiV3Controller.cs
+++ Samples/All/net7-mvc/Senparc.Weixin.Sample.Net7/Controllers/Weixin/TenPay/TenPayApiV3Controller.cs
@@ -546,7 +546,7 @@ namespace Senparc.Weixin.Sample.Net6.Controllers
                 //var dataInfo = new TenPayV3RefundRequestData(TenPayV3Info.AppId, TenPayV3Info.MchId, TenPayV3Info.Key,
                 //    null, nonceStr, null, outTradeNo, outRefundNo, totalFee, refundFee, opUserId, null, notifyUrl: notifyUrl);
                 //TODO:该接口参数二选一传入
-                var dataInfo = new RefundRequsetData(transactionId, null, outRefundNo, "Senparc TenPayV3 demo退款测试", notifyUrl, null, new RefundRequsetData.Amount(refundFee, null, refundFee, "CNY"), null);
+                var dataInfo = new RefundRequestData(transactionId, null, outRefundNo, "Senparc TenPayV3 demo退款测试", notifyUrl, null, new RefundRequestData.Amount(refundFee, null, refundFee, "CNY"), null);
 
 
                 //#region 新方法（Senparc.Weixin v6.4.4+）
--- src/Senparc.Weixin.TenPay/Senparc.Weixin.TenPayV3/Apis/BasePay/Entities/RequestData/RefundRequestData.cs
+++ src/Senparc.Weixin.TenPay/Senparc.Weixin.TenPayV3/Apis/BasePay/Entities/RequestData/RefundRequestData.cs
@@ -31,13 +31,42 @@ Detail: https://github.com/JeffreySu/WeiXinMPSDK/blob/master/license.md
     修改描述：完善注释; 增加构造函数
     
 ----------------------------------------------------------------*/
+#region Apache License Version 2.0
+/*----------------------------------------------------------------
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Schema;
+Copyright 2023 Jeffrey Su & Suzhou Senparc Network Technology Co.,Ltd.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+except in compliance with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the
+License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+either express or implied. See the License for the specific language governing permissions
+and limitations under the License.
+
+Detail: https://github.com/JeffreySu/WeiXinMPSDK/blob/master/license.md
+
+----------------------------------------------------------------*/
+#endregion Apache License Version 2.0
+
+/*----------------------------------------------------------------
+    Copyright (C) 2023 Senparc
+  
+    文件名：RefundRequestData.cs
+    文件功能描述：微信支付申请退款请求数据
+    
+    
+    创建标识：Senparc - 20210814
+
+    修改标识：Senparc - 20210819
+    修改描述：完善注释; 增加构造函数
+
+    修改标识：Senparc - 20220423
+    修改描述：修改类名 RefundRequsetData 为 RefundRequestData
+    
+----------------------------------------------------------------*/
 
 namespace Senparc.Weixin.TenPayV3.Apis.BasePay
 {

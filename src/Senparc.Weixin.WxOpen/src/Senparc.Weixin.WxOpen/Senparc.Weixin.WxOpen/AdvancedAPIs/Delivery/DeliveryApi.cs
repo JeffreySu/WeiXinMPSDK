0fff00d191567b8acbb73abd71fe64a6b80a7259
chinanhb <760833928@qq.com>
2023-05-29 12:08:11 +0800

    新增小程序物流服务接口功能和推送事件

--- src/Senparc.Weixin.WxOpen/src/Senparc.Weixin.WxOpen/Senparc.Weixin.WxOpen/AdvancedAPIs/Delivery/DeliveryApi.cs
+++ src/Senparc.Weixin.WxOpen/src/Senparc.Weixin.WxOpen/Senparc.Weixin.WxOpen/AdvancedAPIs/Delivery/DeliveryApi.cs
@@ -1,4 +1,38 @@
-﻿using Senparc.NeuChar;
+﻿#region Apache License Version 2.0
+/*----------------------------------------------------------------
+
+Copyright 2023 chinanhb & Suzhou Senparc Network Technology Co.,Ltd.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+except in compliance with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the
+License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+either express or implied. See the License for the specific language governing permissions
+and limitations under the License.
+
+Detail: https://github.com/JeffreySu/WeiXinMPSDK/blob/master/license.md
+
+----------------------------------------------------------------*/
+/*----------------------------------------------------------------
+    Copyright (C) 2023 Senparc
+    
+    文件名：DeliveryApi.cs
+    文件功能描述：微信小程序物流服务相关接口
+    
+    
+    创建标识：chinanhb - 20230529
+----------------------------------------------------------------*/
+
+/*
+    API：https://developers.weixin.qq.com/miniprogram/dev/OpenApiDoc/express/express-by-business/
+    
+ */
+#endregion Apache License Version 2.0
+
+using Senparc.NeuChar;
 using Senparc.Weixin.CommonAPIs;
 using Senparc.Weixin.WxOpen.AdvancedAPIs.Delivery.DeliveryJson;
 using Senparc.Weixin.WxOpen.AdvancedAPIs.Delivery.DeliveryJson.JsonResult;

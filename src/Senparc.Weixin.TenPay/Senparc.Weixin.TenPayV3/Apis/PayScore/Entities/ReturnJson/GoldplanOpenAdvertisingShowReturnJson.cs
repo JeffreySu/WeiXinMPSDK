53262b5eca228e4184abc383f7aaaa778f6307d9
姚烽 <yaofeng@fengwall.com>
2023-06-02 20:37:57 +0800

    完善微信支付V3 服务商平台 经营能力相关接口

---
+++ src/Senparc.Weixin.TenPay/Senparc.Weixin.TenPayV3/Apis/PayScore/Entities/ReturnJson/GoldplanOpenAdvertisingShowReturnJson.cs
@@ -0,0 +1,54 @@
+﻿#region Apache License Version 2.0
+/*----------------------------------------------------------------
+
+Copyright 2023 Jeffrey Su & Suzhou Senparc Network Technology Co.,Ltd.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+except in compliance with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the
+License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+either express or implied. See the License for the specific language governing permissions
+and limitations under the License.
+
+Detail: https://github.com/JeffreySu/WeiXinMPSDK/blob/master/license.md
+
+----------------------------------------------------------------*/
+#endregion Apache License Version 2.0
+
+/*----------------------------------------------------------------
+    Copyright (C) 2023 Senparc
+  
+    文件名：GoldplanOpenAdvertisingShowReturnJson.cs
+    文件功能描述：开通广告展示返回模型
+    
+    
+    创建标识：Senparc - 20230602
+    
+----------------------------------------------------------------*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Senparc.Weixin.TenPayV3.Apis.Entities;
+
+namespace Senparc.Weixin.TenPayV3.Apis.PayScore
+{
+    /// <summary>
+    /// 开通广告展示返回模型
+    /// <para>详细请参考微信支付官方文档 https://pay.weixin.qq.com/wiki/doc/apiv3_partner/apis/chapter8_5_4.shtml </para>
+    /// </summary>
+    public class GoldplanOpenAdvertisingShowReturnJson : ReturnJsonBase
+    {
+        /// <summary>
+        /// 无参构造函数
+        /// </summary>
+        public GoldplanOpenAdvertisingShowReturnJson()
+        {
+
+        }
+    }
+}

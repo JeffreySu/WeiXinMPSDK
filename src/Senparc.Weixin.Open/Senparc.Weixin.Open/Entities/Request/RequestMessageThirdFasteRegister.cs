62a860ddf43535a4998aabe1b9d7e772391dafc2
姚烽 <yaofeng@fengwall.com>
2023-07-28 15:32:39 +0800

    #2870

--- src/Senparc.Weixin.Open/Senparc.Weixin.Open/Entities/Request/RequestMessageThirdFasteRegister.cs
+++ src/Senparc.Weixin.Open/Senparc.Weixin.Open/Entities/Request/RequestMessageThirdFasteRegister.cs
@@ -15,9 +15,6 @@
     修改描述：v4.5.4.1 GetAuditStatusResultJson 改名为 GetAuditResultJson，保持全局命名唯一性
 ----------------------------------------------------------------*/
 
-using Senparc.NeuChar.Entities;
-using System;
-
 namespace Senparc.Weixin.Open
 {
     /// <summary>
@@ -35,12 +32,19 @@ namespace Senparc.Weixin.Open
         /// </summary>
         public string appid { get; set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public ReturnCode status { get; set; }
+
         /// <summary>
         /// 第三方授权码
         /// </summary>
         public string auth_code { get; set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public string msg { get; set; }
         
         /// <summary>
@@ -49,35 +53,49 @@ namespace Senparc.Weixin.Open
         public ThirdFasteRegisterInfo info {get;set;}
     }
 
-    ///// <summary>
-    ///// 注册时提交的资料信息
-    ///// </summary>
-    //public class info
-    //{
-    //    /// <summary>
-    //    /// 企业名称
-    //    /// </summary>
-    //    public string name { get; set; }
+    /// <summary>
+    /// 注册时提交的资料信息
+    /// </summary>
+    public class ThirdFasteRegisterInfo
+    {
+        /// <summary>
+        /// 企业小程序 - 企业名称
+        /// </summary>
+        public string name { get; set; }
+        /// <summary>
+        /// 企业小程序 - 企业代码
+        /// </summary>
+        public string code { get; set; }
+        /// <summary>
+        /// 企业小程序 - 企业代码类型 
+        /// </summary>
+        public CodeType code_type { get; set; }
+        /// <summary>
+        /// 企业小程序 - 法人微信号
+        /// </summary>
+        public string legal_persona_wechat { get; set; }
+        /// <summary>
+        /// 企业小程序 - 法人姓名
+        /// </summary>
+        public string legal_persona_name { get; set; }
 
-    //    /// <summary>
-    //    /// 企业代码
-    //    /// </summary>
-    //    public string code { get; set; }
-    //    /// <summary>
-    //    /// 企业代码类型 
-    //    /// </summary>
-    //    public CodeType code_type { get; set; }
-    //    /// <summary>
-    //    /// 法人微信号
-    //    /// </summary>
-    //    public string legal_persona_wechat { get; set; }
-    //    /// <summary>
-    //    /// 法人姓名
-    //    /// </summary>
-    //    public string legal_persona_name { get; set; }
-    //    /// <summary>
-    //    /// 第三方联系电话
-    //    /// </summary>
-    //    public string component_phone { get; set; }
-    //}
+        #region 公共
+        /// <summary>
+        /// 公共 - 第三方联系电话
+        /// </summary>
+        public string component_phone { get; set; }
+        #endregion
+
+
+        #region 个人 
+        /// <summary>
+        /// 个人小程序 - 用户微信号
+        /// </summary>
+        public string wxuser { get; set; }
+        /// <summary>
+        /// 个人小程序 - 用户姓名
+        /// </summary>
+        public string idname { get; set; }
+        #endregion
+    }
 }

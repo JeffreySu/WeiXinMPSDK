194212e70d4d565a956ddfdc8131efedd0385d2c
mc7246 <jiangshans@163.com>
2023-09-08 17:46:56 +0800

    修正收货提醒接口参数

--- src/Senparc.Weixin.WxOpen/src/Senparc.Weixin.WxOpen/Senparc.Weixin.WxOpen/AdvancedAPIs/Sec/Order.cs
+++ src/Senparc.Weixin.WxOpen/src/Senparc.Weixin.WxOpen/Senparc.Weixin.WxOpen/AdvancedAPIs/Sec/Order.cs
@@ -140,14 +140,14 @@ namespace Senparc.Weixin.WxOpen.AdvancedAPIs.Sec
         /// <param name="merchant_trade_no">商户系统内部订单号，只能是数字、大小写字母_-*且在同一个商户号下唯一</param>
         /// <param name="timeOut"></param>
         /// <returns></returns>
-        public static WxJsonResult NotifyConfirmReceive(string accessTokenOrAppId, long receive_time, string transaction_id="", string merchant_id="", string sub_merchant_id="", string merchant_trade_no="", int timeOut = Config.TIME_OUT)
+        public static WxJsonResult NotifyConfirmReceive(string accessTokenOrAppId, long received_time, string transaction_id="", string merchant_id="", string sub_merchant_id="", string merchant_trade_no="", int timeOut = Config.TIME_OUT)
         {
             return WxOpenApiHandlerWapper.TryCommonApi(accessToken =>
             {
                 string urlFormat = Config.ApiMpHost + "/wxa/sec/order/notify_confirm_receive?access_token={0}";
                 var postBody = new
                 {
-                    receive_time,
+                    received_time,
                     transaction_id,
                     merchant_id,
                     sub_merchant_id,

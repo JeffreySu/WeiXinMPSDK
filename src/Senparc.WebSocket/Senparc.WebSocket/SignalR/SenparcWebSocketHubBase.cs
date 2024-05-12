65c0c2c402366f696d26209e7c61ae1faa2ac4eb
JeffreySu <www.jeffrey.su@gmail.com>
2024-05-12 16:41:22 +0800

    Squashed 'src/Senparc.WebSocket/' changes from 156252aee..10be708d1
    10be708d1 发布新版本
    d1deae7ae 升级新版本号
    2a8749762 升级基础包
    b0f59cd34 更新 NeuChar 包 #2113 感谢 @ulyanovsk
    ac26f936f 更新 CO2NET 及 NeuChar 包
    c9b75144a 分配新版本号
    fc8277100 Merge branch 'Developer' of https://github.com/JeffreySu/WeiXinMPSDK into Developer
    0c9add199 v6.7.300 添加 CsRedis 的注册，同时引用最新版本 CO2NET
    b86de41b1 Sample v5.401.2，所有版本升级到 *.*.301
    42523c503 发布 *.*.300 正式版
    06e40f484 完成 Senparc DI 改造，发布正式版
    e0f1fcc5e 发布新版本Preivew1
    332da31d6 更新 NeuChar 及 CO2NET 最新版本
    13cdfe360 modify
    1f4f89c9b 发布正式版，分配版本号
    bf65f6327 更新CO2NET正式版
    673f99bca 升级 到 preview，更新 CO2NET/NeuChar
    c1fbd9fc6 更新 nuget 包：CO2NET、NeuChar
    e792c075c 更新 copyright 年份信息
    af8c0704c WebSocket v0.10.100-preview1
    5a821562d 更新CO2NET和NeuChar，准备分离AspNet
    214a58fbb v6.6.104 引用新版本 NeuChar，优化 MessageHandler 同步方法兼容策略
    8c071e4a8 .net 4.5 Sample 更新 nuget 引用
    d52ea1ee6 MP v16.9.103 解决 MessageHandler 初始化无响应的问题
    
    git-subtree-dir: src/Senparc.WebSocket
    git-subtree-split: 10be708d173ffbbcc33f80f0298d3c73ef52a31a

--- src/Senparc.WebSocket/Senparc.WebSocket/SignalR/SenparcWebSocketHubBase.cs
+++ src/Senparc.WebSocket/Senparc.WebSocket/SignalR/SenparcWebSocketHubBase.cs
@@ -1,5 +1,5 @@
 ﻿/*----------------------------------------------------------------
-    Copyright (C) 2019 Senparc
+    Copyright (C) 2020 Senparc
 
     文件名：SenparcWebSocketHubBase.cs
     文件功能描述：SenparcWebSocketHub 基类
@@ -9,7 +9,7 @@
 
 ----------------------------------------------------------------*/
 
-#if NETSTANDARD2_0 || NETCOREAPP3_0
+#if !NET45
 using Microsoft.AspNetCore.SignalR;
 using Senparc.WebSocket.Exceptions;
 using System;

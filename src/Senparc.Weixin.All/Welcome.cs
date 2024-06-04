2d3183f0634f7166d115ae9f5ae914881fb16427
JeffreySu <www.jeffrey.su@gmail.com>
2024-06-05 05:13:13 +0800

    添加说明

--- src/Senparc.Weixin.All/Welcome.cs
+++ src/Senparc.Weixin.All/Welcome.cs
@@ -3,5 +3,10 @@
     public static class Welcome
     {
         // 使用 Senparc.Weixin.All 一个包即可自动附加所有 Senparc.Weixin.* 项目引用
+
+        public static string SayHello()
+        {
+            return "Hello Senparc.Weixin.All!";
+        }
     }
 }

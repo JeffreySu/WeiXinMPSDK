b2654d00c68dc9be466ce48e1b2ba42c7df0f3b3
JeffreySu <www.jeffrey.su@gmail.com>
2024-07-04 20:24:46 +0800

    添加 SignTest() 单元测试。

---
+++ src/Senparc.Weixin.TenPay/Senparc.Weixin.TenPayV3.Test/TenPayHttpClient/Signer/SM3WithSM2SignerTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Client.TenPayHttpClient.Signer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.TenPayHttpClient.Signer.Tests
+{
+    [TestClass()]
+    public class SM3WithSM2SignerTests
+    {
+        [TestMethod()]
+        public void SignTest()
+        {
+            SM3WithSM2Signer sM3WithSM2Signer = new();
+            var message = "";
+            var privateKey = "";
+            var sign = sM3WithSM2Signer.Sign(message, privateKey);
+            var exceptResult = "1ab21d8355cfa17f8e61194831e81a8f22bec8c728fefb747ed035eb5082aa2b";
+            Assert.AreEqual(exceptResult, sign);
+        }
+    }
+}

cf5f56904123b76a9f0fffea7e29a2612585715a
JeffreySu <www.jeffrey.su@gmail.com>
2024-09-11 23:30:03 +0800

    Updated copyright Year

--- src/Senparc.Weixin.Open/Senparc.Weixin.Open/WxaAPIs/SecurityJson/GetPrivacyInterfaceJsonResult.cs
+++ src/Senparc.Weixin.Open/Senparc.Weixin.Open/WxaAPIs/SecurityJson/GetPrivacyInterfaceJsonResult.cs
@@ -19,7 +19,7 @@ Detail: https://github.com/JeffreySu/WeiXinMPSDK/blob/master/license.md
 #endregion Apache License Version 2.0
 
 /*----------------------------------------------------------------
-    Copyright (C) 2020 Senparc
+    Copyright (C) 2024 Senparc
 
     文件名：GetPrivacyInterfaceJsonResult.cs
     文件功能描述：获取隐私接口列表结果

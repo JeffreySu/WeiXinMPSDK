0445b9766856a8af2a32cc4e11817b4c39a78469
JeffreySu <www.jeffrey.su@gmail.com>
2025-07-31 22:11:13 +0800

    优化 Console 示例 #3163

--- Samples/All/console/Senparc.Weixin.MP.Sample.Consoles/Program.cs
+++ Samples/All/console/Senparc.Weixin.MP.Sample.Consoles/Program.cs
@@ -43,11 +43,16 @@ using Senparc.Weixin.Entities;
 using Senparc.Weixin.MP;
 using Senparc.Weixin.RegisterServices;
 
+var dt_Start = SystemTime.Now;
+
 var builder = new ConfigurationBuilder();
-var environment = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? "Production";
+var environment = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? "";
 
 builder.AddJsonFile("appsettings.json", false, true);
-builder.AddJsonFile($"appsettings.{environment}.json", false, true);
+if (environment.IsNullOrEmpty() is false)
+{
+    builder.AddJsonFile($"appsettings.{environment}.json", false, true);
+}
 Console.WriteLine("完成 appsettings.json 添加");
 
 var config = builder.Build();
@@ -55,11 +60,12 @@ Console.WriteLine("完成 ServiceCollection 和 ConfigurationBuilder 初始化")
 
 //更多绑定操作参见：https://docs.microsoft.com/en-us/aspnet/core/fundamentals/configuration/?view=aspnetcore-8.0
 var senparcSetting = config.GetSection("SenparcSetting").Get<SenparcSetting>();
-var senparcWeixinSetting = config.GetSection("SenparcWeixinSetting").Get< SenparcWeixinSetting>();
+var senparcWeixinSetting = config.GetSection("SenparcWeixinSetting").Get<SenparcWeixinSetting>();
 
 var services = new ServiceCollection();
 services.AddMemoryCache();//使用本地缓存必须添加
 
+var dt_AddSdkStart = SystemTime.Now;
 
 #region 添加微信配置（一行代码）
 
@@ -68,7 +74,12 @@ services.AddSenparcWeixinServices(config);
 
 #endregion
 
+var dt_AddSdkEnd = SystemTime.Now;
+
+
 var app = builder.Build();
+
+var dt_UseSdkStart = SystemTime.Now;
 
 #region 启用微信配置（一句代码）
 
@@ -88,8 +99,14 @@ var registerService = app.UseSenparcWeixin(
 
 #endregion
 
-//可以开始使用
+var dt_UseSdkEnd = SystemTime.Now;
 
+
+/* 开始使用
+ * 此处可以添加高级接口（如推送消息）等业务逻辑，或添加监听
+ */
+
+/*
 var weixinSetting = Senparc.Weixin.Config.SenparcWeixinSetting;
 
 Console.WriteLine("## weixinSetting");
@@ -97,5 +114,18 @@ Console.WriteLine(weixinSetting.ToJson(true, new Newtonsoft.Json.JsonSerializerS
 Console.WriteLine();
 Console.WriteLine("## 【盛派网络小助手】公众号");
 Console.WriteLine(weixinSetting.Items["【盛派网络小助手】公众号"].ToJson(true));
+*/
 
-Console.ReadLine();
+Console.WriteLine();
+var totalMs = (dt_AddSdkEnd - dt_AddSdkStart).TotalMilliseconds + (dt_UseSdkEnd - dt_UseSdkStart).TotalSeconds.ToString("#.##");
+Console.WriteLine($"Senparc.Weixin SDK 已启动。");
+Console.WriteLine($"程序完整启动时间：{SystemTime.DiffTotalMS(dt_Start).ToString("#.##")}毫秒，其中 Senparc.Weixin SDK 启动总用时：{totalMs}毫秒");
+Console.WriteLine();
+Console.WriteLine("开源示例地址：https://github.com/JeffreySu/WeiXinMPSDK/tree/master/Samples/All/console");
+Console.WriteLine();
+Console.WriteLine("输入 exit 退出程序...");
+
+while (Console.ReadLine() != "exit")
+{
+    continue;
+}

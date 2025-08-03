eb46b5c2d80367d3207b52a2343a9bb2df3fe8dd
Wang Qian <qian05@email.swu.edu.cn>
2025-08-03 12:43:16 +0800

    使用NeuChar抽象标准重新封装WorkBotRequestMessageBase类

--- src/Senparc.Weixin.Work/Senparc.Weixin.Work/Entities/Request/WorkBotRequestMessageBase.cs
+++ src/Senparc.Weixin.Work/Senparc.Weixin.Work/Entities/Request/WorkBotRequestMessageBase.cs
@@ -8,23 +8,42 @@
     创建标识：Wang Qian - 20250802
 ----------------------------------------------------------------*/
 using Senparc.NeuChar;
-using Senparc.Weixin.Work.Entities.BotChatObjects;
+using Senparc.NeuChar.Entities;
 
 namespace Senparc.Weixin.Work.Entities
 {
-    
-    public class WorkBotRequestMessageBase
+    /// <summary>
+    /// 会话类型，single\group，分别表示：单聊\群聊
+    /// </summary>
+    public enum ChatType 
     {
-        
-        /// <summary>
-        /// 对应msgid，本次回调的唯一性标志，开发者需据此进行事件排重（可能因为网络等原因重复回调）
-        /// </summary>
-        public string MsgId { get; set; }
+        Single,
+        Group
+    }
 
-        /// <summary>
-        /// 对应aibotid，智能机器人id
-        /// </summary>
-        public string AIBotId { get; set; }
+    public interface IWorkBotRequestMessageBase : IRequestMessageBase
+    {
+        string ChatId { get; set; }
+        ChatType ChatType { get; set; }
+
+    }
+    
+    /// <summary>
+    /// 企业微信智能机器人接收到请求的消息基类
+    /// </summary>
+    public abstract class WorkBotRequestMessageBase : RequestMessageBase, IWorkBotRequestMessageBase
+    {
+        //基类中包含:
+        
+        //RequestMessageBase 中包含：
+        //MsgId:对应msgid，本次回调的唯一性标志，开发者需据此进行事件排重（可能因为网络等原因重复回调）
+        //MsgType:对应msgtype，消息类型
+
+        //MessageBase 中包含:
+        //ToUserName: 对应aibotid，智能机器人id
+        //FromUserName: 对应userid，用户id
+        
+
 
         /// <summary>
         /// 对应chatid，会话id，仅群聊类型时候返回
@@ -36,17 +55,14 @@ namespace Senparc.Weixin.Work.Entities
         /// </summary>
         public ChatType ChatType { get; set; }
 
-        /// <summary>
-        /// 对应from，该事件触发者的信息，里面包含userid
-        /// </summary>
-        public From From { get; set; }
-        
-        /// <summary>
-        /// 消息类型，对应msgtype参数，使用NeuChar中的RequestMsgType枚举，支持文本、图片、语音、视频、位置以及链接信息。
-        /// </summary>
-        public virtual MsgType MsgType 
+        //默认返回Unknown，如果返回Unknown，说明是mixed或者stream消息，要通过msgtype来判断，特殊处理
+        public override RequestMsgType MsgType 
         { 
-            get { return MsgType.Unknown; }
+            get
+            {
+                return RequestMsgType.Unknown;
+            }
         }
+
     }
 }
